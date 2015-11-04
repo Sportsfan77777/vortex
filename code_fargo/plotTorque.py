14b474d92166023c226cc87a2ac2bd5460909cca
Sportsfan77777 <mhammer44444@gmail.com>
2015-11-03 19:26:12 -0700

    created function for the two rla options

--- code_fargo/plotTorque.py
+++ code_fargo/plotTorque.py
@@ -14,7 +14,9 @@ from scipy import signal as sig
 from scipy.ndimage import filters as ff
 
 # Smoothing Function
-smooth = lambda array : ff.gaussian_filter(array, 750) # choose smoothing filter
+smooth = lambda array, kernel_size : ff.gaussian_filter(array, kernel_size) # smoothing filter
+ks = 50.0 # Kernel Size
+ks_small = ks / 4.0 # Smaller kernel to check the normal kernel
 
 # Load Data (and choose subset) = x-axis
 rate = 1 # If 1, choose all of the data. If >1, choose all_data / rate
@@ -26,75 +28,61 @@ x = (data[:,-1])[select]
 # Plot Parameters
 alpha = 0.2 # for non-smoothed curves
 fontsize = 14
+linewidth = 2
 
-############# NO ROCHE LOBE AVOIDANCE #############
+def make_plot(rla = True):
+    if not rla:
+        # No Roche Lobe Avoidance
+        y1_base = data[:,1]
+        y2_base = data[:,2]
+        title = "Torque! (with no roche lobe avoidance)"
+        save_fn = "TorqueEvolution_with_no_rla.png"
+    else:
+        # Roche Lobe Tapering
+        y1_base = data[:,3]
+        y2_base = data[:,4]
+        title = "Torque! (with roche lobe tapering)"
+        save_fn = "TorqueEvolution_with_rla.png"
 
-# Data
+    # Data
 
-y1 = smooth(data[:,1])[select] # Torque from Inner Disk 
-y2 = smooth(data[:,2])[select] # Torque from Outer Disk
-y3 = y1 + y2
+    y1 = smooth(y1_base, ks_small)[select] # Torque from Inner Disk 
+    y2 = smooth(y2_base, ks_small)[select] # Torque from Outer Disk
+    y3 = y1 + y2
 
-s1 = smooth(data[:,1])[select] # Torque from Inner Disk 
-s2 = smooth(data[:,2])[select] # Torque from Outer Disk
-s3 = s1 + s2
+    s1 = smooth(y1_base, ks)[select] # Torque from Inner Disk (smoothed)
+    s2 = smooth(y2_base, ks)[select] # Torque from Outer Disk (smoothed)
+    s3 = s1 + s2
 
-# Curves
+    # Curves
 
-plot.plot(x, y1, c = "r", alpha = alpha)
-plot.plot(x, y2, c = "g", alpha = alpha)
-plot.plot(x, y3, c = "b", alpha = alpha)
+    plot.plot(x, y1, c = "r", alpha = alpha, linewidth = linewidth - 1)
+    plot.plot(x, y2, c = "g", alpha = alpha, linewidth = linewidth - 1)
+    plot.plot(x, y3, c = "b", alpha = alpha, linewidth = linewidth - 1)
 
-plot.plot(x, s1, c = "r", label = "Inner")
-plot.plot(x, s2, c = "g", label = "Outer")
-plot.plot(x, s3, c = "b", label = "Inner + Outer")
+    plot.plot(x, s1, c = "r", label = "Inner", linewidth = linewidth)
+    plot.plot(x, s2, c = "g", label = "Outer", linewidth = linewidth)
+    plot.plot(x, s3, c = "b", label = "Inner + Outer", linewidth = linewidth)
 
-plot.legend()
+    plot.plot([x[0], x[-1]], [0, 0], c = "black", linewidth = linewidth) # Zero Reference Line
 
-# Annotate
+    plot.legend()
 
-plot.title("Torque! (with no roche lobe avoidance)", fontsize = fontsize + 2)
-plot.xlabel("Time", fontsize = fontsize)
-plot.ylabel("Torque", fontsize = fontsize)
+    # Annotate
 
-# Save and close
+    plot.title(title, fontsize = fontsize + 2)
+    plot.xlabel("Timestep", fontsize = fontsize)
+    plot.ylabel("Torque", fontsize = fontsize)
 
-plot.savefig("TorqueEvolution_no_rla.png", bbox_inches = 'tight')
-plot.show()
+    # Save and Close
 
-plot.cla()
+    plot.savefig(save_fn, bbox_inches = 'tight')
+    plot.show()
 
-############# ROCHE LOBE TAPERING #############
+    plot.cla()
 
-# Data
 
-y1 = (data[:,3])[select] # Torque from Inner Disk 
-y2 = (data[:,4])[select] # Torque from Outer Disk
-y3 = y1 + y2
+### PLOTTING ###
 
-s1 = smooth(data[:,3])[select] # Torque from Inner Disk 
-s2 = smooth(data[:,4])[select] # Torque from Outer Disk
-s3 = s1 + s2
-
-# Curves
-
-plot.plot(x, y1, c = "r", alpha = alpha)
-plot.plot(x, y2, c = "g", alpha = alpha)
-plot.plot(x, y3, c = "b", alpha = alpha)
-
-plot.plot(x, s1, c = "r", label = "Inner Disk")
-plot.plot(x, s2, c = "g", label = "Outer Disk")
-plot.plot(x, s3, c = "b", label = "Inner + Outer")
-
-plot.legend()
-
-# Annotate
-
-plot.title("Torque! (with roche lobe tapering)", fontsize = fontsize + 2)
-plot.xlabel("Time", fontsize = fontsize)
-plot.ylabel("Torque (units???)", fontsize = fontsize)
-
-# Save
-
-plot.savefig("TorqueEvolution_with_rla.png", bbox_inches = 'tight')
-plot.show()
+make_plot(rla = True) # Roche Lobe Tapering
+make_plot(rla = False) # No Roche Lobe Avoidance
