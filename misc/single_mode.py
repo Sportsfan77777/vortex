929012a256589df461080c8d5c380c8fa7694308
Sportsfan77777 <mhammer44444@gmail.com>
2015-11-18 16:14:22 -0700

    split single mode to basic and in time

--- misc/single_mode.py
+++ misc/single_mode.py
@@ -5,59 +5,41 @@ plots single mode
 import sys
 import numpy as np 
 from matplotlib import pyplot as plot
+from matplotlib import ticker
 
 import subprocess
 
-### Movie Commands ###
-def make_movie(mode):
-    # Movie Parameters
-    fps = 5
+xs = np.linspace(0, 2*np.pi, 100)
+xs_deg = np.linspace(0, 360, 100)
 
-    directory = "mode%d" % mode
-
-    path = directory + "/" + directory + "_%03d.png"
-    output = directory + "/" + directory + ".mov" 
-
-    # Movie Command
-    command = "ffmpeg -f image2 -r %d -i %s -vcodec mpeg4 -y %s" % (fps, path, output)
-    split_command = command.split()
-    subprocess.Popen(split_command)
-
-
-xs = np.linspace(0, 10, 100)
-times = lambda max_t : np.linspace(0, max_t, 10 * max_t)
+ticks = ticker.MultipleLocator(base = 60)
 
 # Plotting
 fontsize = 20
 linewidth = 3
 
-def make_plot(mode, t_max = 0):
-    ts = times(t_max)
-    for time in ts:
-        """ sin wave with frequency 'mode' """
-        ys = [np.sin(mode * x - np.pi * time / 10) for x in xs]
+def make_plot(mode):
+    """ sin wave with frequency 'mode' """
+    ys = [np.sin(mode * x) for x in xs]
 
-        figure = plot.figure(figsize = (10, 3))
+    figure, ax = plot.subplots(figsize = (10, 3))
 
-        plot.plot(xs, ys, 'b', linewidth = linewidth + 1)
+    plot.plot(xs_deg, ys, 'b', linewidth = linewidth + 1)
 
-        # Limit
-        plot.xlim(0, 2 * np.pi)
-        plot.ylim(-1, 1)
+    # Limit
+    plot.xlim(0, 360)
+    plot.ylim(-1, 1)
 
-        # Annotate
-        plot.xlabel(r"$\phi$", fontsize = fontsize)
-        plot.title("Mode $m = %d$" % mode, fontsize = fontsize + 2)
+    # Annotate
+    plot.xlabel(r"$\phi$ (degrees)", fontsize = fontsize)
+    ax.xaxis.set_major_locator(ticks)
+    plot.title("Mode $m = %d$" % mode, fontsize = fontsize + 2)
 
-        # Save and Close
-        plot.savefig("mode%d/mode%d_%03d.png" % (mode, mode, time), bbox_inches = 'tight')
-        #plot.show()
-        plot.close(figure)
+    # Save and Close
+    plot.savefig("mode%d.png" % (mode), bbox_inches = 'tight')
+    #plot.show()
+    plot.close(figure)
 
 mode_num = int(sys.argv[1])
 
-make_plot(mode_num, t_max = 40)
-
-make_movie(mode_num)
-
-
+make_plot(mode_num)
