c7bc06e83b55ab1a6e4cf27016ab3afd69bf5369
Sportsfan77777 <mhammer44444@gmail.com>
2016-01-14 16:28:10 -0700

    fixed indentation

--- code_fargo/cleanseOrbitFile.py
+++ code_fargo/cleanseOrbitFile.py
@@ -10,9 +10,9 @@ f = open(fn, "r+")
 lines = f.readlines()
 f.seek(0)
 for line in lines:
-	line_sp = line.split()
+    line_sp = line.split()
     if len(line_sp) == 6:
-    	# There should be six entries in each line (http://fargo.in2p3.fr/Output)
+        # There should be six entries in each line (http://fargo.in2p3.fr/Output)
         f.write(line)
 f.truncate()
 f.close()
