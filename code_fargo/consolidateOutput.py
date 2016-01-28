794742bf1f0369af7ecd16e901fae61795477bde
Sportsfan77777 <mhammer44444@gmail.com>
2016-01-27 17:55:21 -0700

    first commit to condensing output

---
+++ code_fargo/consolidateOutput.py
@@ -0,0 +1,59 @@
+"""
+reduces storage by keeping only 1/n of output files
+
+(Step 1) python renameExtraOutput.py
+(Step 2) python consolidateOutput.py
+"""
+
+import sys
+import os
+import shutil
+import subprocess
+import glob
+
+prefixes = ['gasdens', 'gasvrad', 'gasvtheta']
+new_prefixes = ['rm_gasdens', 'rm_gasvrad', 'rm_gasvtheta'] # re-named prefixes
+
+density_files = glob.glob("*gasdens*.dat")
+
+def find_max_frame():
+    max_frame = 0
+    for d_f in density_files:
+        name = d_f.split(".")[0] # for "gasdens999.dat", just "gasdens999"
+        frame_number = int(name[7:]) # just 999
+        if frame_number > max_frame:
+            max_frame = frame_number
+    return max_frame
+
+num_frames = find_max_frame() + 1
+
+############# ############# ############# #############
+
+rate = 5
+
+for new_prefix in new_prefixes:
+    for i in range(num_frames):
+        if (i % rate) == 0:
+            # File should be kept
+            pass
+        else:
+            # Delete File
+            old = new_prefix + i # Note only re-named files can be deleted
+        
+            #os.remove(old)
+            print old, new
+
+
+for prefix in prefixes:
+    for i in range(num_frames):
+        if (i % rate) == 0:
+            # All that is left should be re-numbered
+            new_number = i / rate
+
+            old = prefix + i
+            new = prefix + new_number
+
+            #shutil.move(old, new)
+            print old, new
+
+
---
+++ code_fargo/renameExtraOutput.py
@@ -0,0 +1,46 @@
+"""
+reduces storage by keeping only 1/n of output files
+
+(Step 1) python renameExtraOutput.py
+(Step 2) python consolidateOutput.py
+"""
+
+import sys
+import os
+import shutil
+import subprocess
+
+prefixes = ['gasdens', 'gasvrad', 'gasvtheta']
+new_prefixes = ['rm_gasdens', 'rm_gasvrad', 'rm_gasvtheta']
+
+density_files = glob.glob("*gasdens*.dat")
+
+def find_max_frame():
+    max_frame = 0
+    for d_f in density_files:
+        name = d_f.split(".")[0] # for "gasdens999.dat", just "gasdens999"
+        frame_number = int(name[7:]) # just 999
+        if frame_number > max_frame:
+            max_frame = frame_number
+    return max_frame
+
+num_frames = find_max_frame() + 1
+
+############# ############# ############# #############
+
+rate = 5
+
+for prefix, new_prefix in zip(prefixes, new_prefixes):
+    for i in range(num_frames):
+        if (i % rate) == 0:
+            # File should be kept
+            pass
+        else:
+            # Rename file
+            old = prefix + i
+            new = new_prefix + i
+
+            #shutil.move(old, new)
+            print old, new
+
+
