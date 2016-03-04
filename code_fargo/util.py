632ef1544ac3115c42a1e286a56524ef8e2bab4b
Sportsfan77777 <mhammer44444@gmail.com>
2016-03-04 14:43:14 -0700

    added max frame to util

--- code_fargo/util.py
+++ code_fargo/util.py
@@ -5,7 +5,28 @@ Usage:
 not to be called
 """
 
+import os
+import glob
+
 import utilTorque
+
+#### Miscellaneous ####
+
+def find_max_frame():
+    # glob the right set of files
+    fargo_fn = "fargo2D1D"
+    if os.path.exists(fargo_fn):
+        density_files = glob.glob("gasdens1D*.dat")
+    else:
+        density_files = glob.glob("gasdens*.dat")
+    # Find the highest numbered file
+    max_frame = 0
+    for d_f in density_files:
+        name = d_f.split(".")[0] # for "gasdens999.dat", just "gasdens999"
+        frame_number = int(name[7:]) # just 999
+        if frame_number > max_frame:
+            max_frame = frame_number
+    return max_frame
 
 
 #### Torque Functions ####
