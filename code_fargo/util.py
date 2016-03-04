f12f810e2c91ced314593943f2504e8db244bb53
Sportsfan77777 <mhammer44444@gmail.com>
2016-03-04 14:48:27 -0700

    fixed indexing problem

--- code_fargo/util.py
+++ code_fargo/util.py
@@ -17,13 +17,15 @@ def find_max_frame():
     fargo_fn = "fargo2D1D"
     if os.path.exists(fargo_fn):
         density_files = glob.glob("gasdens1D*.dat")
+        number_index = 9 # the number starts at index 9 after 'gasdens1D'
     else:
         density_files = glob.glob("gasdens*.dat")
+        number_index = 7 # the number starts at index 7 after 'gasdens'
     # Find the highest numbered file
     max_frame = 0
     for d_f in density_files:
         name = d_f.split(".")[0] # for "gasdens999.dat", just "gasdens999"
-        frame_number = int(name[7:]) # just 999
+        frame_number = int(name[number_index:]) # just 999
         if frame_number > max_frame:
             max_frame = frame_number
     return max_frame
