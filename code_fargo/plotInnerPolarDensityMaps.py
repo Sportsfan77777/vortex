4a9a10cce17745a9ee91271ea2023b20574a1de9
Sportsfan77777 <mhammer44444@gmail.com>
2016-03-17 16:19:38 -0700

    first commit to inner polar density

---
+++ code_fargo/plotInnerPolarDensityMaps.py
@@ -0,0 +1,168 @@
+"""
+plot 2-D density maps
+
+python plotInnerPolarDensityMaps.py
+python plotInnerPolarDensityMaps.py frame_number
+python plotInnerPolarDensityMaps.py -1 <<<===== Plots a sample
+python plotInnerPolarDensityMaps.py -m
+"""
+
+import sys
+import os
+import subprocess
+import pickle
+import glob
+from multiprocessing import Pool
+
+import math
+import numpy as np
+
+
+import matplotlib
+#matplotlib.use('Agg')
+from matplotlib import rcParams as rc
+from matplotlib import pyplot as plot
+
+from pylab import rcParams
+from pylab import fromfile
+
+
+save_directory = "innerPolarGasDensityMaps"
+
+
+### Movie Commands ###
+def make_movies():
+    # Movie Parameters
+    fps = 5
+
+    path = save_directory + "/densityMap_%03d.png"
+    output = save_directory + "/densityMap.mov"
+
+    zoom_path = save_directory + "/zoom_densityMap_%03d.png"
+    zoom_output = save_directory + "/densityMap_zoom.mov"
+
+    # Movie Command
+    command = "ffmpeg -f image2 -r %d -i %s -vcodec mpeg4 -y %s" % (fps, path, output)
+    split_command = command.split()
+    subprocess.Popen(split_command)
+
+    command = "ffmpeg -f image2 -r %d -i %s -vcodec mpeg4 -y %s" % (fps, zoom_path, zoom_output)
+    split_command = command.split()
+    subprocess.Popen(split_command)
+
+# Make only movies and then return
+if (len(sys.argv) > 1) and (sys.argv[1] == "-m"):
+    make_movies()
+    # Terminate
+    quit()
+
+
+### Get FARGO Parameters ###
+# Create param file if it doesn't already exist
+param_fn = "params.p"
+if not os.path.exists(param_fn):
+    command = "python pickleParameters.py"
+    split_command = command.split()
+    subprocess.Popen(split_command)
+fargo_par = pickle.load(open(param_fn, "rb"))
+
+num_rad = np.loadtxt("dims.dat")[-2]
+num_theta = np.loadtxt("dims.dat")[-1]
+
+rad = np.loadtxt("used_rad.dat")[:-1]
+theta = np.linspace(0, 2 * np.pi, num_theta)
+
+surface_density_zero = float(fargo_par["Sigma0"])
+scale_height = float(fargo_par["AspectRatio"])
+
+##### PLOTTING #####
+
+# Make Directory
+try:
+    os.mkdir(save_directory)
+except:
+    print "Directory Already Exists"
+
+# Plot Parameters
+cmap = "RdYlBu_r"
+clim = [0, 5]
+
+fontsize = 14
+
+def make_plot(frame, show = False):
+    # Orbit Number
+    time = float(fargo_par["Ninterm"]) * float(fargo_par["DT"])
+    orbit = int(round(time / (2 * np.pi), 0)) * frame
+
+    # Set up figure
+    fig = plot.figure()
+    ax = fig.add_subplot(111, polar = True) 
+
+    # Axis
+    rmax = 1.0 # to match ApJL paper
+
+    plot.xticks([], []) # Angles
+    plot.yticks([rmax], ["%.1f" % rmax]) # Max Radius
+    plot.ylim(0, rmax)
+
+    # Data
+    density = (fromfile("gasdens%d.dat" % frame).reshape(num_rad, num_theta))
+    normalized_density = density / surface_density_zero
+
+    ### Plot ###
+    result = ax.pcolormesh(theta, rad, normalized_density, cmap = cmap)
+    fig.colorbar(result)
+    result.set_clim(clim[0], clim[1])
+
+    # Annotate
+    plot.title("Gas Density Map at Orbit %d" % orbit, fontsize = fontsize + 1)
+
+    # Save and Close
+    plot.savefig("%s/innerDensityMap_%04d.png" % (save_directory, frame), bbox_inches = 'tight', dpi = my_dpi)
+    if show:
+        plot.show()
+    plot.close(fig) # Close Figure (to avoid too many figures)
+
+
+##### Plot One File or All Files #####
+
+def find_max_frame():
+    max_frame = 0
+    for d_f in density_files:
+        name = d_f.split(".")[0] # for "gasdens999.dat", just "gasdens999"
+        frame_number = int(name[7:]) # just 999
+        if frame_number > max_frame:
+            max_frame = frame_number
+    return max_frame
+
+if len(sys.argv) > 1:
+    frame_number = int(sys.argv[1])
+    if frame_number == -1:
+        # Plot Sample
+        max_frame = find_max_frame()
+        sample = np.linspace(50, max_frame, 10) # 10 evenly spaced frames
+        for i in sample:
+            make_plot(i)
+    else:
+        # Plot Single
+        make_plot(frame_number, show = True)
+else:
+    # Search for maximum frame
+    density_files = glob.glob("gasdens*.dat")
+    max_frame = find_max_frame()
+    num_frames = max_frame + 1
+
+    #for i in range(num_frames):
+    #    make_plot(i)
+
+    #### ADD TRY + CATCH BLOCK HERE!!!!! ####
+
+    p = Pool() # default number of processes is multiprocessing.cpu_count()
+    p.map(make_plot, range(num_frames))
+    p.terminate()
+
+    #### Make Movies ####
+    #make_movies()
+
+
+
