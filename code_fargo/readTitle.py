eb6150a14468d3b83194fcb4f0cb001206a7a505
Sportsfan77777 <mhammer44444@gmail.com>
2016-04-03 15:25:50 -0700

    added rw title

---
+++ code_fargo/readTitle.py
@@ -0,0 +1,21 @@
+"""
+reads a title to a directory
+
+Usage:
+(1) from readTitle import readTitle
+(2) python readTitle.py <=== prints title
+
+title is stored in 'title.p'
+"""
+
+import pickle
+
+def readTitle():
+	fn = "title.p"
+	title = pickle.load(open(fn, "rb"))
+	return title
+
+
+if __name__ == "__main__":
+	title = readTitle()
+	print "Title: %s" % title
