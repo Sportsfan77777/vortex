7e1fea989f70e96b49a7ae19519a6ec21e49df33
Sportsfan77777 <mhammer44444@gmail.com>
2016-04-03 17:24:22 -0700

    changed pickle parameters to include planet mass from jup cfg file

--- code_fargo/pickleParameters.py
+++ code_fargo/pickleParameters.py
@@ -30,10 +30,32 @@ def store(line):
         # Store
         par[name] = entry
 
+def store_planet_mass(line):
+    """ stores only the planet's mass """
+    line_sp = line.split()
+
+    if len(line_sp) < 1:
+        # Empty
+        pass
+    else:
+        # Parse
+        planet_name = line_sp[0]
+        if planet_name == "Jupiter":
+            par["PlanetMass"] = line_sp[2]
+
 # Read file line by line
 with open(par_file, "r") as f:
     for line in f:
         store(line)
+
+### NEW: Add planet mass too ###
+files = glob.glob("*.cfg") # usually Jup.cfg
+cfg_file = files[0]
+
+# Read file line by line
+with open(cfg_file, "r") as f:
+    for line in f:
+        store_planet_mass(line) 
 
 dict_name = "params.p"
 p.dump(par, open(dict_name, "wb"))
