4e72d66f6a8a4100b3e1e1d838c6c4281d6e8afd
Sportsfan77777 <mhammer44444@gmail.com>
2016-04-03 18:53:56 -0700

    newest make movie files

---
+++ code_fargo/renumber_to_tmp.py
@@ -0,0 +1,26 @@
+"""
+re-number to tmp files
+(useful for movies)
+
+Usage:
+import renumber, delete_tmp_files
+
+"""
+
+import os
+import shutil
+
+tmp_prefix = "tmp_"
+
+def renumber(old_range, new_range, base_path = None, base_name = None):
+    """ cp old files to new_files with 'tmp' prefix """
+    for (old_number, new_number) in zip(old_range, new_range):
+        old_file = "%s/%s%03d.png" % (base_path, base_name, old_number)
+        tmp_file = "%s/%s%s%03d.png" % (base_path, tmp_prefix, base_name, new_number)
+        shutil.copyfile(old_file, tmp_file)
+
+def delete_tmp_files(tmp_range, base_path = None, base_name = None):
+    """ delete all 'tmp' files in a particular range """
+    for number in tmp_range:
+        tmp_file = "%s/%s%s%03d.png" % (base_path, tmp_prefix, base_name, number)
+        os.remove(tmp_file)
