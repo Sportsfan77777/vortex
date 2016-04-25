90d6bc168bba1b06fb3d6704ef809e2d69fde8a6
Sportsfan77777 <mhammer44444@gmail.com>
2016-04-25 00:48:15 -0700

    changed upper bound

--- code_fargo/plotAsymmetry.py
+++ code_fargo/plotAsymmetry.py
@@ -48,7 +48,7 @@ scale_height = float(fargo_par["AspectRatio"])
 ### Helper Methods ###
 def find_peak(averagedDensity):
     outer_disk_start = np.searchsorted(rad, 1.1) # look for max radial density beyond r = 1.1
-    outer_disk_end = np.searchsorted(rad, 2.5)
+    outer_disk_end = np.searchsorted(rad, 2.2)
     peak_rad_outer_index = np.argmax(averagedDensity[outer_disk_start : outer_disk_end])
 
     peak_index = outer_disk_start + peak_rad_outer_index
