6027cc9708142722f884c002a45c3ef56cdac513
Sportsfan77777 <mhammer44444@gmail.com>
2016-04-26 20:29:28 -0700

    switched back default values

--- code_fargo/plotAsymmetryBasedOnDensity.py
+++ code_fargo/plotAsymmetryBasedOnDensity.py
@@ -144,8 +144,8 @@ def measure_asymmetry(frame):
         return azimuthal_extent, threshold, threshold, threshold
 
 ## Use These Frames ##
-rate = 25 # 5 works better, but is very slow
-start_of_vortex = 700
+rate = 5 # 5 works better, but is very slow
+start_of_vortex = 0
 max_frame = 1500 #util.find_max_frame()
 frame_range = range(start_of_vortex, max_frame, rate)
 
