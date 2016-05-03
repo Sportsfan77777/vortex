8b3f0ee6c54baaee554024e8e37e818dd30ac371
Sportsfan77777 <mhammer44444@gmail.com>
2016-05-02 19:23:59 -0700

    changed outer bound

--- code_fargo/plotFFT_DensityModesOverTime.py
+++ code_fargo/plotFFT_DensityModesOverTime.py
@@ -47,7 +47,7 @@ scale_height = float(fargo_par["AspectRatio"])
 ### Helper Methods ###
 def find_peak(averagedDensity):
     outer_disk_start = np.searchsorted(rad, 1.1) # look for max radial density beyond r = 1.1
-    outer_disk_end = np.searchsorted(rad, 2.6) # look for max radial density before r = 2.6
+    outer_disk_end = np.searchsorted(rad, 2.2) # look for max radial density before r = 2.6
     peak_rad_outer_index = np.argmax(averagedDensity[outer_disk_start : outer_disk_end])
 
     peak_index = outer_disk_start + peak_rad_outer_index
--- code_fargo/plotFFT_VortensityModesOverTime.py
+++ code_fargo/plotFFT_VortensityModesOverTime.py
@@ -56,7 +56,7 @@ scale_height = float(fargo_par["AspectRatio"])
 ### Helper Methods ###
 def find_peak(averagedDensity):
     outer_disk_start = np.searchsorted(rad, 1.1) # look for max radial density beyond r = 1.1
-    outer_disk_end = np.searchsorted(rad, 2.6) # look for max radial density before r = 2.6
+    outer_disk_end = np.searchsorted(rad, 2.2) # look for max radial density before r = 2.6
     peak_rad_outer_index = np.argmax(averagedDensity[outer_disk_start:])
 
     peak_index = outer_disk_start + peak_rad_outer_index
