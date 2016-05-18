ac7fc4891a4963ced63cea427d86a30294ec4803
Sportsfan77777 <mhammer44444@gmail.com>
2016-05-17 18:57:20 -0700

    choose the ending

---
+++ code_fargo/chooseVortexEnd.py
@@ -0,0 +1,21 @@
+"""
+choose when the vortex dies
+
+python chooseVortexEnd.py
+"""
+
+import pickle as p
+import numpy as np
+
+end_fn = "end.p"
+
+end_candidates_fn = "end_candidates.p"
+end_candidates = p.load(open(end_candidates_fn, "rb"))
+
+if len(end_candidates) == 1:
+	end = (end_candidates[0])[-1]
+	p.dump(end, open(end_fn, "wb"))
+else:
+	print end_candidates
+	end = input("Choose the end!")
+	p.dump(end, open(end_fn, "wb"))
