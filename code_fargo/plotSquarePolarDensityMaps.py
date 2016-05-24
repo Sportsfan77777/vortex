23d090fcd1a4154b4bc079dc2f843c22460c123a
Sportsfan77777 <mhammer44444@gmail.com>
2016-05-24 14:17:34 -0700

    first commit to square polar density

---
+++ code_fargo/plotSquarePolarDensityMaps.py
@@ -0,0 +1,188 @@
+"""
+plot 2-D density maps
+
+python plotDensityMaps.py
+python plotDensityMaps.py frame_number
+python plotDensityMaps.py -1 <<<===== Plots a sample
+python plotDensityMaps.py -m
+"""
+
+import sys
+import os
+import subprocess
+import pickle
+import glob
+from multiprocessing import Pool
+
+import math
+import numpy as np
+
+from scipy.interpolate import interp1d as interpolate
+from scipy.ndimage import map_coordinates
+
+import matplotlib
+#matplotlib.use('Agg')
+from matplotlib import rcParams as rc
+from matplotlib import pyplot as plot
+
+from pylab import rcParams
+from pylab import fromfile
+
+import util
+from readTitle import readTitle
+
+save_directory = "squarePolarGasDensityMaps"
+
+### Get FARGO Parameters ###
+# Create param file if it doesn't already exist
+param_fn = "params.p"
+if not os.path.exists(param_fn):
+    command = "python pickleParameters.py"
+    split_command = command.split()
+    subprocess.Popen(split_command)
+fargo_par = pickle.load(open(param_fn, "rb"))
+
+num_rad = np.loadtxt("dims.dat")[-2]
+num_theta = np.loadtxt("dims.dat")[-1]
+
+rad = np.loadtxt("used_rad.dat")[:-1]
+theta = np.linspace(0, 2 * np.pi, num_theta)
+
+surface_density_zero = float(fargo_par["Sigma0"])
+scale_height = float(fargo_par["AspectRatio"])
+
+### Converter ###
+
+def polar_to_cartesian(data, rs, thetas, order = 3):
+    # Source: http://stackoverflow.com/questions/2164570/reprojecting-polar-to-cartesian-grid
+    # Set up xy-grid
+    max_r = rs[-1]
+    resolution = 2 * len(rad)
+
+    xs = np.linspace(-max_r, max_r, resolution)
+    ys = np.linspace(-max_r, max_r, resolution)
+
+    xs_grid, ys_grid = np.meshgrid(xs, ys)
+
+    # Interpolate rt-grid
+
+    interpolated_rs = interpolate(rs, np.arange(len(rs)), bounds_error = False)
+    interpolated_thetas = interpolate(thetas, np.arange(len(t)))
+
+    # Match up xy-grid with rt-grid
+
+    new_rs = np.sqrt(np.power(xs_grid, 2) + np.power(ys_grid, 2))
+    new_thetas = np.arctan2(ys_grid, xs_grid)
+
+    new_interpolated_rs = interpolated_rs(new_rs.ravel())
+    new_interpolated_thetas = interpolated_thetas(new_thetas.ravel())
+
+    # Fix Bounds (outside of polar image, but inside cartesian image)
+    new_interpolated_rs[new_rs.ravel() > max(rs)] = len(rs) - 1
+    new_interpolated_rs[new_rs.ravel() < min(rs)] = 0
+
+    return map_coordinates(data, np.array([new_interpolated_rs, new_interpolated_thetas]), order = order).reshape(new_rs.shape)
+
+##### PLOTTING #####
+
+# Make Directory
+try:
+    os.mkdir(save_directory)
+except:
+    print "Directory Already Exists"
+
+# Plot Parameters
+cmap = "RdYlBu_r"
+clim = [0, 2]
+
+fontsize = 14
+my_dpi = 100
+
+
+def make_plot(frame, show = False):
+    # For each frame, make two plots (one with normal 'r' and one with '(r - 1) / h')
+    def choose_axis(i, axis):
+        # Orbit Number
+        time = float(fargo_par["Ninterm"]) * float(fargo_par["DT"])
+        orbit = int(round(time / (2 * np.pi), 0)) * i
+
+        # Set up figure
+        fig = plot.figure(figsize = (700 / my_dpi, 600 / my_dpi), dpi = my_dpi)
+        ax = fig.add_subplot(111)
+
+        # Axis
+        #angles = np.linspace(0, 2 * np.pi, 7)
+        #degree_angles = ["%d" % d_a for d_a in np.linspace(0, 360, 7)]
+
+        #plot.ylim(0, 2 * np.pi)
+        #plot.yticks(angles, degree_angles)
+        #if axis == "zoom":
+        #    x = (rad - 1) / scale_height
+        #    prefix = "zoom_"
+        #    plot.xlim(0, 40) # to match the ApJL paper
+        #    xlabel = r"($r - r_p$) $/$ $h$"
+        #else:
+        #    x = rad
+        #    prefix = ""
+        #    plot.xlim(float(fargo_par["Rmin"]), float(fargo_par["Rmax"]))
+        #    xlabel = "Radius"
+
+        # Data
+        density = (fromfile("gasdens%d.dat" % i).reshape(num_rad, num_theta)) /surface_density_zero
+        density_cart = polar_to_cartesian(density, rad, theta)
+
+        ### Plot ###
+        result = ax.pcolormesh(x, theta, np.transpose(density_cart), cmap = cmap)
+        fig.colorbar(result)
+        result.set_clim(clim[0], clim[1])
+
+        # Annotate
+        this_title = readTitle()
+        plot.xlabel(xlabel, fontsize = fontsize)
+        plot.ylabel(r"$\phi$", fontsize = fontsize)
+        plot.title("Gas Density Map at Orbit %d\n%s" % (orbit, this_title), fontsize = fontsize + 1)
+
+        # Save and Close
+        plot.savefig("%s/%sdensityMap_%04d.png" % (save_directory, prefix, i), bbox_inches = 'tight', dpi = my_dpi)
+        if show:
+            plot.show()
+        plot.close(fig) # Close Figure (to avoid too many figures)
+
+    i = frame
+    choose_axis(i, "normal")
+    choose_axis(i, "zoom")
+
+
+##### Plot One File or All Files #####
+
+if len(sys.argv) > 1:
+    frame_number = int(sys.argv[1])
+    if frame_number == -1:
+        # Plot Sample
+        max_frame = util.find_max_frame()
+        sample = np.linspace(50, max_frame, 10) # 10 evenly spaced frames
+        for i in sample:
+            make_plot(i)
+    else:
+        # Plot Single
+        make_plot(frame_number, show = True)
+else:
+    # Search for maximum frame
+    density_files = glob.glob("gasdens*.dat")
+    max_frame = find_max_frame()
+    num_frames = max_frame + 1
+
+    #for i in range(num_frames):
+    #    make_plot(i)
+
+    #### ADD TRY + CATCH BLOCK HERE!!!!! ####
+
+    p = Pool() # default number of processes is multiprocessing.cpu_count()
+    p.map(make_plot, range(num_frames))
+    p.terminate()
+
+    #### Make Movies ####
+    #make_movies()
+
+
+
