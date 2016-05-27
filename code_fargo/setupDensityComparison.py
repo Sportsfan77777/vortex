f226e0ba41fa2af4c34425d24a7f3579f2b12cdb
Sportsfan77777 <mhammer44444@gmail.com>
2016-05-26 18:11:57 -0700

    fixed typo

--- code_fargo/setupDensityComparison.py
+++ code_fargo/setupDensityComparison.py
@@ -120,28 +120,28 @@ plot.plot([line5_10, line5_10], [vertical_min_value, vertical_max_value], linewi
 
 ## Labels ##
 # 10^-4
-text_y4 = 1.20 * max_value
+text_y4 = max_value + 0.5
 plot.text(line4_20, text_y4, "20", horizontalalignment = 'center')
 plot.text(line4_10, text_y4, "10", horizontalalignment = 'center')
 plot.text(line4_5, text_y4, "5", horizontalalignment = 'center')
 
 midpoint4 = (line4_20 + line4_5) / 2.0
-plot.text(midpoint4, 1.27**2 * text_y4, r"$\dot{M}$ = $10^{-4}$ $M_J$", horizontalalignment = 'center')
+plot.text(midpoint4, text_y4 + 0.5, r"$\dot{M}$ = $10^{-4}$ $M_J$", horizontalalignment = 'center')
 
 # 10^-5
-text_y5 = 1.05 * max_value
+text_y5 = max_value + 0.3
 plot.text(line5_20, text_y5, "20 AU", horizontalalignment = 'center')
 plot.text(line5_10, text_y5, "10 AU", horizontalalignment = 'center')
 
 midpoint5 = (line5_20 + line5_10) / 2.0
-plot.text(midpoint5, 1.25 * 1.30 * text_y5, r"$\dot{M}$ = $10^{-5}$ $M_J$", horizontalalignment = 'center')
+plot.text(midpoint5, text_y5 + 0.5, r"$\dot{M}$ = $10^{-5}$ $M_J$", horizontalalignment = 'center')
 
 # Braces
-brace4_x, brace4_y = range_brace(line4_20, line4_5, height = 10000)
-plot.plot(brace4_x, 1.23 * text_y4 + brace4_y, color = "black", linewidth = brace_linewidth, clip_on = False)
+brace4_x, brace4_y = range_brace(line4_20, line4_5, height = 1)
+plot.plot(brace4_x, text_y4 + brace4_y, color = "black", linewidth = brace_linewidth, clip_on = False)
 
-brace5_x, brace5_y = range_brace(line5_20, line5_10, height = 10000)
-plot.plot(brace5_x, 1.23 * text_y5 + brace5_y, color = "black", linewidth = brace_linewidth, clip_on = False)
+brace5_x, brace5_y = range_brace(line5_20, line5_10, height = 1)
+plot.plot(brace5_x, text_y5 + brace5_y, color = "black", linewidth = brace_linewidth, clip_on = False)
 
 #### Curves ####
 plot.plot(case4_x, case4_y, marker = "s", markersize = markersize, linewidth = linewidth, label = case4_label) # M5, v7
