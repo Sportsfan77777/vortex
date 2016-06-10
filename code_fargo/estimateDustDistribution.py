3cb8519cfbd0c2ec6fef827e9c6a9c5b1bb23686
Michael Hammer <mhammer44444@gmail.com>
2016-06-09 20:03:32 -0700

    switched to azimuthal and annotated

--- code_fargo/estimateDustDistribution.py
+++ code_fargo/estimateDustDistribution.py
@@ -3,8 +3,31 @@ Uses Lyra+Lin 2013 (http://iopscience.iop.org/article/10.1088/0004-637X/775/1/17
 to calculate dust distribution
 """
 
+import sys
+
 import numpy as np
 from matplotlib import pyplot as plot
+
+# Constant
+default_S = 3
+
+if len(sys.argv) > 1:
+	default_S = int(sys.argv[1])
+
+def max_y(s = default_S):
+	if s == 1:
+		return 10
+	elif s == 3:
+		return 30
+	elif s == 5:
+		return 50
+	elif s == 10:
+		return 120
+
+# Helper Functions
+
+def semi_minor(angle, aspect_ratio, radius):
+	return radius * (angle * (np.pi / 180.0)) / aspect_ratio
 
 def calculate_xi(aspect_ratio):
 	return 1 + aspect_ratio**(-2)
@@ -21,9 +44,9 @@ def scale_function_sq(aspect_ratio):
 
 	return first_term - second_term
 
-def get_dust(x, aspect_ratio, max_density, S = 1):
+def get_dust(x, aspect_ratio, max_density, S = default_S):
 	f_sq = scale_function_sq(aspect_ratio)
-	print f_sq
+	#print f_sq
 
 	coeff = max_density * (S + 1)**(1.5)
 
@@ -33,18 +56,29 @@ def get_dust(x, aspect_ratio, max_density, S = 1):
 	return coeff * exp
 
 ten_density = 2.8
-ten_aspect_ratio = 9.89
+ten_radius_over_dr = 1.7 / 0.3
+ten_extent = 120.0 * (np.pi / 180)
+ten_aspect_ratio = ten_radius_over_dr * ten_extent
+
+print ten_aspect_ratio
 
 thousand_density = 1.85
-thousand_aspect_ratio = 26.17
+thousand_radius_over_dr = 1.5 / 0.25
+thousand_extent = 240.0 * (np.pi / 180)
+thousand_aspect_ratio = thousand_radius_over_dr * thousand_extent
+
+print thousand_aspect_ratio
 
 ten_dust = lambda x : get_dust(x, ten_aspect_ratio, ten_density)
 thousand_dust = lambda x : get_dust(x, thousand_aspect_ratio, thousand_density)
 
-xs = np.linspace(0, 5, 500)
+num_xs = 500
 
-ten_ys = [ten_dust(x) for x in xs]
-thousand_ys = [thousand_dust(x) for x in xs]
+ten_xs = np.linspace(-180, 180, num_xs)
+ten_ys = [ten_dust(semi_minor(x, ten_aspect_ratio / 2.0, ten_radius_over_dr)) for x in ten_xs]
+
+thousand_xs = np.linspace(-180, 180, num_xs)
+thousand_ys = [thousand_dust(semi_minor(x, thousand_aspect_ratio / 2.0, thousand_radius_over_dr)) for x in thousand_xs]
 
 ### PLOTTING ###
 
@@ -52,38 +86,36 @@ thousand_ys = [thousand_dust(x) for x in xs]
 linewidth = 4
 fontsize = 16
 
+alpha = 0.3
+
 my_dpi = 100
 
-def make_plot(use_offset = False):
-
+def make_plot():
 	# Curves
-	if use_offset:
-		offset = 0.56
-	else:
-		offset = 1.0
-	plot.plot(xs * offset, ten_ys, linewidth = linewidth, label = r"$T_{growth} = 10$ $\rm{orbits}$")
-
-	if use_offset:
-		offset = 0.33
-	else:
-		offset = 1.0
-	plot.plot(xs * offset, thousand_ys, linewidth = linewidth, label = r"$T_{growth} = 1000$ $\rm{orbits}$")
+	plot.plot(ten_xs, ten_ys, linewidth = linewidth, label = r"$T_{growth} = 10$ $\rm{orbits}$")
+	plot.plot(thousand_xs, thousand_ys, linewidth = linewidth, label = r"$T_{growth} = 1000$ $\rm{orbits}$")
 
 	# Axes
-	if use_offset:
-		plot.xlim(0, 2)
-	else:
-		plot.xlim(0, 3)
-	plot.ylim(0, 8)
+	y_max = max_y(default_S)
+	plot.xlim(-180, 180)
+	plot.ylim(0, y_max)
+
+	angles = np.linspace(-180, 180, 7)
+	plot.xticks(angles)
+
+	# Hatched Regions
+	plot.text(0, 6, "Vortex Extents", fontsize = fontsize - 2, horizontalalignment = "center")
+	plot.fill_between([-60, 60, 60, -60], [3, 3, 4.5, 4.5], hatch = "+", color = "b", edgecolor = "b", alpha = alpha, linewidth = 0)
+	plot.fill_between([-120, 120, 120, -120], [0.5, 0.5, 2, 2], hatch = "X", color = "g", edgecolor = "g", alpha = alpha, linewidth = 0)
 
 	# Annotate
-	plot.xlabel(r"Distance from Vortex Center: $|r - r_c| / H$", fontsize = fontsize)
+	plot.xlabel(r"Distance from Vortex Center: $\phi - \phi_c$", fontsize = fontsize)
 	plot.ylabel("Normalized Dust Density", fontsize = fontsize)
-	plot.title("Comparing Dust Distributions for\n" + r"$m_p = 1$ $M_J$, $\nu_{disk} = 10^{-7}$", fontsize = fontsize + 1)
+	plot.title("Steady-state (mm-size) Dust Distributions for\n" + r"$m_p = 1$ $M_J$, $\nu_{disk} = 10^{-7}$", fontsize = fontsize + 1)
 	plot.legend(loc = "upper right")
 
 	# Save + Show
-	plot.savefig("dust_comparison.png", bbox_inches = "tight", dpi = my_dpi)
+	plot.savefig("dust_comparison_s%d.png" % default_S, bbox_inches = "tight", dpi = my_dpi)
 	plot.show()
 
 make_plot()
