44eac9f931d50ba4c099f1925e6c033caae101f8
Michael Hammer <mhammer44444@gmail.com>
2016-06-30 20:44:22 -0400

    this works and itis annotatedd

--- code_fargo/estimateTripleDust.py
+++ code_fargo/estimateTripleDust.py
@@ -15,17 +15,12 @@ default_S = 3
 if len(sys.argv) > 1:
     default_S = int(sys.argv[1])
 
-def max_y(s = default_S):
-    if s == 1:
-        return 10
-    elif s == 3:
-        return 30
-    elif s == 5:
-        return 50
-    elif s == 10:
-        return 120
-
 # Helper Functions
+
+def resort(arr):
+    print arr
+    print len(arr)
+    return [arr[0], arr[2], arr[4], arr[1], arr[3], arr[5]]
 
 def semi_minor(angle, aspect_ratio, radius):
     return radius * (angle * (np.pi / 180.0)) * 0.06 * 2 #aspect_ratio
@@ -59,29 +54,37 @@ def get_dust(x, aspect_ratio, max_density, S = default_S):
 ### PLOTTING ###
 # Options
 growth = [1000, 4000, 1000]
-dashes = [[8, 8], [10**5, 1], [10, 4]]
+dashes = [[14, 10, 6, 10], [10**5, 1], [3, 3]]
+
+mass = [1, 5, 5]
+viscosity = [-7, -7, -6]
+
+sizes = [r"$\rm{cm}$", r"$\rm{mm}$", r"$\rm{\mu m}$"]
 
 # Parameters
-linewidth = 4
+linewidths = [4, 5, 4]
 fontsize = 16
 
 alpha = 0.3
 
 my_dpi = 100
 
-def add_to_plot(number, aspect_ratios, densities, r_ratios, str_r_ratios, angles):
+def add_to_plot(number, aspect_ratios, densities, r_ratios, rs, drs, angles):
     # Unwrap
     aspect1, aspect2 = aspect_ratios
     density1, density2 = densities
     r_over_dr1, r_over_dr2 = r_ratios
-    str_r_over_dr1, str_r_over_dr2 = str_r_ratios
+    r1, r2 = rs
+    dr1, dr2 = drs
     extent1, extent2 = angles
 
+    print density1, density2
+
     # Use these particle sizes
-    S_range = 3 * np.array([10**(-3), 10**(0), 10**(1)]) # um, mm, cm
+    S_range = 3 * np.array([10**(1), 10**(0), 10**(-3)]) # cm, mm, um
 
     # Subplot
-    for S in S_range:
+    for i, S in enumerate(S_range):
         # Distributions
         dust1 = lambda x : get_dust(x, aspect1, density1, S = S)
         dust2 = lambda x : get_dust(x, aspect2, density2, S = S)
@@ -89,48 +92,87 @@ def add_to_plot(number, aspect_ratios, densities, r_ratios, str_r_ratios, angles
         # Data
         num_xs = 500
 
-        xs1 = np.linspace(-180, 180, num_xs)
+        xs1 = np.linspace(0, 180, num_xs)
         ys1 = [dust1(semi_minor(x, aspect1 / 2.0, r_over_dr1)) for x in xs1]
 
-        xs2 = np.linspace(-180, 180, num_xs)
-        ys2 = [dust1(semi_minor(x, aspect2 / 2.0, r_over_dr2)) for x in xs2]
+        xs2 = np.linspace(0, 180, num_xs)
+        ys2 = [dust2(semi_minor(x, aspect2 / 2.0, r_over_dr2)) for x in xs2]
 
         # Identify vortex range
-        start1 = np.searchsorted(ten_xs, -extent1)
-        end1 = np.searchsorted(ten_xs, extent1)
+        start1 = np.searchsorted(xs1, -extent1 / 2)
+        end1 = np.searchsorted(xs1, extent1 / 2)
 
-        start2 = np.searchsorted(thousand_xs, -extent2)
-        end2 = np.searchsorted(thousand_xs, extent2)
+        start2 = np.searchsorted(xs2, -extent2 / 2)
+        end2 = np.searchsorted(xs2, extent2 / 2)
 
         # Labels
-        label1 = r"$S = " + str(S)+ r"$T_{growth} = 10$ $\rm{orbits}$"
-        label2 = r"$S = " + str(S)+ r"$, $T_{growth} = " + str(growth[number]) + "$ $\rm{orbits}$"
+        label1 = sizes[i] #+ r"$T_{growth} = 10$ $\rm{orbits}$"
+        label2 = "" #+ r"$, $T_{growth} = " + str(growth[i]) + r"$ $\rm{orbits}$"
 
         # Select Subplot
         ax = fig.add_subplot(1, 3, number + 1)
 
         # Curves
-        plot.plot(xs1, ys1, c = "b", alpha = alpha, linewidth = linewidth, dashes = dashes[number])
-        plot.plot(xs2, ys2, c = "g", alpha = alpha, linewidth = linewidth, dashes = dashes[number])
+        #plot.plot(xs1, ys1, c = "b", alpha = alpha, linewidth = linewidths[i], dashes = dashes[i])
+        #plot.plot(xs2, ys2, c = "g", alpha = alpha, linewidth = linewidths[i], dashes = dashes[i])
 
-        plot.plot(xs1[start1 : end1], ys1[start1 : end1], c = "b", linewidth = linewidth, label = label1)
-        plot.plot(xs2[start2 : end2], ys2[start2 : end2], c = "g", linewidth = linewidth, label = label2)    
-    
-    # Annotate
-    plot.xlabel(r"$\phi - \phi_{center}$", fontsize = fontsize)
-    plot.ylabel("Normalized Dust Density", fontsize = fontsize)
-    plot.title("Dust Distributions for\n" + r"$m_p = 1$ $M_J$, $\nu_{disk} = 10^{-7}$", fontsize = fontsize + 1)
-    plot.legend(loc = "lower right")
+        plot.plot(xs1[start1 : end1], ys1[start1 : end1], c = "b", linewidth = linewidths[i], dashes = dashes[i], label = label1, zorder = 5)
+        plot.plot(xs2[start2 : end2], ys2[start2 : end2], c = "g", linewidth = linewidths[i], dashes = dashes[i], label = label2, zorder = 1)
 
-def make_plot():
     # Axes
-    y_max = max_y(default_S)
-    plot.xlim(-180, 180)
-    #plot.ylim(0, 1000)
+    plot.xlim(0, 180)
+    plot.ylim(10**1, 10**4)
+    plot.yscale("log")
 
-    angles = np.linspace(-180, 180, 7)
+    if number == 2:
+        angles = np.linspace(0, 180, 7)
+    else:
+        angles = np.linspace(0, 150, 6)
     plot.xticks(angles)
 
+    if number != 0:
+        # Remove unless 1st frame
+        ax.set_yticklabels([])
+
+    # Annotate
+    margin = 130
+
+    top = 3.85
+    base1 = 2.9
+    split = 2.73
+    base2 = 1.7
+    bottom = 1.6
+
+    # Vertical
+    plot.plot([margin - 7, margin - 7], [10**(bottom), 10**(top)], c = "k")
+
+    plot.plot([margin - 7, 180], [10**(top), 10**(top)], c = "k") # Horizontal
+
+    plot.text(margin, 10**(base1 + 0.8), r"$T_{growth} = $" + "%d" % 10)
+    plot.text(margin, 10**(base1 + 0.6), r"$\chi = $ " + "%.1f" % aspect1)
+    plot.text(margin, 10**(base1 + 0.4), r"$r = $" + "%.1f" % (r1))
+    plot.text(margin, 10**(base1 + 0.2), r"$dr = $" + "%.02f" % (dr1))
+    plot.text(margin, 10**(base1), r"$\rho_{peak} = $" + "%.1f" % density1)
+
+    plot.plot([margin - 7, 180], [10**(split), 10**(split)], c = "k") # Horizontal
+
+    plot.text(margin, 10**(base2 + 0.8), r"$T_{growth} = $" + "%d" % growth[number])
+    plot.text(margin, 10**(base2 + 0.6), r"$\chi = $ " + "%.1f" % aspect2)
+    plot.text(margin, 10**(base2 + 0.4), r"$r = $" + "%.1f" % (r2))
+    plot.text(margin, 10**(base2 + 0.2), r"$dr = $" + "%.02f" % (dr2))
+    plot.text(margin, 10**(base2), r"$\rho_{peak} = $" + "%.1f" % density2)
+
+    plot.plot([margin - 7, 180], [10**(bottom), 10**(bottom)], c = "k") # Horizontal
+
+    plot.xlabel(r"$\phi - \phi_{center}$", fontsize = fontsize)
+    if number == 0:
+        plot.ylabel("Normalized Dust Density", fontsize = fontsize)
+    plot.title(r"$m_p = " + str(mass[number]) + r" $ $M_J$, $\nu_{disk} = 10^{" + str(viscosity[number]) + r"}$", fontsize = fontsize + 1, y = 1.01)
+
+    if number == 2:
+        plot.legend(loc = "upper right", bbox_to_anchor = [1.35, 1])
+
+def make_plot():
     # Save + Show
     plot.savefig("triple_dust_comparison.png", bbox_inches = "tight", dpi = my_dpi)
     plot.show()
@@ -142,7 +184,8 @@ def make_plot():
 aspect_ratios = []
 densities = []
 r_ratios = []
-str_r_ratios = []
+rs = []
+drs = []
 angles = []
 
 # Constant
@@ -176,9 +219,10 @@ print thousand_aspect_ratio
 # Collect 1 #
 
 aspect_ratios.append((ten_aspect_ratio, thousand_aspect_ratio))
-densties.append((ten_density, thousand_density))
+densities.append((ten_density, thousand_density))
 r_ratios.append((ten_radius_over_dr, thousand_radius_over_dr))
-str_r_ratios.append(("%.1f / %.02f" % (ten_radius, ten_dr), ("%.1f / %.02f" % (thousand_radius, thousand_dr))))
+rs.append((ten_radius, thousand_radius))
+drs.append((ten_dr, thousand_dr))
 angles.append((ten_angle, thousand_angle))
 
 ### Second Case: 5 MJ, v = 10^-7 ###
@@ -209,9 +253,10 @@ print thousand_aspect_ratio
 # Collect 2 #
 
 aspect_ratios.append((ten_aspect_ratio, four_thousand_aspect_ratio))
-densties.append((ten_density, four_thousand_density))
+densities.append((ten_density, four_thousand_density))
 r_ratios.append((ten_radius_over_dr, four_thousand_radius_over_dr))
-str_r_ratios.append(("%.1f / %.02f" % (ten_radius, ten_dr), ("%.1f / %.02f" % (four_thousand_radius, four_thousand_dr))))
+rs.append((ten_radius, four_thousand_radius))
+drs.append((ten_dr, four_thousand_dr))
 angles.append((ten_angle, four_thousand_angle))
 
 ### Third Case: 5 MJ, v = 10^-6 ###
@@ -243,17 +288,20 @@ print thousand_aspect_ratio
 # Collect 3 #
 
 aspect_ratios.append((ten_aspect_ratio, thousand_aspect_ratio))
-densties.append((ten_density, thousand_density))
+densities.append((ten_density, thousand_density))
 r_ratios.append((ten_radius_over_dr, thousand_radius_over_dr))
-str_r_ratios.append(("%.1f / %.02f" % (ten_radius, ten_dr), ("%.1f / %.02f" % (thousand_radius, thousand_dr))))
+rs.append((ten_radius, thousand_radius))
+drs.append((ten_dr, thousand_dr))
 angles.append((ten_angle, thousand_angle))
 
 ##### Set up plot #####
 
-fig = plot.figure()
+fig = plot.figure(figsize = (15, 5))
+# Remove hspace between plots
+plot.subplots_adjust(wspace = 0, hspace = 0)
 
-for i, (aspect_ratios_i, densities_i, r_ratios_i, str_r_ratios_i, angles_i) in enumerate(zip(aspect_ratios, densities, r_ratios, str_r_ratios, angles)):
-    add_to_plot(i, aspect_ratios_i, densities_i, r_ratios_i, str_r_ratios_i, angles_i)
+for i, (aspect_ratios_i, densities_i, r_ratios_i, rs_i, drs_i, angles_i) in enumerate(zip(aspect_ratios, densities, r_ratios, rs, drs, angles)):
+    add_to_plot(i, aspect_ratios_i, densities_i, r_ratios_i, rs_i, drs_i, angles_i)
 
 make_plot()
 plot.close(fig)
