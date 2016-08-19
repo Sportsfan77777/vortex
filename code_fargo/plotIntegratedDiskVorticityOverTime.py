2fa432a3edc9d44ce46411d035a4e1064e168408
Michael Hammer <mhammer44444@gmail.com>
2016-08-19 11:30:11 -0700

    subtract background with one

--- code_fargo/plotIntegratedDiskVorticityOverTime.py
+++ code_fargo/plotIntegratedDiskVorticityOverTime.py
@@ -77,6 +77,8 @@ def sum_vorticity(args):
     vtheta -= (vtheta_keplerian)
     vorticity = util.velocity_curl(vrad, vtheta, rad, theta, frame = 0)
 
+    vorticity = np.abs(vorticity) # should be all negative
+
     # Add up vorticity
     dr = rad[1] - rad[0] # assumes arithmetic grid
     d_phi = theta[1] - theta[0]
