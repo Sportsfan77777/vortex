2f0f786d319734d4685560f1f6895c3499fd1431
Michael Hammer <mhammer44444@gmail.com>
2016-08-30 12:13:47 -0700

    tweaks

--- code_fargo/compareGrowthRates.py
+++ code_fargo/compareGrowthRates.py
@@ -56,9 +56,6 @@ ax1 = plot.subplot(gs[0])
 ax2 = plot.subplot(gs[1])
 
 # Plot Curves
-sq = 75
-ax2.plot([-sq, sq], c = "k", linewidth = 2)
-
 for taper in tapers:
     frame_range = pickle.load(open("excess_mass_frames_taper%d.p" % taper, "rb"))
     mass_over_time = pickle.load(open("excess_mass_values_taper%d.p" % taper, "rb"))
@@ -73,7 +70,7 @@ for taper in tapers:
 
     # Center growth rates on peak
     if taper == 10:
-        max_index = 75
+        max_index = np.searchsorted(frame_range, 75)
     else:
         max_index = np.argmax(growth_rates)
     #max_index = np.argmax(growth_rates)
@@ -88,6 +85,10 @@ for taper in tapers:
     if frame_range[-1] > max_frame:
         max_frame = frame_range[-1]
 
+# Reference Line
+sq = 75
+ax2.plot([-sq, sq], [0, 0] c = "k", linewidth = 2)
+
 # Annotate
 title = r"$m_p = " + str(mass) + r" $ $M_J$, $\nu_{disk} = 10^{" + str(viscosity) + r"}$"
 ax1.set_xlabel("Number of Planet Orbits", fontsize = fontsize)
@@ -95,9 +96,9 @@ ax1.set_ylabel("Excess Mass", fontsize = fontsize)
 ax1.set_title(title, fontsize = fontsize + 2)
 
 ax2.set_xlabel(r"$t - t_{max-growth}$", fontsize = fontsize)
-ax2.set_ylabel("Growth Rate", fontsize = fontsize)
+ax2.set_ylabel("Growth Rate", fontsize = fontsize, labelpad = -20)
 
-ax1.legend(loc = "upper right", bbox_to_anchor = (2.1, 0.95))
+ax1.legend(loc = "upper right", bbox_to_anchor = (2.05, 0.95))
 
 # Axes
 ax1.set_xlim(0.0, 1500)
@@ -106,7 +107,8 @@ ax2.set_xlim(-sq, sq)
 ax2.set_xlim
 
 # Save + Close
-plot.savefig("growthRates_m%d_v%d.png" % (mass, abs(viscosity)))
+plot.savefig("growthRates_m%d_v%d.png" % (mass, abs(viscosity)), bbox_inches = "tight")
+plot.savefig("growthRates_m%d_v%d.pdf" % (mass, abs(viscosity)), bbox_inches = "tight", format = "pdf")
 plot.show()
 
 plot.close()
