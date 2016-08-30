752b31aab0daf17f786fa5d5ccb0cf42cfe6da39
Michael Hammer <mhammer44444@gmail.com>
2016-08-30 12:08:08 -0700

    tweaks

--- code_fargo/compareGrowthRates.py
+++ code_fargo/compareGrowthRates.py
@@ -50,14 +50,14 @@ alpha = 0.5
 max_frame = 0
 
 # Set up figures
-fig = plot.figure(figsize = (12, 8))
+fig = plot.figure(figsize = (12, 6))
 gs = gridspec.GridSpec(1, 2, width_ratios = [2, 1])
 ax1 = plot.subplot(gs[0])
 ax2 = plot.subplot(gs[1])
 
 # Plot Curves
 sq = 75
-ax2.plot([-sq, sq])
+ax2.plot([-sq, sq], c = "k", linewidth = 2)
 
 for taper in tapers:
     frame_range = pickle.load(open("excess_mass_frames_taper%d.p" % taper, "rb"))
@@ -72,11 +72,11 @@ for taper in tapers:
     growth_rates = np.diff(smoothed_log_mass_over_time) / np.diff(frame_range)
 
     # Center growth rates on peak
-    #if taper == 10:
-    #    max_index = 75
-    #else:
-    #    max_index = np.argmax(growth_rates)
-    max_index = np.argmax(growth_rates)
+    if taper == 10:
+        max_index = 75
+    else:
+        max_index = np.argmax(growth_rates)
+    #max_index = np.argmax(growth_rates)
 
     shifted_frames = np.array(frame_range) - frame_range[max_index]
 
@@ -97,11 +97,13 @@ ax1.set_title(title, fontsize = fontsize + 2)
 ax2.set_xlabel(r"$t - t_{max-growth}$", fontsize = fontsize)
 ax2.set_ylabel("Growth Rate", fontsize = fontsize)
 
-ax2.legend(loc = "upper right", bbox_to_anchor = (1.2, 0.95))
+ax1.legend(loc = "upper right", bbox_to_anchor = (2.1, 0.95))
 
 # Axes
 ax1.set_xlim(0.0, 1500)
+
 ax2.set_xlim(-sq, sq)
+ax2.set_xlim
 
 # Save + Close
 plot.savefig("growthRates_m%d_v%d.png" % (mass, abs(viscosity)))
