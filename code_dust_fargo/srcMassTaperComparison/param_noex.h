1392d8152c0a872c4af91057da69ab1c0cbb69ee
Michael Hammer <mhammer44444@gmail.com>
2017-03-09 19:41:25 -0700

    compare old version

---
+++ code_dust_fargo/srcMassTaperComparison/param_noex.h
@@ -0,0 +1,47 @@
+/** \file param_noex.h
+
+Created automatically during compilation
+from var.c. Do not edit. See Perl script
+"varparser.pl" for details.
+*/
+real	DT;
+real	SIGMA0;
+int	NINTERM;
+int	NTOT;
+char	OUTPUTDIR[512];
+char	OPENINNERBOUNDARY[512];
+char	ADVLABEL[512];
+char	TRANSPORT[512];
+char	PLANETCONFIG[512];
+real	MASSTAPER;
+char	GRIDSPACING[512];
+int	NRAD;
+int	NSEC;
+real	RMIN;
+real	RMAX;
+real	THICKNESSSMOOTHING;
+real	ROCHESMOOTHING;
+real	ASPECTRATIO;
+real	VISCOSITY;
+real	ALPHAVISCOSITY;
+real	SIGMASLOPE;
+real	RELEASERADIUS;
+real	RELEASEDATE;
+real	OMEGAFRAME;
+char	DISK[512];
+char	FRAME[512];
+char	OUTERSOURCEMASS[512];
+char	WRITEDENSITY[512];
+char	WRITEVELOCITY[512];
+char	INDIRECTTERM[512];
+char	EXCLUDEHILL[512];
+real	IMPOSEDDISKDRIFT;
+real	FLARINGINDEX;
+real	ECCENTRICITY;
+real	CAVITYRADIUS;
+real	CAVITYRATIO;
+real	CAVITYWIDTH;
+real	TRANSITIONRADIUS;
+real	TRANSITIONRATIO;
+real	TRANSITIONWIDTH;
+real	LAMBDADOUBLING;
