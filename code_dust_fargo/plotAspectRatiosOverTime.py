62629de31e4f1277e2caefb15986b9d272b170ad
Michael Hammer <mhammer44444@gmail.com>
2017-03-19 13:10:06 -0700

    added all features

--- code_dust_fargo/plotAspectRatiosOverTime.py
+++ code_dust_fargo/plotAspectRatiosOverTime.py
@@ -70,6 +70,9 @@ def get_extents(args):
     # Unwrap Args
     i, frame = args
 
+    # Print Update
+    print "%d" % (frame)
+
     # Get Dust Data
     density = (fromfile("gasddens%d.dat" % frame).reshape(num_rad, num_theta)) / surface_density_zero
 
@@ -115,9 +118,52 @@ def get_extents(args):
         else:
             az_widths10[i] = median_count
 
-        # Print Update
+    ####### Radial Extents #######
 
-        #print "%d: %.4f, %.4f, %.4f, %.4f" % (frame, m50, m75, m90, m99)
+    # Get Vortex Vicinity Indices
+    averagedDensity = np.average(density, axis = 1)
+    peak_rad, peak_density = find_peak(averagedDensity)
+
+    vortex_start = np.max([1.05, peak_rad - 5.0 * scale_height])
+    vortex_end = peak_rad + 5.0 * scale_height
+
+    vortex_start_i = np.searchsorted(rad, vortex_start)
+    vortex_end_i = np.searchsorted(rad, vortex_end)
+
+    # Extract Near Vortex
+    vortex_rad = rad[vortex_start_i : vortex_end_i]
+    vortex_density = density[vortex_start_i : vortex_end_i]
+
+    # Count number of cells above each threshold
+    for t_i, threshold in enumerate(thresholds):
+        # Store Copy
+        tmp_vortex_density = np.copy(vortex_density)
+
+        # Mark Above/Below Threshold
+        tmp_vortex_density[tmp_vortex_density <= threshold] = 0
+        tmp_vortex_density[tmp_vortex_density >  threshold] = 1
+
+        counts = np.sum(tmp_vortex_density, axis = 0)
+        high_count = np.percentile(counts, 90) # Take 90% (maybe max works fine?)
+
+        # Store High Count (converted to dr)
+        high_count *= ((rad[-1] - rad[0]) / num_rad)
+
+        if t_i == 0:
+            r_widths0[i] = high_count
+        elif t_i == 1:
+            r_widths1[i] = high_count
+        elif t_i == 2:
+            r_widths2[i] = high_count
+        elif t_i == 3:
+            r_widths5[i] = high_count
+        else:
+            r_widths10[i] = high_count
+
+    ####### Radii #######
+    rs[i] = peak_rad
+
+
 
 ## Use These Frames ##
 rate = 5 # 5 works better, but is very slow
@@ -187,8 +233,73 @@ def make_azimuthal_extent_plot():
 
     plot.close()
 
+def make_radial_extent_plot():
+    # Figure
+    fig = plot.figure(figsize = (700 / my_dpi, 600 / my_dpi), dpi = my_dpi)
+
+    # Curves
+    plot.plot(frame_range, r_widths0, linewidth = linewidth, label = "0.05")
+    plot.plot(frame_range, r_widths1, linewidth = linewidth, label = "0.1")
+    plot.plot(frame_range, r_widths2, linewidth = linewidth, label = "0.2")
+    plot.plot(frame_range, r_widths5, linewidth = linewidth, label = "0.5")
+    plot.plot(frame_range, r_widths10, linewidth = linewidth, label = "1.0")
+
+    # Annotate
+    this_title = readTitle()
+    plot.xlabel("Number of Planet Orbits", fontsize = fontsize)
+    plot.ylabel("Radial Extents", fontsize = fontsize)
+    plot.title(this_title, fontsize = fontsize)
+
+    plot.legend(loc = "upper left")
+
+    # Limits
+    plot.xlim(0, frame_range[-1])
+    #plot.ylim(0.0, 1.0)
+
+    # Save + Close
+    plot.savefig("radial_extents.png")
+    plot.show()
+
+    plot.close()
+
+def make_aspect_ratio_plot():
+    # Figure
+    fig = plot.figure(figsize = (700 / my_dpi, 600 / my_dpi), dpi = my_dpi)
+
+    # Collect Data
+    aspect_ratios0 = np.array(rs) * np.array(az_widths0) / np.array(r_widths0)
+    aspect_ratios1 = np.array(rs) * np.array(az_widths1) / np.array(r_widths1)
+    aspect_ratios2 = np.array(rs) * np.array(az_widths2) / np.array(r_widths2)
+    aspect_ratios5 = np.array(rs) * np.array(az_widths5) / np.array(r_widths5)
+    aspect_ratios10 = np.array(rs) * np.array(az_widths10) / np.array(r_widths10)
+
+    # Curves
+    plot.plot(frame_range, aspect_ratios0, linewidth = linewidth, label = "0.05")
+    plot.plot(frame_range, aspect_ratios1, linewidth = linewidth, label = "0.1")
+    plot.plot(frame_range, aspect_ratios2, linewidth = linewidth, label = "0.2")
+    plot.plot(frame_range, aspect_ratios5, linewidth = linewidth, label = "0.5")
+    plot.plot(frame_range, aspect_ratios10, linewidth = linewidth, label = "1.0")
+
+    # Annotate
+    this_title = readTitle()
+    plot.xlabel("Number of Planet Orbits", fontsize = fontsize)
+    plot.ylabel("Aspect Ratios", fontsize = fontsize)
+    plot.title(this_title, fontsize = fontsize)
+
+    plot.legend(loc = "upper left")
+
+    # Limits
+    plot.xlim(0, frame_range[-1])
+    #plot.ylim(0.0, 1.0)
+
+    # Save + Close
+    plot.savefig("aspect_ratios.png")
+    plot.show()
+
+    plot.close()
+
 
 make_azimuthal_extent_plot()
-#make_radial_extent_plot()
-#make_aspect_ratio_plot()
+make_radial_extent_plot()
+make_aspect_ratio_plot()
 
