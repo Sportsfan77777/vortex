14a514b743044a4b20f37586b987eb7dfb044861
Michael Hammer <mhammer44444@gmail.com>
2017-05-05 11:31:54 -0700

    added interpolation

--- code_dust_fargo/combineDensityToCGS.py
+++ code_dust_fargo/combineDensityToCGS.py
@@ -11,6 +11,7 @@ from multiprocessing import Pool
 
 import math
 import numpy as np
+from scipy import interpolate as sp_int 
 
 from pylab import fromfile
 
@@ -24,10 +25,14 @@ radius_unit = radius * (1.496 * 10**13) # (AU / cm)
 density_unit = mass_unit / radius_unit**2 # unit conversion factor
 
 # Grain Sizes
-sizes = ["hum", "hmm", "mm", "hcm", "cm"]
+sizes = [0.01, 0.03, 0.1, 0.3, 1.0]
+size_labels = ["hum", "hmm", "mm", "hcm", "cm"]
+
+log_interpolated_sizes = np.linspace(np.log10(sizes[0]), np.log10(sizes[-1]), 100)
+interpolated_sizes = np.power(10.0, log_interpolated_sizes) # to be used in interpolation
 
 # Save As
-save_name = "all"
+save_name = "interpolated"
 
 ######################################################################
 
@@ -35,14 +40,14 @@ save_name = "all"
 fn = sys.argv[1]
 
 # Replace size with %s
-for size in sizes:
+for size in size_labels:
     fn = fn.replace(size, r"%s")
 
 new_fn = "cgs_%s" % fn # output file
 
 # Compile list of filenames
 fns = []
-for size in sizes:
+for size in size_labels:
     fns.append(fn % size)
 
 ### Get FARGO Parameters ###
@@ -71,20 +76,25 @@ def find_argmax(density):
 middle = np.searchsorted(theta, np.pi)
 density_arrays = {}
 
-for (size_i, fn_i) in zip(sizes, fns):
+for (size_i, fn_i) in zip(size_labels, fns):
     tmp_density = fromfile(fn_i).reshape(num_rad, num_theta)
     location_i = find_argmax(tmp_density)
 
     shift_i = int(middle - location_i)
     density_arrays[size_i] = np.roll(tmp_density, shift_i, axis = 1)
 
-# Convert Data and Combine (Interleave) 
+# Convert Data
 combination_array = np.zeros((num_rad * num_theta, len(sizes)))
 for i, size_i in enumerate(sizes):
     combination_array[:, i] = (density_arrays[size_i] * density_unit).flatten('F') # F = column-major
 
-combination_array = combination_array.flatten() # interleave to 1-d
+# Interpolate to More Grain Sizes
+interpolation_function = sp_int.interp1d(sizes, combination_array, axis = -1)
+interpolated_combination_array = interpolation_function(interpolated_sizes)
+
+# Combine (Interleave)
+interleaved_array = interpolated_combination_array.flatten() # interleave to 1-d
 
 # Save New Data
-np.savetxt(new_fn % save_name, combination_array)
+np.savetxt(new_fn % save_name, interleaved_array)
 
