1213acf23b17501f1fa9356e71cff6841904ea19
Michael Hammer <mhammer44444@gmail.com>
2017-05-08 12:59:31 -0700

    updated parameter retrieval to account for different rad and theta

--- code_dust_fargo/plotIntensityMaps.py
+++ code_dust_fargo/plotIntensityMaps.py
@@ -37,11 +37,11 @@ pickled = util.pickle_parameters()
 param_fn = "params.p"
 fargo_par = pickle.load(open(param_fn, "rb"))
 
-num_rad = float(fargo_par["Nrad"])
-num_theta = float(fargo_par["Nsec"])
+rad = np.loadtxt("radial.dat")
+theta = np.loadtxt("azimuthal.dat")
 
-rad = np.linspace(float(fargo_par["Rmin"]), float(fargo_par["Rmax"]), num_rad + 1)
-theta = np.linspace(0, 2 * np.pi, num_theta + 1)
+num_rad = len(rad)
+num_theta = len(theta)
 
 surface_density_zero = float(fargo_par["Sigma0"])
 scale_height = float(fargo_par["AspectRatio"])
