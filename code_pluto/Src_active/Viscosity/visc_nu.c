390ecc58fc6a1e1e0155e409ae0cf77685c887cc
Michael Hammer <mhammer44444@gmail.com>
2017-06-19 21:22:11 +0800

    started viscosity function

--- code_pluto/Src_active/Viscosity/visc_nu.c
+++ code_pluto/Src_active/Viscosity/visc_nu.c
@@ -18,6 +18,39 @@ void Visc_nu(double *v, double x1, double x2, double x3,
  *  \return This function has no return value.
  * ************************************************************************** */
 {
-  *nu1 = 0.0;
-  *nu2 = 0.0;
+  #if GEOMETRY == CYLINDRICAL || POLAR
+      R = x1;
+  #elif GEOMETRY == SPHERICAL
+      R = x1 * sin(x2)
+      z = x1 * cos(x2)
+  #endif
+
+  *nu1 = kinematic_viscosity(R, z);
+  *nu2 = 0.0; // bulk viscosity
+}
+
+double kinematic_viscosity(double R, double z) {
+  /* viscosity profile */
+  double visc_amplitude, visc_ramp_center, visc_ramp_width;
+  double negative_z_angle, positive_z_angle;
+  double viscosity;
+
+  if (visc_alpha) {
+
+  }
+  else if (mass_accretion_rate) {
+
+  }
+  else {
+    // constant
+    
+  }
+
+  positive_z_angle = (z - visc_ramp_center) / visc_ramp_width;
+  negative_z_angle = (z + visc_ramp_center) / visc_ramp_width;
+  z_profile = 0.5 * (2.0 + tanh(positive_z_angle) - tanh(negative_z_angle))
+  
+  viscosity = visc_amplitude * z_profile;
+  
+  return viscosity;
 }
