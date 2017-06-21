a2491666edad657061affb11127bac3999b7cd28
Michael Hammer <mhammer44444@gmail.com>
2017-06-21 14:06:45 +0800

    added different parameter options

--- code_pluto/Src_active/Viscosity/visc_nu.c
+++ code_pluto/Src_active/Viscosity/visc_nu.c
@@ -31,24 +31,47 @@ void Visc_nu(double *v, double x1, double x2, double x3,
 
 double kinematic_viscosity(double R, double z) {
   /* viscosity profile */
-  double visc_amplitude, visc_ramp_center, visc_ramp_width;
-  double negative_z_angle, positive_z_angle;
+  double visc_lower_amplitude, visc_upper_amplitude;
+  double lower_alpha, upper_alpha, lower_accretion_rate, upper_accretion_rate;
+  double ramp, ramp_amplitude, ramp_center, ramp_width, negative_z_angle, positive_z_angle;
   double viscosity;
 
-  if (visc_alpha) {
+  if (g_inputParam[P_ViscosityType] == 1) {
+    // alpha viscosity
+    lower_alpha = g_inputParam[P_BaseViscosity];
+    upper_alpha = g_inputParam[P_MaxViscosity];
 
+    visc_lower_amplitude = lower_alpha * soundSpeed(r0) * scaleHeight(r0);
+    visc_upper_amplitude = upper_alpha * soundSpeed(r0) * scaleHeight(r0);
   }
-  else if (mass_accretion_rate) {
+  else if (g_inputParam[P_ViscosityType] == 2) {
+    // mass accretion rate
+    lower_accretion_rate = g_inputParam[P_BaseViscosity];
+    upper_accretion_rate = g_inputParam[P_MaxViscosity];
 
+    visc_lower_amplitude = lower_accretion_rate / density2D(r0);
+    visc_upper_amplitude = upper_accretion_rate / density2D(r0);
+  }
+  else if (g_inputParam[P_ViscosityType] == 3) {
+    // constant
+    visc_lower_amplitude = g_inputParam[P_BaseViscosity];
+    visc_upper_amplitude = g_inputParam[P_MaxViscosity];
   }
   else {
-    // constant
-    
+    // zero
+    return 0.0; // exit!
   }
 
-  positive_z_angle = (z - visc_ramp_center) / visc_ramp_width;
-  negative_z_angle = (z + visc_ramp_center) / visc_ramp_width;
-  z_profile = 0.5 * (2.0 + tanh(positive_z_angle) - tanh(negative_z_angle))
+  // The rest of the amplitude
+
+  // Z-profile
+  ramp_amplitude = visc_upper_amplitude - visc_lower_amplitude;
+
+  positive_z_angle = (z - ramp_center) / ramp_width;
+  negative_z_angle = (z + ramp_center) / ramp_width;
+  ramp = 0.5 * (2.0 + tanh(positive_z_angle) - tanh(negative_z_angle))
+
+  z_profile = 1.0 + (ramp_amplitude - 1.0) * ramp;
   
   viscosity = visc_amplitude * z_profile;
   
