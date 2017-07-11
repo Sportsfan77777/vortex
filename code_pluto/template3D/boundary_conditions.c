52e7082d6cca6f2572d6465dfeb70162bd696cf0
Michael Hammer <mhammer44444@gmail.com>
2017-07-11 19:11:11 +0800

    started a real boundary conditions file

--- code_pluto/template3D/boundary_conditions.c
+++ code_pluto/template3D/boundary_conditions.c
@@ -5,6 +5,77 @@ Different types of boundary conditions (This file does nothing.)
 /* ///////////////////////////////////////////////////////////////////// */
 
 
+#include "pluto.h"
+
+/// Fixed Boundary Condition ///
+
+void FixedBoundary(const Data *d, RBox *box, int side, Grid *grid) {
+  // Applies a fixed boundary at the specified boundary (side)
+  int   i, j, k, nv;
+  double *x1, *x2, *x3, R, th, z, OmegaK, v[256];
+  
+  x1 = grid[IDIR].x;
+  x2 = grid[JDIR].x;
+  x3 = grid[KDIR].x;
+
+  BOUNDARY_LOOP(side, k,j,i){
+    #if GEOMETRY == SPHERICAL
+       R = x1[i]*sin(x2[j]);
+       th = x2[j];
+       z = x1[i]*cos(x2[j]);
+    #endif
+    d->Vc[RHO][k][j][i]   = density3D(R, z);
+    d->Vc[VX1][k][j][i]   = radialVelocity_rComponent(R, th, z);
+    #if GEOMETRY == CYLINDRICAL
+        d->Vc[VX2][k][j][i]   = 0.0; // vz or vtheta
+    #elif GEOMETRY == SPHERICAL
+        d->Vc[VX2][k][j][i]   = radialVelocity_thetaComponent(R, th, z); // vtheta
+    #elif GEOMETRY == POLAR && DIMENSIONS == 3
+        d->Vc[VX3][k][j][i]   = 0.0; // vz
+    #endif
+    d->Vc[iVPHI][k][j][i] = azimuthalVelocity3D(R, z);
+    #if EOS == IDEAL
+        d->Vc[PRS][k][j][i]  = pressure(R, z);
+    #endif
+  }
+}
+
+/// Evanescent Boundary (internal) ///
+
+void EvanescentBoundary(const Data *d, RBox *box, int side, Grid *grid) {
+  /// Applies evanescent boundary conditions in the specified damping zone
+  int   i, j, k, nv;
+  double *x1, *x2, *x3, R, th, z, OmegaK, v[256];
+
+  // Add if side == 0? (or put that before the call in init.c?)
+
+  DOM_LOOP(k,j,i){
+    #if GEOMETRY == CYLINDRICAL 
+          R = x1[i];
+          z = x2[j];
+    #elif GEOMETRY == POLAR
+          R = x1[i];
+    #if DIMENSIONS == 3
+          z = x3[k];
+    #else
+          z = 0.0;
+    #endif
+    #elif GEOMETRY == SPHERICAL
+          R  = x1[i]*sin(x2[j]);
+          z  = x1[i]*cos(x2[j]);
+    #endif
+}
+
+
+
+
+
+
+
+
+
+
+
 /* ********************************************************************* */
 void UserDefBoundary (const Data *d, RBox *box, int side, Grid *grid) 
 /* 
@@ -105,6 +176,24 @@ void UserDefBoundary (const Data *d, RBox *box, int side, Grid *grid)
       d->Vc[VX3][k][j][i] += externalTorque(R, z) * g_dt;
     }
   }
+
+  /// Evanescent Boundary Conditions ///
+
+  DOM_LOOP(k,j,i){
+    #if GEOMETRY == CYLINDRICAL 
+          R = x1[i];
+          z = x2[j];
+    #elif GEOMETRY == POLAR
+          R = x1[i];
+    #if DIMENSIONS == 3
+          z = x3[k];
+    #else
+          z = 0.0;
+    #endif
+    #elif GEOMETRY == SPHERICAL
+          R  = x1[i]*sin(x2[j]);
+          z  = x1[i]*cos(x2[j]);
+    #endif
 
 /* original below ************************
 
