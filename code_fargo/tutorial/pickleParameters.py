511aaacbc8bbe062d09968de1c4c54bde26d5a85
Michael Hammer <mhammer44444@gmail.com>
2017-10-04 16:48:53 -0700

    improved structure of pickle parameters

--- code_fargo/tutorial/pickleParameters.py
+++ code_fargo/tutorial/pickleParameters.py
@@ -12,65 +12,96 @@ par = {}
 files = glob.glob("*.par")
 par_file = files[0]
 
-def store(line):
-    """ For a line 'a b', stores 'b' under 'a' in the dictionary """
-    line_sp = line.split()
+def parse_parameters(par_dictionary):
+    """ stores parameters in *.par file """
 
-    if len(line_sp) < 2:
-        # Empty
-        pass
-    elif line_sp[0][0] == "#":
-        # Comment
-        pass
-    else:
-        # Parse
-        name = line_sp[0]
-        entry = line_sp[1]
+    def store(line):
+        """ For a line 'a b', stores 'b' under 'a' in the dictionary """
+        line_sp = line.split()
 
-        # Convert to Proper Type (Integer, Float, or String?)
-        is_int = False
-        try:
-            entry = int(entry) # Integer?
-            is_int = True
-        except ValueError:
+        if len(line_sp) < 2:
+            # Empty
             pass
+        elif line_sp[0][0] == "#":
+            # Comment
+            pass
+        else:
+            # Parse
+            name = line_sp[0]
+            entry = line_sp[1]
 
-        if not is_int:
+            # Convert to Proper Type (Integer, Float, or String?)
+            is_int = False
             try:
-                entry = float(entry) # Float?
+                entry = int(entry) # Integer?
+                is_int = True
             except ValueError:
-                pass # String!
+                pass
 
-        # Store
-        par[name] = entry
+            if not is_int:
+                try:
+                    entry = float(entry) # Float?
+                except ValueError:
+                    pass # String!
 
-def store_planet_mass(line):
-    """ stores only the planet's mass """
-    line_sp = line.split()
+            # Store
+            par_dictionary[name] = entry
 
-    if len(line_sp) < 1:
-        # Empty
-        pass
-    else:
-        # Parse
-        planet_name = line_sp[0]
-        if planet_name == "Jupiter":
-            par["PlanetMass"] = float(line_sp[2])
+    # Find *.par file (assume there is only one *.par file)
+    files = glob.glob("*.par")
+    par_file = files[0]
 
-# Read file line by line
-with open(par_file, "r") as f:
-    for line in f:
-        store(line)
+    # Read file line by line
+    with open(par_file, "r") as f:
+        for line in f:
+            store(line)
 
-### NEW: Add planet mass too ###
-files = glob.glob("*.cfg") # usually Jup.cfg
-cfg_file = files[0]
+    files = glob.glob("*.cfg") # usually Jup.cfg
+    cfg_file = files[0]
 
 # Read file line by line
 with open(cfg_file, "r") as f:
     for line in f:
         store_planet_mass(line) 
 
-dict_name = "params.p"
-p.dump(par, open(dict_name, "wb"))
 
+def parse_planet_mass(par_dictionary):
+    """ parse planet's mass """
+
+    def store(line):
+        """ stores only the planet's mass """
+        line_sp = line.split()
+
+        if len(line_sp) < 1:
+            # Empty
+            pass
+        else:
+            # Parse
+            planet_name = line_sp[0]
+            if planet_name == "Jupiter":
+                par_dictionary["PlanetMass"] = float(line_sp[2])
+
+    # Find *.cfg file (assume there is only one *.cfg file)
+    files = glob.glob("*.cfg") # usually Jup.cfg
+    cfg_file = files[0]
+
+    # Read file line by line
+    with open(cfg_file, "r") as f:
+        for line in f:
+            store_planet_mass(line) 
+
+
+def pickle_parameter_dictionary():
+    """ dump all parameters into a pickle file """
+
+    parameter_dictionary = {}
+
+    parse_parameters(parameter_dictionary)
+    parse_planet_mass(parameter_dictionary)
+
+    dict_name = "params.p"
+    p.dump(parameter_dictionary, open(dict_name, "wb"))
+
+# Main
+if __name__ is "__main__":
+    pickle_parameter_dictionary()
