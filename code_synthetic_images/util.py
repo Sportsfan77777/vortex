9685788af8dc88f3ac19eca9e26c179ba27bb252
Michael Hammer <mhammer44444@gmail.com>
2017-10-12 16:21:00 -0700

    added new version of pickle parameters and added directory as a parameter

---
+++ code_synthetic_images/util.py
@@ -0,0 +1,116 @@
+"""
+stores all function calls
+
+Usage:
+not to be called
+"""
+
+import os
+import subprocess
+import glob
+import time
+import pickle
+
+import numpy as np
+
+import utilTorque
+import utilVorticity
+
+#### Miscellaneous ####
+
+def get_pickled_parameters(directory = "."):
+    """ Retrieve parameters from *.par file """
+    param_fn = "%s/params.p" % directory
+
+    if not os.path.exists(param_fn):
+        # create pickle if it doesn't exist
+        pickle_parameter_dictionary(directory = directory)
+
+    return pickle.load(open(param_fn, "rb"))
+
+def find_max_frame():
+    # glob the right set of files
+    fargo_fn = "fargo2D1D"
+    if os.path.exists(fargo_fn):
+        density_files = glob.glob("gasdens1D*.dat")
+        number_index = 9 # the number starts at index 9 after 'gasdens1D'
+    else:
+        density_files = glob.glob("gasdens*.dat")
+        number_index = 7 # the number starts at index 7 after 'gasdens'
+    # Find the highest numbered file
+    max_frame = 0
+    for d_f in density_files:
+        name = d_f.split(".")[0] # for "gasdens999.dat", just "gasdens999"
+        frame_number = int(name[number_index:]) # just 999
+        if frame_number > max_frame:
+            max_frame = frame_number
+    return max_frame
+
+def getWaveLocation(density, radii, theta, start_radius = 1.10, end_radius = 2.30):
+    # Find Limits
+    start_i = np.searchsorted(radii, start_radius)
+    end_i = np.searchsorted(radii, end_radius)
+
+    radii = radii[start_i : end_i] # truncate to selected range
+
+    # Find maxes
+    density_near_vortex = density[start_i : end_i]
+    wave_locations = np.zeros(len(density_near_vortex))
+
+    guess_theta = 2 * np.pi
+    delta_theta = 10.0 * (np.pi / 180.0)
+    for i, r_i in enumerate(radii):
+        # Guess Bounds
+        upper_guess = guess_theta # + delta_theta
+        lower_guess = guess_theta - delta_theta # search only below previous location
+
+        # Mask Values Away From Wave 
+        density_ring = density_near_vortex[i]
+        mask = np.ones(len(density_ring)) # 1 is masked
+
+        if lower_guess < 0.0:
+            lower_guess_i = np.searchsorted(theta, lower_guess + 2 * np.pi)
+            upper_guess_i = np.searchsorted(theta, upper_guess)
+
+            mask[lower_guess_i : ] = 0
+            mask[ : upper_guess_i] = 0
+
+        elif upper_guess > 2 * np.pi:
+            lower_guess_i = np.searchsorted(theta, lower_guess)
+            upper_guess_i = np.searchsorted(theta, upper_guess - 2 * np.pi)
+
+            mask[lower_guess_i : ] = 0
+            mask[ : upper_guess_i] = 0
+
+        else:
+            lower_guess_i = np.searchsorted(theta, lower_guess)
+            upper_guess_i = np.searchsorted(theta, upper_guess)
+            
+            mask[lower_guess_i : upper_guess_i] = 0
+
+        masked_density_ring = np.ma.array(density_ring, mask = mask)
+        wave_index = np.ma.argmax(masked_density_ring, fill_value = -1.0)
+
+        # Store Result + Update Next Guess
+        wave_locations[i] = theta[wave_index]
+        guess_theta = wave_locations[i]
+
+    return radii, wave_locations
+
+#### Torque Functions ####
+
+""" """
+torque = utilTorque.torque
+
+inner_torque = utilTorque.inner_torque
+
+outer_torque = utilTorque.outer_torque
+
+inner_torque_contributions = utilTorque.inner_torque_contributions
+
+outer_torque_contributions = utilTorque.outer_torque_contributions
+
+#### Vorticity ####
+
+velocity_curl = utilVorticity.velocity_curl
+
