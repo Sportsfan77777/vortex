09cabe40be807aade1307c96603f45a83f265b9b
Michael Hammer <mhammer44444@gmail.com>
2017-10-19 11:14:51 -0700

    added write parameters

--- code_synthetic_images/makeSyntheticImages.py
+++ code_synthetic_images/makeSyntheticImages.py
@@ -28,11 +28,24 @@ def new_argument_parser(description = "Generate input for synthetic images."):
     parser.add_argument('-c', dest = "num_cores", type = int, default = 1,
                          help = 'number of cores (default: 1)')
 
+    # Synthetic Image Parameters
+    parser.add_argument('-w', dest = "wavelength", type = float, default = 870,
+                         help = 'wavelength (in um) (default: None)')
+
+    parser.add_argument('-i', dest = "inclination", type = int, default = 0,
+                         help = 'inclination (in degrees) (default: 0)')
+    parser.add_argument('-T', dest = "temperature", type = int, default = 5770,
+                         help = 'stellar temperature (in K) (default: 5770)')
+    parser.add_argument('-R', dest = "radius", type = float, default = 6.96e10,
+                         help = 'stellar radius (in cm) (default: 6.96e10)')
+    parser.add_argument('-d', dest = "distance", type = int, default = 140,
+                         help = 'distance (in pc) (default: 140)')
+
     # Save Parameters
     parser.add_argument('--id', dest = "id_number", type = int, default = 0,
                          help = 'id number (up to 4 digits) for this set of plot parameters (default: None)')
-    parser.add_argument('--save', dest = "save_directory", default = ".",
-                         help = 'save directory (default: ".")')
+    parser.add_argument('--save', dest = "save_directory", default = None,
+                         help = 'save directory (default: "lambda%d % wavelength")')
 
     return parser
 
@@ -41,8 +54,25 @@ def new_argument_parser(description = "Generate input for synthetic images."):
 
 ### Task Functions ###
 
+def write_parameters():
+	""" Step 0: Write synthetic image parameters to file """
+
+	# Five Parameters
+	stellar_radius = args.radius # in cm
+	stellar_temperature = args.temperature # in K
+	wavelength = args.wavelength # in um
+	inclination = args.inclination
+	distance = args.distance # in pc
+
+	# Write to File
+	txt = "%.1e\n%d\n%.1f\n%1.f\n%d" % (stellar_radius, stellar_temperature, wavelength, inclination, distance)
+	fn = "parameters.dat"
+	with open(fn, "w") as f:
+		f.write(txt)
+
 def setup_tmp_directory():
 	""" Step 1: Make tmp directory """
+	cwd = os.getcwd()
 
 	# Make Directory
 	random_number = random.randint(0, 999999)
@@ -51,7 +81,10 @@ def setup_tmp_directory():
 
 	# Fill it with necessary files
 	necessary_files = []
+	os.chdir(tmp_dir)
 	os.symlink(target, name)
+
+	os.chdir(cwd)
 
 
 def full_procedure(frame):
