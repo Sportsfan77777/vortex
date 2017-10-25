e393c1c23273148b36c6317a7eb59675d1701d18
Michael Hammer <mhammer44444@gmail.com>
2017-10-24 18:04:51 -0700

    added updated make generic movies

---
+++ code_dust_fargo/makeGenericMovies.py
@@ -0,0 +1,118 @@
+"""
+usage: makeGenericMovies.py [-h] [--dir DIRECTORY] [--type MOVIE_CHOICE]
+                            [--save_name MOVIE_NAME]
+                            [--range FILE_RANGE FILE_RANGE FILE_RANGE]
+                            [--fps FPS]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --dir DIRECTORY       location of files and output (default: current)
+  --type MOVIE_CHOICE   select file prefix (choices: density (default),
+                        polarDensity)
+  --save_name MOVIE_NAME
+                        select movie suffix (default: movie)
+  --range START_FRAME END_FRAME RATE
+                        range(start, end, rate) (default: [0, 100, 1])
+  --fps FPS             movie frames per second (default: 5)
+"""
+
+import os, shutil, subprocess
+import time
+import argparse
+
+### Movie Options ###
+movie_dictionary = {}
+movie_dictionary["density"] = "densityMap_"
+movie_dictionary["polarDensity"] = "polarDensityMap_"
+
+###############################################################################
+
+### Input Parameters ###
+
+def new_argument_parser(description = "Manage movie input parameters."):
+    parser = argparse.ArgumentParser()
+
+    # Files
+    parser.add_argument('--dir', dest = "directory", default = ".",
+                         help = 'location of files and output (default: current)')
+    parser.add_argument('--type', dest = "movie_choice", default = "density", choices = movie_dictionary.keys(),
+                         help = 'select file prefix (choices: density (default), polarDensity)')
+    parser.add_argument('--id', dest = "name_id", type = int, default = None,
+                         help = 'id number for files -- useful for varying plot parameters (default: None)')
+    parser.add_argument('--save_name', dest = "movie_name", default = "movie",
+                         help = 'select movie suffix (default: movie)')
+
+    # File Range
+    parser.add_argument('--range', dest = "file_range", type = int, nargs = 3, default = [0, 100, 1],
+                         help = 'range(start, end, rate) (default: [0, 100, 1])')
+
+    # Movie Parameters
+    parser.add_argument('--fps', dest = "fps", type = int, default = 5,
+                         help = 'movie frames per second (default: 5)')
+
+    return parser
+
+## Parse Arguments ##
+args = new_argument_parser().parse_args()
+
+# Files
+directory = args.directory
+name = movie_dictionary[args.movie_choice]
+if args.name_id is not None:
+   name += "id%d_" % args.name_id
+movie_name = name + args.movie_name
+
+# File Range
+start_frame = args.file_range[0]
+end_frame = args.file_range[1]
+rate = args.file_range[2]
+
+# Movie Parameters
+fps = args.fps
+
+###############################################################################
+
+### Helper Functions ###
+
+def renumber(old_range, new_range, base_path, base_name):
+    """ re-number files to be consecutive with 'tmp_' prefix """
+    for (old_number, new_number) in zip(old_range, new_range):
+        old_file = "%s/%s%04d.png" % (base_path, base_name, old_number)
+        tmp_file = "%s/tmp_%s%04d.png" % (base_path, base_name, new_number)
+        shutil.copyfile(old_file, tmp_file)
+
+def delete_tmp_files(tmp_range, base_path, base_name):
+    """ delete all 'tmp' files created with renumber """
+    for number in tmp_range:
+        tmp_file = "%s/tmp_%s%04d.png" % (base_path, base_name, number)
+        os.remove(tmp_file)
+
+def make_movies():
+    """ terminal ffmpeg command """
+    ### Make Movie Command (Input: frames per second; path; output_name) ###
+    path = "%s/tmp_%s%s.png" % (directory, base_name, "%04d")
+    output = "%s/%s.mp4" % (directory, movie_name)
+
+    command = "ffmpeg -f image2 -r %d -i %s -vcodec mpeg4 -y %s" % (fps, path, output)
+    subprocess.Popen(command.split())
+
+###############################################################################
+
+### Make Movies ###
+
+# Re-number to consecutive frames
+base_path = directory
+base_name = name
+
+old_range = range(start_frame, end_frame + 1, rate)
+new_range = range(len(old_range))
+
+renumber(old_range, new_range, base_path, base_name)
+
+# Make Movies!
+make_movies()
+
+# Delete temporary files
+time.sleep(3) # 3 seconds --- (don't delete files before movie is created)
+delete_tmp_files(new_range, base_path, base_name)
+
