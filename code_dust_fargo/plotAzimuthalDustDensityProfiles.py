26688d7b99555c1f4fab8f0fbab74992a47b8e6d
Michael Hammer <mhammer44444@gmail.com>
2017-10-29 19:38:14 -0700

    fixed some things

--- code_dust_fargo/plotAzimuthalDustDensityProfiles.py
+++ code_dust_fargo/plotAzimuthalDustDensityProfiles.py
@@ -43,8 +43,6 @@ def new_argument_parser(description = "Plot gas density maps."):
     # Plot Parameters (variable)
     parser.add_argument('--hide', dest = "show", action = 'store_false', default = True,
                          help = 'for single plot, do not display plot (default: display plot)')
-    parser.add_argument('--id', dest = "id_number", type = int, default = 0,
-                         help = 'id number (up to 4 digits) for this set of synthetic image parameters (default: None)')
     parser.add_argument('-v', dest = "version", type = int, default = None,
                          help = 'version number (up to 4 digits) for this set of plot parameters (default: None)')
 
@@ -72,7 +70,7 @@ def new_argument_parser(description = "Plot gas density maps."):
 ### Parse Arguments ###
 args = new_argument_parser().parse_args()
 
-### Get ID%04d Parameters ###
+### Get Fargo Parameters ###
 fargo_par = util.get_pickled_parameters()
 
 num_rad = fargo_par["Nrad"]; num_theta = fargo_par["Nsec"]
@@ -84,6 +82,8 @@ surface_density_zero = fargo_par["Sigma0"] / 100
 disk_mass = 2 * np.pi * surface_density_zero * (r_max - r_min) / jupiter_mass # M_{disk} = (2 \pi) * \Sigma_0 * r_p * (r_out - r_in)
 
 scale_height = fargo_par["AspectRatio"]
+
+size = fargo_par["Psize"]
 
 ### Get Input Parameters ###
 
@@ -111,7 +111,6 @@ show = args.show
 rad = np.linspace(r_min, r_max, num_rad)
 theta = np.linspace(0, 2 * np.pi, num_theta)
 
-id_number = args.id_number
 version = args.version
 if args.r_lim is None:
     x_min = r_min; x_max = r_max
@@ -152,9 +151,50 @@ def find_min(averagedDensity, peak_rad):
         # No Gap Yet
         return peak_rad, 0
 
+def find_center(density, threshold_value = 0.05):
+    """ return shift needed to shift vortex center to 180 degrees """
+    ### Identify center using threshold ###
+    # Search outer disk only
+    outer_disk_start = np.searchsorted(rad, 1.1) # look for max density beyond r = 1.1
+    outer_disk_end = np.searchsorted(rad, 2.3) # look for max density before r = 2.3
+    density_segment = density[outer_disk_start : outer_disk_end]
+
+    # Get peak in azimuthal profile
+    avg_density = np.average(density_segment, axis = 1) # avg over theta
+    segment_arg_peak = np.argmax(avg_density)
+    arg_peak = np.searchsorted(rad, rad[outer_disk_start + segment_arg_peak])
+    peak_rad = rad[arg_peak]
+
+    # Zoom in on peak --- Average over half a scale height
+    half_width = 0.25 * scale_height
+    zoom_start = np.searchsorted(rad, peak_rad - half_width)
+    zoom_end = np.searchsorted(rad, peak_rad + half_width)
+
+    density_sliver = density[zoom_start : zoom_end]
+    avg_density_sliver = np.average(density_sliver, axis = 0) # avg over rad
+
+    # Move Minimum to Zero Degrees (vortex cannot cross zero)
+    arg_min = np.argmin(avg_density_sliver)
+    shift_min = int(0 - arg_min)
+    avg_density_sliver = np.roll(avg_density_sliver, shift_min)
+
+    # Spot two threshold crossovers
+    threshold = threshold_value * surface_density_zero
+
+    left_edge = np.searchsorted(avg_density_sliver, threshold)
+    right_edge = len(theta) - np.searchsorted(avg_density_sliver[::-1], threshold) - 1
+
+    center = (left_edge + right_edge) / 2.0
+
+    ### Calculate shift for true center to 180 degrees ###
+    middle = np.searchsorted(theta, np.pi)
+    shift_c = int(middle - (center - shift_min))
+
+    return shift_c
+
 ### Data ###
 
-def get_data(frame):
+def get_data(frame, density):
     """ Gather azimuthal radii and profiles """
     # Find Peak in Radial Profile (in Outer Disk)
     density = (fromfile("gasddens%d.dat" % frame).reshape(num_rad, num_theta))
@@ -163,6 +203,10 @@ def get_data(frame):
     averagedDensity = np.average(density, axis = 1)
     peak_rad, peak_density = find_peak(averagedDensity)
     min_rad, min_density = find_min(averagedDensity, peak_rad)
+
+    # Shift to Center
+    shift_c = find_center()
+    normalized_density = np.roll(normalized_density, shift_c)
 
     # Gather Azimuthal Profiles
     num_profiles = args.num_profiles
@@ -198,7 +242,7 @@ def make_plot(frame, azimuthal_radii, azimuthal_profiles, show = False):
 
     plot.xlabel(r"$\phi$", fontsize = fontsize + 2)
     plot.ylabel("Density", fontsize = fontsize)
-    plot.title("Composite Azimuthal Dust Density\n(t = %.1f)" % (orbit), fontsize = fontsize + 1)
+    plot.title("Azimuthal Dust Density [%f cm] \n(t = %.1f)" % (size, orbit), fontsize = fontsize + 1)
 
     plot.legend(loc = "upper right", bbox_to_anchor = (1.28, 1.0)) # outside of plot)
 
@@ -209,10 +253,10 @@ def make_plot(frame, azimuthal_radii, azimuthal_profiles, show = False):
     plot.xticks(angles)
 
     # Save, Show, and Close
-    if id_number is None:
+    if version is None:
         save_fn = "%s/azimuthalDensityProfiles_%04d.png" % (save_directory, frame)
     else:
-        save_fn = "%s/id%04d_azimuthalDensityProfiles_%04d.png" % (save_directory, id_number, frame)
+        save_fn = "%s/v%04d_azimuthalDensityProfiles_%04d.png" % (save_directory, version, frame)
     plot.savefig(save_fn, bbox_inches = 'tight', dpi = dpi)
 
     if show:
