41343c8902767a58db7c72eab717c9ff605dd2c8
Michael Hammer <mhammer44444@gmail.com>
2017-11-09 14:28:29 -0700

    added util to synthetic

--- code_synthetic_images/util.py
+++ code_synthetic_images/util.py
@@ -8,6 +8,10 @@ not to be called
 import os
 import pickle
 
+import numpy as np
+
+from pylab import fromfile
+
 from pickleParameters import pickle_parameter_dictionary
 
 #### Miscellaneous ####
@@ -21,6 +25,61 @@ def get_pickled_parameters(directory = "."):
         pickle_parameter_dictionary(directory = directory)
 
     return pickle.load(open(param_fn, "rb"))
+
+def read_data(frame, fn, fargo_par, directory = "."):
+    """ read data"""
+    ######## Get Parameters #########
+    num_rad = fargo_par["Nrad"]
+    num_theta = fargo_par["Nsec"]
+
+    ########### Method ##############
+    # Dictionary
+    basenames = {}
+    basenames['gas'] = "gasdens%d.dat"; basenames['dust'] = "gasddens%d.dat"
+
+    # Specific Data
+    basename = basenames[fn] % frame
+    density = (fromfile("%s/%s" % (directory, basename)).reshape(num_rad, num_theta))
+    return density
+
+def read_gas_data(frame, fargo_par, normalize = True, directory = "."):
+    """ read dust data """
+    ######## Get Parameters #########
+    surface_density_zero = fargo_par["Sigma0"]
+
+    ########### Method ##############
+    data = read_data(frame, 'gas', fargo_par, directory = directory) 
+    if normalize:
+        data /= surface_density_zero
+    return data
+
+def read_dust_data(frame, fargo_par, normalize = True, directory = "."):
+    """ read dust data """
+    ######## Get Parameters #########
+    surface_density_zero = fargo_par["Sigma0"]
+
+    ########### Method ##############
+    data = read_data(frame, 'dust', fargo_par, directory = directory) 
+    if normalize:
+        data /= (surface_density_zero / 100.0)
+    return data
+
+def get_size_label(size):
+    """ return label corresponding to size """
+    sizes = np.array([1.0, 0.3, 0.1, 0.03, 0.01, 0.0001])
+    size_labels = [r"$1$ $\rm{cm}$", r"$0.3$ $\rm{cm}$", r"$1$ $\rm{mm}$", r"$0.3$ $\rm{mm}$", r"$100$ $\rm{\mu m}$", r"$1$ $\rm{\mu m}$"]
+
+    arg_size = np.abs(sizes - size).argmin() # find closest size
+    return size_labels[arg_size]
+
+def get_threshold(size):
+    """ return label corresponding to size """
+    ######## abstract this into a new method that takes an array as input!!!!!! ############
+    sizes = np.array([1.0, 0.3, 0.1, 0.03, 0.01, 0.0001]) 
+    thresholds = [10.0, 5.0, 2.0, 1.7, 0.8, 0.7]
+
+    arg_size = np.abs(sizes - size).argmin() # find closest size
+    return thresholds[arg_size]
 
 def find_max_frame():
     """ Get last orbit """
