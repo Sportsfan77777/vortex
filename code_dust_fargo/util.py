df792df9e4a66ad2241921bb630160f3ac85993d
Michael Hammer <mhammer44444@gmail.com>
2017-11-08 19:21:26 -0700

    lowered thresholds for small particles

--- code_dust_fargo/util.py
+++ code_dust_fargo/util.py
@@ -54,7 +54,7 @@ def get_threshold(size):
     """ return label corresponding to size """
     ######## abstract this into a new method that takes an array as input!!!!!! ############
     sizes = np.array([1.0, 0.3, 0.1, 0.03, 0.01, 0.0001]) 
-    thresholds = [5, 5, 2, 2, 2, 2]
+    thresholds = [5, 5, 2, 1.5, 1.5, 1.3]
 
     arg_size = np.abs(sizes - size).argmin() # find closest size
     return thresholds[arg_size]
