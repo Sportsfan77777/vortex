5922ca9cf9f3d593487381f7e1098fb1ce485d70
Michael Hammer <mhammer44444@gmail.com>
2017-11-20 13:21:53 -0700

    first commit to square

---
+++ code_synthetic_images/square.py
@@ -0,0 +1,48 @@
+"""
+functions related to converting between cartesian and polar
+
+Usage:
+not to be called
+"""
+
+import numpy as np
+
+from scipy import signal
+from scipy.interpolate import interp1d as interpolate
+from scipy.ndimage import map_coordinates
+
+def polar_to_cartesian(data, rs, thetas, order = 3):
+    # Source: http://stackoverflow.com/questions/2164570/reprojecting-polar-to-cartesian-grid
+    # Set up xy-grid
+    max_r = rs[-1]
+    resolution = 2 * len(rad)
+
+    xs = np.linspace(-max_r, max_r, resolution)
+    ys = np.linspace(-max_r, max_r, resolution)
+
+    xs_grid, ys_grid = np.meshgrid(xs, ys)
+
+    # Interpolate rt-grid
+
+    interpolated_rs = interpolate(rs, np.arange(len(rs)), bounds_error = False)
+    interpolated_thetas = interpolate(thetas, np.arange(len(thetas)), bounds_error = False)
+
+    # Match up xy-grid with rt-grid
+
+    new_rs = np.sqrt(np.power(xs_grid, 2) + np.power(ys_grid, 2))
+    new_thetas = np.arctan2(ys_grid, xs_grid)
+
+    # Convert from [-pi, pi] to [0, 2 * pi]
+    new_thetas[new_thetas < 0] = new_thetas[new_thetas < 0] + 2 * np.pi
+
+    new_interpolated_rs = interpolated_rs(new_rs.ravel())
+    new_interpolated_thetas = interpolated_thetas(new_thetas.ravel())
+
+    # Fix Bounds (outside of polar image, but inside cartesian image)
+    new_interpolated_rs[new_rs.ravel() > max(rs)] = len(rs) - 1
+    new_interpolated_rs[new_rs.ravel() < min(rs)] = 0
+
+    cart_data = map_coordinates(data, np.array([new_interpolated_rs, new_interpolated_thetas]), order = order).reshape(new_rs.shape)
+
+    return xs_grid, ys_grid, cart_data
+
