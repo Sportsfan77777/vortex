b57abfb8a40299e2cd8e54fe4c5c8ca214c2acb9
Michael Hammer <mhammer44444@gmail.com>
2017-11-30 14:27:03 -0700

    added compare plot script

--- code_synthetic_images/compareConvolvedCartesianIntensityMaps.py
+++ code_synthetic_images/compareConvolvedCartesianIntensityMaps.py
@@ -222,7 +222,7 @@ def make_plot(frame, show = False):
     os.chdir(dir2)
 
     # Data
-    intensity_cart = util.read_data(frame, 'cartesian_intensity', fargo_par, id_number = id_number, beam = beam)
+    intensity_cart = util.read_data(frame, 'cartesian_intensity', fargo_par, id_number = id_number)
     _, _, xs_grid, ys_grid = sq.get_cartesian_grid(rad)
 
     # Normalize
