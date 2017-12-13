1e21ae1e7dec13855aa441e7cde42f7bdc477754
Michael Hammer <mhammer44444@gmail.com>
2017-12-12 19:16:49 -0700

    first commit to switch agg

---
+++ code_synthetic_images/switchAgg.py
@@ -0,0 +1,34 @@
+"""
+turn matplotlib.use('Agg') on
+
+Usage: 
+python switchAgg.py on fn
+python switchAgg.py off fn
+"""
+
+off = "#matplotlib.use('Agg')"
+on = "matplotlib.use('Agg')"
+
+def find_replace(old, new, fn):
+    # Read
+    with open(fn, 'r') as f:
+       txt = f.read()
+
+    # Replace
+    txt = txt.replace(old, new)
+
+    # Write
+    with open(fn, 'w') as f:
+       f.write(txt)
+
+######## Find and Replace! ########
+switch = sys.argv[1]
+fn = sys.argv[2] # Argument is fn
+
+if switch is "on":
+    # Turn on
+    find_replace(off, on, fn)
+elif switch is "off":
+    # Turn off
+    find_replace(on, off, fn)
+
