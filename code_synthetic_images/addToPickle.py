313aed216d98bac2522a62c1a7bedb297b5c19e2
Michael Hammer <mhammer44444@gmail.com>
2017-12-24 13:41:37 -0500

    implemented add to pickle

--- code_synthetic_images/addToPickle.py
+++ code_synthetic_images/addToPickle.py
@@ -1,9 +1,19 @@
 """
 adds entry (or changes entry) in a pickle file
 
+requires pickle stores a dictionary!
+
 Usage:
 python addToPickle.py storage.p Thing 55 # Adds Thing to storage.p with a value of 55
 """
 
 import pickle
+import sys
 
+fn = sys.argv[1]
+entry = sys.argv[2]
+value = sys.argv[3]
+
+dictionary = pickle.load(open(fn, 'rb'))
+dictionary[entry] = value
+pickle.dump(dictionary, open(fn, 'wb'))
