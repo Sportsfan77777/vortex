a42d3d127ef23f320cbc429e6a8638ce2c6ed63f
Michael Hammer <mhammer44444@gmail.com>
2017-12-25 00:44:05 -0500

    edited some things

--- code_paper2/plotDustDensityMultigrainMaps.py
+++ code_paper2/plotDustDensityMultigrainMaps.py
@@ -38,10 +38,14 @@ def new_argument_parser(description = "Plot convolved intensity maps."):
     parser = argparse.ArgumentParser()
 
     # Frame Selection
-    parser.add_argument('frames', type = int, nargs = 3,
-                         help = 'select three frames to plot')
+    parser.add_argument('frames', type = int, nargs = '+',
+                         help = 'select single frame or range(start, end, rate). error if nargs != 1 or 3')
     parser.add_argument('-c', dest = "num_cores", type = int, default = 1,
                          help = 'number of cores (default: 1)')
+
+    # Size Selection
+    parser.add_argument('sizes', nargs = 3, default = ["cm", "mm", "um"],
+                         help = 'select 3 sizes (default: [cm, mm, um])')
 
     # Files
     parser.add_argument('--dir', dest = "save_directory", default = "cartesianIntensityEvolution",
@@ -111,10 +115,13 @@ arc_beam = beam_size * planet_radius / distance
 ### Get Input Parameters ###
 
 # Frames
-frame_range = args.frames
+frame_range = util.get_frame_range(args.frames)
 
 # Number of Cores 
 num_cores = args.num_cores
+
+# Sizes
+sizes = args.sizes
 
 # Files
 save_directory = args.save_directory
@@ -151,37 +158,42 @@ rc['ytick.labelsize'] = labelsize
 
 ###############################################################################
 
-def add_to_plot(ax, frame, num_frames, frame_i):
+def add_to_plot(frame, ax, size, num_sizes, frame_i):
     print frame, num_frames, frame_i
 
     # Declare Subplot
     #ax = plot.subplot(1, num_frames, frame_i, sharex = prev_ax, sharey = prev_ax, aspect = "equal")
 
     # Data
-    intensity_cart = util.read_data(frame, 'cartesian_intensity', fargo_par, id_number = id_number)
-    _, _, xs_grid, ys_grid = sq.get_cartesian_grid(rad)
+    this_fargo_par = fargo_par.copy()
+    this_fargo_par["PSIZE"] = size
 
-    # Normalize
-    if normalize:
-        intensity_cart /= np.max(intensity_cart)
+    density = util.read_data(frame, 'dust', this_fargo_par, id_number = id_number, directory = "../%s-size" % size)
+    if center:
+        if taper < 10.1:
+            shift = az.get_azimuthal_peak(density, fargo_par)
+        else:
+            threshold = util.get_threshold(size)
+            shift = az.get_azimuthal_center(density, fargo_par, threshold = threshold * surface_density_zero)
+        density = np.roll(density, shift_c)
+    normalized_density = density / surface_density_zero
 
-    # Re-scale to planet_radius
-    xs_grid *= planet_radius; ys_grid *= planet_radius
+    # Convert gas density to cartesian
+    _, _, xs_grid, ys_grid, normalized_density = sq.polar_to_cartesian(normalized_density, rad, theta)
 
-    ### Get Shift ###
-    gas_fargo_par = util.get_pickled_parameters(directory = "../../../cm-size") ## shorten name?
+    ### Plot ###
+    if size == "um":
+        cmap = "viridis"
+    result = plot.pcolormesh(xs_grid, ys_grid, np.transpose(normalized_density), cmap = cmap)
+    result.set_clim(clim[0], clim[1])
 
-    ## Need to extract parameters, and add 'rad' and 'theta' ##
-    gas_rad = np.linspace(gas_fargo_par['Rmin'], gas_fargo_par['Rmax'], gas_fargo_par['Nrad'])
-    gas_theta = np.linspace(0, 2 * np.pi, gas_fargo_par['Nsec'])
-    gas_fargo_par['rad'] = gas_rad; gas_fargo_par['theta'] = gas_theta
-    gas_surface_density_zero = gas_fargo_par['Sigma0']
-    dust_surface_density_zero = gas_surface_density_zero / 100.0
+    # Get rid of interior
+    circle = plot.Circle((0, 0), min(rad), color = "black")
+    ax.add_artist(circle)
 
-    dust_density = util.read_data(frame, 'dust', gas_fargo_par, id_number = id_number, directory = "../../../cm-size")
-
-    # Find shift with center of dust density
-    shift = az.get_azimuthal_center(dust_density, gas_fargo_par, threshold = 10.0 * dust_surface_density_zero)
+    # Add planet orbit
+    planet_orbit = plot.Circle((0, 0), 1, color = "white", fill = False, alpha = 0.8, linestyle = "dashed", zorder = 50)
+    ax.add_artist(planet_orbit)
 
     # Locate Planet
     if shift < -len(gas_theta):
@@ -193,22 +205,6 @@ def add_to_plot(ax, frame, num_frames, frame_i):
     planet_x = np.cos(planet_theta)
     planet_y = np.sin(planet_theta)
 
-    ##### Plot #####
-    result = ax.pcolormesh(xs_grid, ys_grid, np.transpose(intensity_cart), cmap = cmap)
-    result.set_clim(clim[0], clim[1])
-
-    # Get rid of interior
-    circle = plot.Circle((0, 0), min(rad), color = "black")
-    ax.add_artist(circle)
-
-    # Add beam size
-    beam = plot.Circle((-2 * planet_radius, -2 * planet_radius), (beam_size / 2) * planet_radius, color = "white")
-    ax.add_artist(beam)
-
-    # Add planet orbit
-    planet_orbit = plot.Circle((0, 0), 1 * planet_radius, color = "white", fill = False, alpha = 0.8, linestyle = "dashed", zorder = 50)
-    ax.add_artist(planet_orbit)
-
     # Label star and planet
     time = fargo_par["Ninterm"] * fargo_par["DT"]
     orbit = (time / (2 * np.pi)) * frame
@@ -219,16 +215,21 @@ def add_to_plot(ax, frame, num_frames, frame_i):
 
     planet_size = current_mass / planet_mass
     plot.scatter(0, 0, c = "white", s = 300, marker = "*", zorder = 100) # star
-    plot.scatter(planet_x * planet_radius, planet_y * planet_radius, c = "white", s = int(70 * planet_size), marker = "D", zorder = 100) # planet
+    plot.scatter(planet_x, planet_y, c = "white", s = int(70 * planet_size), marker = "D", zorder = 100) # planet
 
     # Annotate Axes
-    ax.set_xlabel("x [AU]", fontsize = fontsize)
+    ax.set_xlabel(r"$x$ [$r_p$]", fontsize = fontsize)
     if frame_i == 1:
-        ax.set_ylabel("y [AU]", fontsize = fontsize)
-    ax.set_title(r"$t$ $=$ $%.1f$ [$m_p(t)$ $=$ $%.2f$ $M_J$]" % (orbit, current_mass), fontsize = fontsize + 1)
+        ax.set_ylabel(r"$y$ [$r_p$]", fontsize = fontsize)
+
+    title = "Dust (%s-size) Density" % size
+    if size == "um":
+        title = "Gas Density"
+    ax.set_title(title)
+    frame_title = r"$t$ $=$ $%.1f$ [$m_p(t)$ $=$ $%.2f$ $M_J$]" % (orbit, current_mass)
 
     # Axes
-    box_size = box * planet_radius
+    box_size = 2.5
     ax.set_xlim(-box_size, box_size)
     ax.set_ylim(-box_size, box_size)
     ax.set_aspect('equal')
@@ -244,37 +245,41 @@ def add_to_plot(ax, frame, num_frames, frame_i):
         cax = divider.append_axes("right", size = "8%", pad = 0.2)
         #cax = fig.add_axes([0.9, 0.1, 0.03, 0.8])
         cbar = fig.colorbar(result, cax = cax)
-        cbar.set_label(r"Normalized Intensity", fontsize = fontsize, rotation = 270, labelpad = 25)
+        cbar.set_label(r"$\Sigma$ / $\Sigma_\mathrm{0,}$ $_\mathrm{dust}$", fontsize = fontsize, rotation = 270, labelpad = 25)
 
         if frame_i != num_frames:
             fig.delaxes(cax) # to balance out frames that don't have colorbar with the one that does
 
-    return ax
+    return ax, frame_title
     
-def make_plot(frame_range):
+def make_plot(frame, sizes):
     # Set up figure
-    fig = plot.figure(figsize = (15, 6), dpi = dpi)
+    fig = plot.figure(figsize = (19, 6), dpi = dpi)
     gs = gridspec.GridSpec(1, len(frame_range))
 
-    for i, frame in enumerate(frame_range):
-        ax = fig.add_subplot(gs[i])
-        add_to_plot(ax, frame, len(frame_range), i + 1)
+    size_str = ""
+    for i, size_i in enumerate(sizes):
+        ax, frame_title = fig.add_subplot(gs[i])
+        add_to_plot(frame, ax, size_i, len(frame_range), i + 1)
+        size_str += "size_i_"
+    size_str = size_str[:-1] # Trim last '_'
 
     #### Finish Plot ####
 
     # Title
-    title = r'$M_p = %d$ $M_J$, $\nu = 10^{%d}$, $T_\mathrm{growth} = %d$ $\rm{orbits}$, $b = %.02f^{\prime\prime} \times %.02f^{\prime\prime}$' % (int(planet_mass / 0.001), round(np.log(viscosity) / np.log(10), 0), taper_time, arc_beam, arc_beam)
-    #title = "Intensity Evolution"
+    title = r'$M_p = %d$ $M_J$, $\nu = 10^{%d}$, $T_\mathrm{growth} = %d$ $\rm{orbits}$ | %s' % (int(planet_mass / 0.001), round(np.log(viscosity) / np.log(10), 0), taper_time, frame_title)
     fig.suptitle(title, y = 0.96, bbox = dict(facecolor = 'none', edgecolor = 'black', linewidth = 1.5, pad = 7.0), fontsize = fontsize + 4)
 
     # Save and Close
     plot.tight_layout()
 
     # Save, Show,  and Close
+    size_str
+
     if version is None:
-        save_fn = "%s/id%04d_intensityCartGrid_%04d-%04d-%04d.png" % (save_directory, id_number, frame_range[0], frame_range[1], frame_range[2])
+        save_fn = "%s/id%04d_intensityCartGrid_%04d_%s.png" % (save_directory, id_number, frame, size_str)
     else:
-        save_fn = "%s/v%04d_id%04d_intensityCartGrid_%04d-%04d-%04d.png" % (save_directory, version, id_number, frame_range[0], frame_range[1], frame_range[2])
+        save_fn = "%s/v%04d_id%04d_intensityCartGrid_%04d_%s.png" % (save_directory, version, id_number, frame, size_str)
     plot.savefig(save_fn, bbox_inches = 'tight', dpi = dpi)
 
     if show:
