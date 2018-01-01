be3475917b7054301ee9ff5f44de8152afba249e
Michael Hammer <mhammer44444@gmail.com>
2018-01-01 17:52:15 -0500

    fixing data

--- code_paper2/plotAzimuthalDustDensityTwoByTwoProfiles.py
+++ code_paper2/plotAzimuthalDustDensityTwoByTwoProfiles.py
@@ -154,6 +154,12 @@ fargo_par["theta"] = theta
 
 ###############################################################################
 
+##### PLOTTING #####
+
+colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
+          '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
+          '#bcbd22', '#17becf']
+
 def add_to_plot(frame, fig, ax, size_name, num_sizes, frame_i):
     # Convert size to number
     size = util.get_size(size_name)
