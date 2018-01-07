40e09701caf2540c846c53009a0ea5a0c0d86d60
Michael Hammer <mhammer44444@gmail.com>
2018-01-07 00:26:11 -0500

    fixed title

--- code_paper2/plotAzimuthalDustDensityEvolutionTwoByTwo.py
+++ code_paper2/plotAzimuthalDustDensityEvolutionTwoByTwo.py
@@ -287,7 +287,7 @@ def add_to_plot(frame, fig, ax, num_frames, frame_i):
     plot.text(-170, 0.85 * plot.ylim()[-1], rc_line, fontsize = fontsize, horizontalalignment = 'left')
 
     if frame_i % 2 == 0:    
-        center_x = 1.35 * plot.xlim()[-1]
+        center_x = 1.38 * plot.xlim()[-1]
         top_y = plot.ylim()[-1]
 
         if frame_i == 2:
