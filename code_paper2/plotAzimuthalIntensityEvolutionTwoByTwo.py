742b7b274b50bfe5bb9ab59a55c1902a4fe0673c
Michael Hammer <mhammer44444@gmail.com>
2018-01-07 14:52:19 -0500

    fixing typos

--- code_paper2/plotAzimuthalIntensityEvolutionTwoByTwo.py
+++ code_paper2/plotAzimuthalIntensityEvolutionTwoByTwo.py
@@ -217,7 +217,7 @@ def add_to_plot(frame, fig, ax, num_frames, frame_i):
     if max_y is None:
         plot.ylim(0, plot.ylim()[-1]) # No Input
     else:
-        plot.ylim(0, max_y[frame_i - 1]) # Input
+        plot.ylim(0, max_y) # Input
 
     if frame_i <= 2:
         # Remove unless bottom
