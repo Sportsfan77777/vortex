a25c83973567251b420931a4de8d69ad335b441b
Michael Hammer <mhammer44444@gmail.com>
2018-01-07 00:28:29 -0500

    fixed title

--- code_paper2/plotAzimuthalDustDensityEvolutionTwoByTwo.py
+++ code_paper2/plotAzimuthalDustDensityEvolutionTwoByTwo.py
@@ -190,7 +190,7 @@ colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
           '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
           '#bcbd22', '#17becf']
 
-labels = [r"$\mathrm{+0.50\ h}$", r"$\mathrm{+0.25\ h}$", r"$\mathrm{+0\ h}$", r"$\mathrm{-0.25\ h}$", r"$\mathrm{-0.50\ h}$"]
+labels = [r"$\mathrm{-0.50\ h}$", r"$\mathrm{-0.25\ h}$", r"$\mathrm{+0\ h}$", r"$\mathrm{+0.25\ h}$", r"$\mathrm{+0.50\ h}$"]
 
 def add_to_plot(frame, fig, ax, num_frames, frame_i):
     # Convert size to number
