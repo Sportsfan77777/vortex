e5b83e179fdd29621629cce3d610551eb24b3c1d
Michael Hammer <mhammer44444@gmail.com>
2018-01-11 11:32:41 -0700

    fixed t = 10 call

--- code_synthetic_images/generateSyntheticInput.py
+++ code_synthetic_images/generateSyntheticInput.py
@@ -173,7 +173,7 @@ def polish(density, sizes, cavity_cutoff = 0.92, scale = 1):
 def center_vortex(density):
     """ Step 2: center the vortex so that the peak is at 180 degrees """
     if massTaper < 10.1:
-        for i in enumerate(sizes):
+        for i, size in enumerate(sizes):
             shift_i = az.get_azimuthal_peak(density[:, :, i], fargo_par)
             density[:, :, i] = np.roll(density[:, :, i], shift_i, axis = 1)
         return density
