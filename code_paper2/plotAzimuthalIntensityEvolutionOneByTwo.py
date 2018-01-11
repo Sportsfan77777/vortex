a1e276789db0646297b199c4fc13526f93fca694
Michael Hammer <mhammer44444@gmail.com>
2018-01-11 16:35:51 -0700

    first commit to one by two

--- code_paper2/plotAzimuthalIntensityEvolutionOneByTwo.py
+++ code_paper2/plotAzimuthalIntensityEvolutionOneByTwo.py
@@ -238,7 +238,7 @@ def add_to_plot(frame, fig, ax, num_frames, frame_i):
 
     # Extra Annotation
     rc_line = r"$r_\mathrm{c} = %.02f$" % azimuthal_radii[(num_profiles - 1) / 2]
-    plot.text(-170, 0.85 * plot.ylim()[-1], rc_line, fontsize = fontsize, horizontalalignment = 'left')
+    plot.text(-170, 0.90 * plot.ylim()[-1], rc_line, fontsize = fontsize, horizontalalignment = 'left')
 
     if frame_i == 2:    
         center_x = 1.38 * plot.xlim()[-1]
