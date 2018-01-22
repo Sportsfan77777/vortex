9163220c0e026a50a034383433d230d57f6a97b3
Michael Hammer <mhammer44444@gmail.com>
2018-01-22 15:39:41 -0700

    filled in cartesian to polar method

--- code_synthetic_images/square.py
+++ code_synthetic_images/square.py
@@ -25,7 +25,7 @@ def get_cartesian_grid(rad):
 
 def get_polar_grid(xs, ys, min_r = 0):
     """ return polar grid associated with cartesian to polar"""
-    max_r = np.sqrt(np.max(xs)**2 + np.max(ys)**2)
+    max_r = np.sqrt(np.power(np.max(xs), 2) + np.power(np.max(ys), 2))
     num_r = len(xs); num_theta = len(ys)
 
     # Set up rt-grid
@@ -46,9 +46,9 @@ def polar_to_cartesian(data, rs, thetas, order = 3):
     # Set up xy-grid
     xs, ys, xs_grid, ys_grid = get_cartesian_grid(rs)
 
-    # Interpolate rt-grid
-    interpolated_rs = interpolate(rs, np.arange(len(rs)), bounds_error = False)
-    interpolated_thetas = interpolate(thetas, np.arange(len(thetas)), bounds_error = False)
+    # Interpolators for the rt-grid (Functions!!!!)
+    rs_interpolator = interpolate(rs, np.arange(len(rs)), bounds_error = False)
+    thetas_interpolator = interpolate(thetas, np.arange(len(thetas)), bounds_error = False)
 
     # Match up xy-grid with rt-grid
     new_rs = np.sqrt(np.power(xs_grid, 2) + np.power(ys_grid, 2))
@@ -57,8 +57,9 @@ def polar_to_cartesian(data, rs, thetas, order = 3):
     # Convert from [-pi, pi] to [0, 2 * pi]
     new_thetas[new_thetas < 0] = new_thetas[new_thetas < 0] + 2 * np.pi
 
-    new_interpolated_rs = interpolated_rs(new_rs.ravel())
-    new_interpolated_thetas = interpolated_thetas(new_thetas.ravel())
+    # Get new_rs and new_thetas with the interpolators
+    new_interpolated_rs = rs_interpolator(new_rs.ravel())
+    new_interpolated_thetas = thetas_interpolator(new_thetas.ravel())
 
     # Fix Bounds (outside of polar image, but inside cartesian image)
     new_interpolated_rs[new_rs.ravel() > max(rs)] = len(rs) - 1
@@ -69,5 +70,22 @@ def polar_to_cartesian(data, rs, thetas, order = 3):
     return xs, ys, xs_grid, ys_grid, cart_data
 
 def cartesian_to_polar(data, xs, ys, order = 3):
-    pass
+    # Set up rt-grid
+    rs, thetas, rs_grid, thetas_grid = get_polar_grid(xs, ys)
+
+    # Interpolators for the rt-grid (Functions!!!!)
+    xs_interpolator = interpolate(xs, np.arange(len(xs)), bounds_error = False)
+    ys_interpolator = interpolate(ys, np.arange(len(ys)), bounds_error = False)
+
+    # Match up rt-grid with xy-grid
+    new_xs = rs_grid * np.cos(thetas_grid)
+    new_ys = rs_grid * np.sin(thetas_grid)
+
+    # Get new_xs and new_ys with the interpolators
+    new_interpolated_xs = xs_interpolator(new_xs.ravel())
+    new_interpolated_ys = xs_interpolator(new_ys.ravel())
+
+    polar_data = map_coordinates(data, np.array([new_interpolated_xs, new_interpolated_ys]), order = order).reshape(new_xs.shape)
+
+    return rs, thetas, rs_grid, thetas_grid, polar_data
 
