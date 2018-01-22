dff6aa9d941b1fdb1233ab49e792d12c2e116cda
Michael Hammer <mhammer44444@gmail.com>
2018-01-21 18:40:34 -0700

    first commit to alma az profiles

---
+++ code_paper2/plotRealAlmaAzimuthalProfiles.py
@@ -0,0 +1,32 @@
+"""
+plots ALMA azimuthal intensity profiles
+
+Usage: python plotRealAlmaAzimuthlProfiles.py [name]
+"""
+
+import sys, os, subprocess
+import pickle, glob
+from multiprocessing import Pool
+import argparse
+
+import math
+import numpy as np
+
+import matplotlib
+#matplotlib.use('Agg')
+from matplotlib import rcParams as rc
+from matplotlib import pyplot as plot
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+from matplotlib import gridspec
+
+from pylab import rcParams
+from pylab import fromfile
+
+import util
+import square as sq
+import azimuthal as az
+
+from colormaps import cmaps
+for key in cmaps:
+    plot.register_cmap(name = key, cmap = cmaps[key])
