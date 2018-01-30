34cc281c7a155fe0d36b2f47963f0e330b8ce56d
Michael Hammer <mhammer44444@gmail.com>
2018-01-30 14:57:06 -0700

    fixed read

--- code_paper2/plotRealAlmaData.py
+++ code_paper2/plotRealAlmaData.py
@@ -163,9 +163,9 @@ def make_plot(show = False):
 
     # Save, Show, and Close
     if version is None:
-        save_fn = "%s/almaImage_%s.png" % (save_directory, name)
+        save_fn = "%s/almaImage_%s.png" % (save_directory, header['name'])
     else:
-        save_fn = "%s/v%04d_almaImage_%s.png" % (save_directory, version, name)
+        save_fn = "%s/v%04d_almaImage_%s.png" % (save_directory, version, header['name'])
     plot.savefig(save_fn, bbox_inches = 'tight', dpi = dpi)
 
     if show:
