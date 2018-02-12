0a341ab0662b0ba1f2de3b31a168a2b94c565487
Michael Hammer <mhammer44444@gmail.com>
2018-02-11 19:52:42 -0700

    added arc weight

--- code_paper2/plotIntensityTaperComparison.py
+++ code_paper2/plotIntensityTaperComparison.py
@@ -275,7 +275,7 @@ def add_to_plot(frame, fig, ax, num_sizes, frame_i):
 
     # Label
     taper_title = r"$T_\mathrm{growth} = %d$" % taper_time
-    plot.text(0.0 * box_size, 2, taper_title, fontsize = fontsize, color = 'white', horizontalalignment = 'center', bbox=dict(facecolor = 'black', edgecolor = 'white', pad = 10.0))
+    plot.text(0.0 * box_size, 2 * arc_weight, taper_title, fontsize = fontsize, color = 'white', horizontalalignment = 'center', bbox=dict(facecolor = 'black', edgecolor = 'white', pad = 10.0))
 
     # Title
     title = r"$t$ $=$ $%.1f$  [$m_p(t)$ $=$ $%.2f$ $M_J$]" % (orbit, current_mass)
