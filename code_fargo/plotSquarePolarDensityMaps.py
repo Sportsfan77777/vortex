3ed5928cb36a8cfc489eaae753f954560ebb9941
Michael Hammer <mhammer44444@gmail.com>
2018-03-05 17:27:33 -0700

    added text

--- code_fargo/plotSquarePolarDensityMaps.py
+++ code_fargo/plotSquarePolarDensityMaps.py
@@ -183,8 +183,8 @@ def make_plot(frame, show = False):
     plot.text(0.0, 3.14, title1, horizontalalignment = 'center', bbox = dict(facecolor = 'none', edgecolor = 'black', linewidth = 1.5, pad = 7.0), fontsize = fontsize + 2)
 
     # Text
-    text_mass = r"$M_\mathrm{p} = %d$ $M_\mathrm{Jup}" % (int(planet_mass))
-    text_visc = r"$\alpha_\mathrm{disk} = 3 \times 10^{%d}" % (int(np.log(viscosity) / np.log(10)) + 2)
+    text_mass = r"$M_\mathrm{p} = %d$ $M_\mathrm{Jup}$" % (int(planet_mass))
+    text_visc = r"$\alpha_\mathrm{disk} = 3 \times 10^{%d}$" % (int(np.log(viscosity) / np.log(10)) + 2)
     plot.text(-0.9 * box_size, 2, text_mass, fontsize = fontsize, color = 'black', horizontalalignment = 'left', bbox=dict(facecolor = 'white', edgecolor = 'black', pad = 10.0))
     plot.text(0.9 * box_size, 2, text_visc, fontsize = fontsize, color = 'black', horizontalalignment = 'right', bbox=dict(facecolor = 'white', edgecolor = 'black', pad = 10.0))
 
