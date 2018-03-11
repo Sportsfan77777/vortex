6785419492fa3ff52c33198f88ac552a1748c673
Michael Hammer <mhammer44444@gmail.com>
2018-03-10 19:46:59 -0700

    tweaks

--- code_dust_fargo/plotDustDensityMaps.py
+++ code_dust_fargo/plotDustDensityMaps.py
@@ -225,7 +225,7 @@ def make_plot(frame, show = False):
     if use_contours:
         levels = np.linspace(low_contour, high_contour, num_levels)
         colors = generate_colors(num_levels)
-        plot.contour(x, y, np.transpose(normalized_density), levels = levels, origin = 'upper', linewidths = 1, extent = extent, colors = colors)
+        plot.contour(x, y, np.transpose(normalized_density), levels = levels, origin = 'upper', linewidths = 1, colors = colors)
 
     # Axes
     plot.xlim(x_min, x_max)
