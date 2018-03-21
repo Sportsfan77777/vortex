a920ec3388221ec2d4543245b8f148af9d3f1674
Michael Hammer <mhammer44444@gmail.com>
2018-03-20 19:15:05 -0700

    first commit to plot intensity peak histogram

---
+++ code_paper2/plotIntensityPeakHistogram.py
@@ -0,0 +1,206 @@
+"""
+plot 2-D density maps
+
+python plotDensityMaps.py
+python plotDensityMaps.py frame_number
+python plotDensityMaps.py -1 <<<===== Plots a sample
+python plotDensityMaps.py -m
+"""
+
+import sys, os, subprocess
+import pickle, glob
+from multiprocessing import Pool
+import argparse
+
+import math
+import numpy as np
+
+import matplotlib
+#matplotlib.use('Agg')
+from matplotlib import rcParams as rc
+from matplotlib import pyplot as plot
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+from matplotlib import gridspec
+
+from pylab import rcParams
+from pylab import fromfile
+
+import util
+import square as sq
+import azimuthal as az
+
+from colormaps import cmaps
+for key in cmaps:
+    plot.register_cmap(name = key, cmap = cmaps[key])
+
+
+def new_argument_parser(description = "Plot azimuthal density profiles in two by two grid."):
+    parser = argparse.ArgumentParser()
+
+    # Frame Selection
+    parser.add_argument('frames', type = int, nargs = 2,
+                         help = 'select four frames to display the cm-size dust density maps')
+
+    # Files
+    parser.add_argument('--dir', dest = "save_directory", default = "azimuthalIntensityEvolution",
+                         help = 'save directory (default: azimuthalIntensityEvolution)')
+
+    # Plot Parameters (variable)
+    parser.add_argument('--hide', dest = "show", action = 'store_false', default = True,
+                         help = 'for single plot, do not display plot (default: display plot)')
+    parser.add_argument('--id', dest = "id_number", type = int, default = 0,
+                         help = 'id number (up to 4 digits) for this set of input parameters (default: 0)')
+    parser.add_argument('-v', dest = "version", type = int, default = None,
+                         help = 'version number (up to 4 digits) for this set of plot parameters (default: None)')
+
+    parser.add_argument('--max_y', dest = "max_y", nargs = '+', type = float, default = None,
+                         help = 'max_y for each frame, or same for all (default: None)')
+    parser.add_argument('--profiles', dest = "num_profiles", type = int, default = 5,
+                         help = 'number of profiles (default: 5)')
+    parser.add_argument('-s', dest = "num_scale_heights", type = float, default = 8.0,
+                         help = 'number of scale heights (default: 8.0)')
+
+    parser.add_argument('-n', dest = "normalize", action = 'store_false', default = True,
+                         help = 'normalize by max (default: normalize)')
+    parser.add_argument('-t', dest = "threshold", type = float, default = None,
+                         help = 'threshold for centering vortex with its center (default: varies with size)')
+    
+    # Plot Parameters (rarely need to change)
+    parser.add_argument('--fontsize', dest = "fontsize", type = int, default = 19,
+                         help = 'fontsize of plot annotations (default: 19)')
+    parser.add_argument('--labelsize', dest = "labelsize", type = int, default = 16,
+                         help = 'labelsize of plot annotations (default: 16)')
+    parser.add_argument('--linewidth', dest = "linewidth", type = int, default = 3,
+                         help = 'linewidths in plot (default: 3)')
+    parser.add_argument('--alpha', dest = "alpha", type = float, default = 0.65,
+                         help = 'line transparency in plot (default: 0.65)')
+    parser.add_argument('--dpi', dest = "dpi", type = int, default = 100,
+                         help = 'dpi of plot annotations (default: 100)')
+
+    return parser
+
+###############################################################################
+
+### Parse Arguments ###
+args = new_argument_parser().parse_args()
+
+### Get ID%04d Parameters ###
+fn = "id%04d_par.p" % (args.id_number)
+fargo_par = pickle.load(open(fn, "rb"))
+
+num_rad = fargo_par["Nrad"]; num_theta = fargo_par["Nsec"]
+r_min = fargo_par["Rmin"]; r_max = fargo_par["Rmax"]
+
+jupiter_mass = 1e-3
+planet_mass = fargo_par["PlanetMass"] / jupiter_mass
+taper_time = fargo_par["MassTaper"]
+
+surface_density_zero = fargo_par["Sigma0"]
+dust_surface_density_zero = surface_density_zero / 100
+disk_mass = 2 * np.pi * dust_surface_density_zero * (r_max - r_min) / jupiter_mass # M_{disk} = (2 \pi) * \Sigma_0 * r_p * (r_out - r_in)
+
+scale_height = fargo_par["AspectRatio"]
+viscosity = fargo_par["Viscosity"]
+
+planet_radius = fargo_par["Radius"]
+
+beam_size = fargo_par["Beam"]
+wavelength = fargo_par["Wavelength"]
+distance = fargo_par["Distance"]
+
+arc_beam = beam_size * planet_radius / distance
+
+### Get Input Parameters ###
+
+# Frames
+frame_range = util.get_frame_range(args.frames)
+
+# Files
+save_directory = args.save_directory
+if not os.path.isdir(save_directory):
+    os.mkdir(save_directory) # make save directory if it does not already exist
+
+# Plot Parameters (variable)
+normalize = args.normalize
+
+show = args.show
+max_y = args.max_y
+if normalize:
+    max_y = 1
+
+num_profiles = args.num_profiles
+num_scale_heights = args.num_scale_heights
+
+rad = np.linspace(r_min, r_max, num_rad)
+theta = np.linspace(0, 2 * np.pi, num_theta)
+
+id_number = args.id_number
+version = args.version
+
+threshold = args.threshold
+#if threshold is None:
+#    threshold = util.get_threshold(size)
+
+# Plot Parameters (constant)
+fontsize = args.fontsize
+labelsize = args.labelsize
+linewidth = args.linewidth
+alpha = args.alpha
+dpi = args.dpi
+
+rc['xtick.labelsize'] = labelsize
+rc['ytick.labelsize'] = labelsize
+
+### Add new parameters to dictionary ###
+fargo_par["rad"] = rad
+fargo_par["theta"] = theta
+
+###############################################################################
+
+### Data ###
+
+def get_peak(frame):
+    """return peak in azimuthal profile of vortex"""
+    intensity_polar = util.read_data(frame, 'polar_intensity', fargo_par, id_number = id_number)
+    peak = az.get_azimuthal_peak(intensity_polar, fargo_par)
+    return peak
+
+###############################################################################
+
+##### PLOTTING #####
+
+def make_plot(show = False):
+    fig = plot.figure(figsize = (7, 5), dpi = dpi)
+    ax = fig.add_subplot(111)
+
+    # Get Data
+    peaks = np.array([get_peak(frame) for frame in frame_range])
+
+    # Plot
+    bins = np.linspace(-60, 60, 13)
+    data = peaks
+    plot.hist(data, bins = bins)
+
+    # Save, Show, and Close
+    frame_str = ""
+    for i, frame_i in enumerate(frame_range):
+        frame_str += "%04d-" % frame_i
+    frame_str = frame_str[:-1] # Trim last '_'
+
+    if version is None:
+        save_fn = "%s/intensityPeakHistogram_%s.%s" % (save_directory, frame_str, png)
+    else:
+        save_fn = "%s/v%04d_intensityPeakHistogram_%s.%s" % (save_directory, version, frame_str, png)
+    plot.savefig(save_fn, bbox_inches = 'tight', dpi = dpi)
+
+    if show:
+        plot.show()
+
+    plot.close(fig) # Close Figure (to avoid too many figures)
+
+##### Make Plots! #####
+
+make_plot(show = show)
+
+
