6d62d1306ed06d4062f6653bd59b0656d7915b81
Michael Hammer <mhammer44444@gmail.com>
2018-06-28 18:25:29 -0400

    first commit to dust trapping criterion map

---
+++ code_dust_fargo/plotDustTrappingCriterionMap.py
@@ -0,0 +1,356 @@
+"""
+plot 2-D dust density maps
+
+usage: plotDustDensityMaps.py [-h] [-c NUM_CORES] [--dir SAVE_DIRECTORY]
+                              [--hide] [-v VERSION] [--range R_LIM R_LIM]
+                              [--shift] [--cmap CMAP] [--cmax CMAX]
+                              [--fontsize FONTSIZE] [--dpi DPI]
+                              frames [frames ...]
+
+positional arguments:
+  frames                select single frame or range(start, end, rate). error
+                        if nargs != 1 or 3
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c NUM_CORES          number of cores (default: 1)
+  --dir SAVE_DIRECTORY  save directory (default: dustDensityMaps)
+  --hide                for single plot, do not display plot (default: display
+                        plot)
+  -v VERSION            version number (up to 4 digits) for this set of plot
+                        parameters (default: None)
+  --range R_LIM R_LIM   radial range in plot (default: [r_min, r_max])
+  --shift               center frame on vortex peak or middle (default: do not
+                        center)
+  --cmap CMAP           color map (default: viridis)
+  --cmax CMAX           maximum density in colorbar (default: 10 for hcm+, 2.5
+                        otherwise)
+  --fontsize FONTSIZE   fontsize of plot annotations (default: 16)
+  --dpi DPI             dpi of plot annotations (default: 100)
+"""
+
+import sys, os, subprocess
+import pickle, glob
+from multiprocessing import Pool
+import argparse
+
+import math
+import numpy as np
+
+import matplotlib
+from matplotlib import rcParams as rc
+from matplotlib import pyplot as plot
+
+from pylab import rcParams
+from pylab import fromfile
+
+import util
+import utilVorticity
+import azimuthal as az
+from readTitle import readTitle
+
+from colormaps import cmaps
+for key in cmaps:
+    plot.register_cmap(name = key, cmap = cmaps[key])
+
+###############################################################################
+
+### Input Parameters ###
+
+def new_argument_parser(description = "Plot dust density maps."):
+    parser = argparse.ArgumentParser()
+
+    # Frame Selection
+    parser.add_argument('frames', type = int, nargs = '+',
+                         help = 'select single frame or range(start, end, rate). error if nargs != 1 or 3')
+    parser.add_argument('-c', dest = "num_cores", type = int, default = 1,
+                         help = 'number of cores (default: 1)')
+
+    # Files
+    parser.add_argument('--dir', dest = "save_directory", default = "vorticityMaps",
+                         help = 'save directory (default: vorticityMaps)')
+
+    # Quantity to plot
+    parser.add_argument('--rossby', dest = "rossby", action = 'store_true', default = False,
+                         help = 'plot rossby number instead of vorticity (default: plot vorticity)')
+    parser.add_argument('--residual', dest = "residual", action = 'store_false', default = True,
+                         help = 'use v_theta or v_theta - v_kep (default: use residual)')
+
+    # Plot Parameters (variable)
+    parser.add_argument('--hide', dest = "show", action = 'store_false', default = True,
+                         help = 'for single plot, do not display plot (default: display plot)')
+    parser.add_argument('-v', dest = "version", type = int, default = None,
+                         help = 'version number (up to 4 digits) for this set of plot parameters (default: None)')
+
+    parser.add_argument('--range', dest = "r_lim", type = float, nargs = 2, default = None,
+                         help = 'radial range in plot (default: [r_min, r_max])')
+    parser.add_argument('--shift', dest = "center", action = 'store_true', default = False,
+                         help = 'center frame on vortex peak or middle (default: do not center)')
+
+    # Plot Parameters (contours)
+    parser.add_argument('--contour', dest = "use_contours", action = 'store_true', default = False,
+                         help = 'use contours or not (default: do not use contours)')
+    parser.add_argument('--low', dest = "low_contour", type = float, default = 1.1,
+                         help = 'lowest contour (default: 1.1)')
+    parser.add_argument('--high', dest = "high_contour", type = float, default = 3.5,
+                         help = 'highest contour (default: 3.5)')
+    parser.add_argument('--num_levels', dest = "num_levels", type = int, default = None,
+                         help = 'number of contours (choose this or separation) (default: None)')
+    parser.add_argument('--separation', dest = "separation", type = float, default = 0.1,
+                         help = 'separation between contours (choose this or num_levels) (default: 0.1)')
+    
+    # Plot Parameters (rarely need to change)
+    parser.add_argument('--cmap', dest = "cmap", default = "viridis",
+                         help = 'color map (default: viridis)')
+    parser.add_argument('--crange', dest = "c_lim", type = float, nargs = 2, default = None,
+                         help = 'range in colorbar (default: [0, 2])')
+
+    parser.add_argument('--fontsize', dest = "fontsize", type = int, default = 16,
+                         help = 'fontsize of plot annotations (default: 16)')
+    parser.add_argument('--dpi', dest = "dpi", type = int, default = 100,
+                         help = 'dpi of plot annotations (default: 100)')
+
+    return parser
+
+###############################################################################
+
+### Parse Arguments ###
+args = new_argument_parser().parse_args()
+
+### Get Fargo Parameters ###
+fargo_par = util.get_pickled_parameters()
+
+num_rad = fargo_par["Nrad"]; num_theta = fargo_par["Nsec"]
+r_min = fargo_par["Rmin"]; r_max = fargo_par["Rmax"]
+
+jupiter_mass = 1e-3
+planet_mass = fargo_par["PlanetMass"] / jupiter_mass
+taper_time = fargo_par["MassTaper"]
+
+surface_density_zero = fargo_par["Sigma0"]
+disk_mass = 2 * np.pi * surface_density_zero * (r_max - r_min) / jupiter_mass # M_{disk} = (2 \pi) * \Sigma_0 * r_p * (r_out - r_in)
+
+scale_height = fargo_par["AspectRatio"]
+viscosity = fargo_par["Viscosity"]
+
+size = fargo_par["PSIZE"]
+
+### Get Input Parameters ###
+
+# Frames
+if len(args.frames) == 1:
+    frame_range = args.frames
+elif len(args.frames) == 3:
+    start = args.frames[0]; end = args.frames[1]; rate = args.frames[2]
+    frame_range = range(start, end + 1, rate)
+else:
+    print "Error: Must supply 1 or 3 frame arguments\nWith one argument, plots single frame\nWith three arguments, plots range(start, end + 1, rate)"
+    exit()
+
+# Number of Cores 
+num_cores = args.num_cores
+
+# Files
+save_directory = args.save_directory
+if not os.path.isdir(save_directory):
+    os.mkdir(save_directory) # make save directory if it does not already exist
+
+# Quantity to Plot
+rossby = args.rossby
+residual = args.residual
+
+# Plot Parameters (variable)
+show = args.show
+
+rad = np.linspace(r_min, r_max, num_rad)
+theta = np.linspace(0, 2 * np.pi, num_theta)
+
+version = args.version
+if args.r_lim is None:
+    x_min = r_min; x_max = r_max
+else:
+    x_min = args.r_lim[0]; x_max = args.r_lim[1]
+center = args.center
+
+# Plot Parameters (contours)
+use_contours = args.use_contours
+low_contour = args.low_contour
+high_contour = args.high_contour
+num_levels = args.num_levels
+if num_levels is None:
+    separation = args.separation
+    num_levels = int(round((high_contour - low_contour) / separation + 1, 0))
+
+# Plot Parameters (constant)
+cmap = args.cmap
+if args.c_lim is None:
+    clim = [0, 2]
+else:
+    clim = [args.c_lim[0], args.c_lim[1]]
+
+fontsize = args.fontsize
+dpi = args.dpi
+
+### Add new parameters to dictionary ###
+fargo_par["rad"] = rad
+fargo_par["theta"] = theta
+
+###############################################################################
+
+# Initial Density
+density = (fromfile("gasdens0.dat").reshape(num_rad, num_theta))
+
+### Helper Functions ###
+def velocity_perturbation(vrad, vtheta):
+    """ | du | """
+    # Get delta v_theta
+    vk = np.power(rad, -0.5)
+    dvtheta = vtheta - vk[:, None]
+
+    # Get magnitude
+    magnitude = vrad * vrad + dvtheta * dvtheta
+    return np.sqrt(magnitude)
+
+def pressure_gradient_term(density):
+    """ | grad dP | (r / rho) / (2 v_Kep) """
+    # Keplerian velocity
+    vk = np.power(rad, -0.5)
+
+    # Pressure: dP = c_s^2 * (rho - rho_0)
+    height = scale_height * rad
+    omegaK = np.power(rad, -1.5)
+    sound_speed_squared = np.power(height, 2) * np.power(omegaK, 2)
+    pressure = sound_speed_squared[:, None] * (density - density_zero)
+
+    # Pressure Gradient
+
+    # Magnitude of pressure perturbation gradient
+    pressure_gradient_magnitude = np.sqrt(dprad * dprad + dptheta * dptheta)
+
+    return pressure_gradient_magnitude * (rad[:, None] / density) / (2 * vk[:, None])
+
+
+###############################################################################
+
+def generate_colors(n):
+    c = ['b', 'g', 'springgreen']
+    colors = []
+    for i in range(n):
+        colors.append(c[i % len(c)])
+    return colors
+
+##### PLOTTING #####
+
+def make_plot(frame, show = False):
+    # Set up figure
+    fig = plot.figure(figsize = (7, 6), dpi = dpi)
+    ax = fig.add_subplot(111)
+
+    # Data
+    vrad = (fromfile("gasvrad%d.dat" % frame).reshape(num_rad, num_theta)) # add a read_vrad to util.py!
+    vtheta = (fromfile("gasvtheta%d.dat" % frame).reshape(num_rad, num_theta)) # add a read_vrad to util.py!
+
+    vorticity = utilVorticity.velocity_curl(vrad, vtheta, rad, theta, rossby = rossby, residual = residual)
+
+    # Shift
+    density = (fromfile("gasdens%d.dat" % frame).reshape(num_rad, num_theta)) / surface_density_zero
+    dust_density = (fromfile("gasddens%d.dat" % frame).reshape(num_rad, num_theta))
+    if center:
+        if taper_time < 10.1:
+            shift_c = az.get_azimuthal_peak(dust_density, fargo_par)
+        else:
+            threshold = util.get_threshold(size)
+            shift_c = az.get_azimuthal_center(dust_density, fargo_par, threshold = threshold * surface_density_zero / 100.0)
+        vorticity = np.roll(vorticity, shift_c)
+        density = np.roll(density, shift_c)
+
+    ### Plot ###
+    x = rad
+    y = theta * (180.0 / np.pi)
+    result = ax.pcolormesh(x, y, np.transpose(vorticity), cmap = cmap)
+
+    cbar = fig.colorbar(result)
+    result.set_clim(clim[0], clim[1])
+
+    if use_contours:
+        levels = np.linspace(low_contour, high_contour, num_levels)
+        colors = generate_colors(num_levels)
+        plot.contour(x, y, np.transpose(density), levels = levels, origin = 'upper', linewidths = 1, colors = colors, alpha = 0.8)
+
+    # Axes
+    plot.xlim(x_min, x_max)
+    plot.ylim(0, 360)
+
+    angles = np.linspace(0, 360, 7)
+    plot.yticks(angles)
+
+    # Annotate Axes
+    time = fargo_par["Ninterm"] * fargo_par["DT"]
+    orbit = (time / (2 * np.pi)) * frame
+
+    if orbit >= taper_time:
+        current_mass = planet_mass
+    else:
+        current_mass = np.power(np.sin((np.pi / 2) * (1.0 * orbit / taper_time)), 2) * planet_mass
+
+    #title = readTitle()
+
+    unit = "r_\mathrm{p}"
+    plot.xlabel(r"Radius [$%s$]" % unit, fontsize = fontsize)
+    plot.ylabel(r"$\phi$", fontsize = fontsize)
+
+    #if title is None:
+    #    plot.title("Dust Density Map\n(t = %.1f)" % (orbit), fontsize = fontsize + 1)
+    #else:
+    #    plot.title("Dust Density Map\n%s\n(t = %.1f)" % (title, orbit), fontsize = fontsize + 1)
+
+    x_range = x_max - x_min; x_mid = x_min + x_range / 2.0
+    y_text = 1.14
+
+    title1 = r"$T_\mathrm{growth} = %d$ $\mathrm{orbits}$" % (taper_time)
+    title2 = r"$t = %d$ $\mathrm{orbits}}$  [$m_\mathrm{p}(t)\ =\ %.2f$ $M_\mathrm{Jup}$]" % (orbit, current_mass)
+    plot.title("%s" % (title2), y = 1.015, fontsize = fontsize + 1)
+    plot.text(x_mid, y_text * plot.ylim()[-1], title1, horizontalalignment = 'center', bbox = dict(facecolor = 'none', edgecolor = 'black', linewidth = 1.5, pad = 7.0), fontsize = fontsize + 2)
+
+    # Text
+    text_mass = r"$M_\mathrm{p} = %d$ $M_\mathrm{Jup}$" % (int(planet_mass))
+    text_visc = r"$\alpha_\mathrm{disk} = 3 \times 10^{%d}$" % (int(np.log(viscosity) / np.log(10)) + 2)
+    #plot.text(-0.9 * box_size, 2, text_mass, fontsize = fontsize, color = 'black', horizontalalignment = 'left', bbox=dict(facecolor = 'white', edgecolor = 'black', pad = 10.0))
+    #plot.text(0.9 * box_size, 2, text_visc, fontsize = fontsize, color = 'black', horizontalalignment = 'right', bbox=dict(facecolor = 'white', edgecolor = 'black', pad = 10.0))
+    plot.text(-0.84 * x_range / 2.0 + x_mid, y_text * plot.ylim()[-1], text_mass, fontsize = fontsize, color = 'black', horizontalalignment = 'right')
+    plot.text(0.84 * x_range / 2.0 + x_mid, y_text * plot.ylim()[-1], text_visc, fontsize = fontsize, color = 'black', horizontalalignment = 'left')
+
+    # Label colorbar
+    if rossby:
+       cbar_name = r"$\mathrm{Rossby}$ $\mathrm{number}$"
+    else:
+       cbar_name = r"$\mathrm{Vorticity}$"
+    cbar.set_label(cbar_name, fontsize = fontsize, rotation = 270, labelpad = 25)
+
+    # Save, Show, and Close
+    if version is None:
+        save_fn = "%s/vorticityMap_%04d.png" % (save_directory, frame)
+    else:
+        save_fn = "%s/v%04d_vorticityMap_%04d.png" % (save_directory, version, frame)
+    plot.savefig(save_fn, bbox_inches = 'tight', dpi = dpi)
+
+    if show:
+        plot.show()
+
+    plot.close(fig) # Close Figure (to avoid too many figures)
+
+##### Make Plots! #####
+
+# Iterate through frames
+
+if len(frame_range) == 1:
+    make_plot(frame_range[0], show = show)
+else:
+    if num_cores > 1:
+        p = Pool(num_cores) # default number of processes is multiprocessing.cpu_count()
+        p.map(make_plot, frame_range)
+        p.terminate()
+    else:
+        for frame in frame_range:
+            make_plot(frame)
+
