6111fbe6e61f85702e184b2bfd9d217c15c2e3f8
Michael Hammer <mhammer44444@gmail.com>
2018-08-08 16:17:06 -0700

    first commit to compare intensity peak histograms

--- code_paper2/compareIntensityPeakHistograms.py
+++ code_paper2/compareIntensityPeakHistograms.py
@@ -169,7 +169,7 @@ def make_plot(show = False):
 
     # Get Data
     for i, (beam_i, threshold_i) in enumerate(zip(beams, thresholds)):
-        data[i] = pickle.load("id%04d_b%02d_t%02d_intensityPeaks.p" % (id_number, beam_i, int(round(100.0 * threshold_i, 0))))
+        data[i] = pickle.load(open("id%04d_b%02d_t%02d_intensityPeaks.p" % (id_number, beam_i, int(round(100.0 * threshold_i, 0))), "rb"))
 
     # Plot
     bins = np.linspace(-90, 90, 19) # Make this parameters
