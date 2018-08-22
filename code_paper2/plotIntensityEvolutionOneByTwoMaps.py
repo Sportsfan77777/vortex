e1111512f5992b2c304b49a4754126bd9391d71f
Michael Hammer <mhammer44444@gmail.com>
2018-08-21 23:00:12 -0700

    changed text

--- code_paper2/plotIntensityEvolutionOneByTwoMaps.py
+++ code_paper2/plotIntensityEvolutionOneByTwoMaps.py
@@ -125,8 +125,8 @@ if not os.path.isdir(save_directory):
     os.mkdir(save_directory) # make save directory if it does not already exist
 
 # Old Format
-old_num_rad = old_res[0]
-old_num_theta = old_res[1]
+old_num_rad = args.old_res[0]
+old_num_theta = args.old_res[1]
 
 # Plot Parameters (variable)
 show = args.show
@@ -252,23 +252,23 @@ def add_to_plot(frame, fig, ax, num_sizes, frame_i):
     ax.spines['bottom'].set_color('w'); ax.spines['top'].set_color('w'); ax.spines['left'].set_color('w'); ax.spines['right'].set_color('w')
     ax.tick_params(colors = 'white', labelcolor = 'black', width = 1, length = 5)
 
+    # Label
+    taper_title = r"$T_\mathrm{growth} = %d$" % taper_time
+    plot.text(0.0 * box_size, 2 * arc_weight, taper_title, fontsize = fontsize, color = 'white', horizontalalignment = 'center', bbox=dict(facecolor = 'black', edgecolor = 'white', pad = 10.0))
+
     # Title
     title = r"$t$ $=$ $%.1f$  [$m_p(t)$ $=$ $%.2f$ $M_J$]" % (orbit, current_mass)
     plot.title("%s" % (title), y = 1.015, fontsize = fontsize + 1)
 
     # Title
-    left_x = -0.8 * box_size; line_y = 1.28 * box_size; linebreak = 0.2 * box_size
-    right_x = 1.3 * box_size
+    left_x = -1.2 * box_size; line_y = 1.24 * box_size; linebreak = 0.2 * box_size
+    right_x = 1.2 * box_size
     if frame_i == 1:
         line1 = r'$M_p = %d$ $M_J$' % planet_mass
-        line2 = r'$\nu = 10^{%d}$' % round(np.log(viscosity) / np.log(10), 0)
-        plot.text(left_x, line_y + linebreak, line1, horizontalalignment = 'left', fontsize = fontsize + 2)
-        plot.text(left_x, line_y, line2, horizontalalignment = 'left', fontsize = fontsize + 2)
+        plot.text(left_x, line_y + 0.2 * linebreak, line1, horizontalalignment = 'left', fontsize = fontsize + 2)
     elif frame_i == 2:
-        line3 = r'$T_\mathrm{growth} = %d$ $\rm{orbits}$' % taper_time
-        line4 = r"$%.03f^{\prime\prime} \times \ \ %.03f^{\prime\prime}$" % (arc_beam, arc_beam)
-        plot.text(right_x, line_y + linebreak, line3, horizontalalignment = 'right', fontsize = fontsize + 2)
-        plot.text(right_x, line_y, line4, horizontalalignment = 'right', fontsize = fontsize + 2)
+        line2 = r'$\nu = 10^{%d}$' % round(np.log(viscosity) / np.log(10), 0)
+        plot.text(right_x, line_y + 0.2 * linebreak, line2, horizontalalignment = 'right', fontsize = fontsize + 2)
 
     # Add Colorbar (Source: http://stackoverflow.com/questions/23270445/adding-a-colorbar-to-two-subplots-with-equal-aspect-ratios)
     if colorbar:
@@ -298,8 +298,9 @@ def make_plot(show = False):
     frame_str = frame_str[:-1] # Trim last '_'
 
     #### Finish Plot ####
-    title = r"$N_\mathrm{r} \times \ N_\mathrm{\phi} = %d \times \ %d$" % (old_num_rad, old_num_theta)
-    fig.suptitle(title, y = 0.99, verticalalignment = "bottom", bbox = dict(facecolor = 'none', edgecolor = 'black', linewidth = 1.5, pad = 7.0), fontsize = fontsize + 4)
+    title = r"$%.02f^{\prime\prime} (%d \mathrm{\ AU})$" % (arc_beam, args.beam_size)
+    #title = r"$N_\mathrm{r} \times \ N_\mathrm{\phi} = %d \times \ %d$" % (old_num_rad, old_num_theta)
+    fig.suptitle(title, y = 0.93, verticalalignment = "bottom", bbox = dict(facecolor = 'none', edgecolor = 'black', linewidth = 1.5, pad = 7.0), fontsize = fontsize + 4)
 
     # Save and Close
     plot.tight_layout()
