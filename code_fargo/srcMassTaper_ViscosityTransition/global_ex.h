075610589b0a4caa2db4e61028415baa5a3f56f4
Sportsfan77777 <mhammer44444@gmail.com>
2019-01-13 21:25:38 -0700

    new directory

---
+++ code_fargo/srcMassTaper_ViscosityTransition/global_ex.h
@@ -0,0 +1,47 @@
+/** \file global_ex.h
+
+This file is created      
+automatically during      
+compilation from global.h. Do not edit. 
+See perl script           
+"varparser.pl" for details
+
+\file global.h
+
+Declares all global variables.
+Used to construct automatically
+the file global_ex.h. The file
+global.h cannot contain any comment,
+as it would not be parsed correctly
+by varparser.pl
+*/                          
+
+extern int CPU_Rank;
+extern int CPU_Number;
+extern boolean CPU_Master;
+extern int IMIN;
+extern int IMAX;
+extern int Zero_or_active;
+extern int Max_or_active;
+extern int One_or_active;
+extern int MaxMO_or_active;		/* MO: Minus One */
+extern int GLOBALNRAD;
+extern real Rinf[MAX1D], Rsup[MAX1D], Rmed[MAX1D], Surf[MAX1D];
+extern real InvRmed[MAX1D], InvSurf[MAX1D], InvDiffRmed[MAX1D];
+extern real InvDiffRsup[MAX1D], InvRinf[MAX1D], Radii[MAX1D], GlobalRmed[MAX1D];
+extern real SigmaMed[MAX1D], SigmaInf[MAX1D], MassTaper;
+extern real OmegaFrame, PhysicalTime, PhysicalTimeInitial;
+extern int TimeStep;
+extern real SOUNDSPEED[MAX1D];
+extern real GLOBAL_SOUNDSPEED[MAX1D];
+extern boolean Merge, AdvecteLabel, FakeSequential, MonitorIntegral, debug, OnlyInit;
+extern boolean	GotoNextOutput, StoreSigma, ViscosityAlpha, RocheSmoothing;
+extern boolean CentrifugalBalance, ExcludeHill, SloppyCFL, Stockholm;
+extern MPI_Status fargostat;
+extern PolarGrid *CellAbscissa, *CellOrdinate;
+extern PolarGrid *RhoStar, *RhoInt, *Potential;
+extern boolean LogGrid;
+extern boolean OverridesOutputdir;
+extern char NewOutputdir[1024];
+extern boolean FakeAccretion;
+extern boolean SinSquaredTaper, ParabolaTaper;
---
+++ code_fargo/srcMassTaper_ViscosityTransition/param.h
@@ -0,0 +1,49 @@
+/** \file param.h
+
+Created automatically during compilation
+from var.c. Do not edit. See Perl script
+"varparser.pl" for details.
+*/
+extern	real	DT;
+extern	real	SIGMA0;
+extern	int	NINTERM;
+extern	int	NTOT;
+extern	char	OUTPUTDIR[512];
+extern	char	OPENINNERBOUNDARY[512];
+extern	char	ADVLABEL[512];
+extern	char	TRANSPORT[512];
+extern	char	PLANETCONFIG[512];
+extern	real	MASSTAPER;
+extern	char	GRIDSPACING[512];
+extern	int	NRAD;
+extern	int	NSEC;
+extern	real	RMIN;
+extern	real	RMAX;
+extern	real	THICKNESSSMOOTHING;
+extern	real	ROCHESMOOTHING;
+extern	real	ASPECTRATIO;
+extern	real	VISCOSITY;
+extern	real	ALPHAVISCOSITY;
+extern	real	SIGMASLOPE;
+extern	real	RELEASERADIUS;
+extern	real	RELEASEDATE;
+extern	real	OMEGAFRAME;
+extern	char	DISK[512];
+extern	char	FRAME[512];
+extern	char	OUTERSOURCEMASS[512];
+extern	char	WRITEDENSITY[512];
+extern	char	WRITEVELOCITY[512];
+extern	char	INDIRECTTERM[512];
+extern	char	EXCLUDEHILL[512];
+extern	real	IMPOSEDDISKDRIFT;
+extern	real	FLARINGINDEX;
+extern	real	ECCENTRICITY;
+extern	real	CAVITYRADIUS;
+extern	real	CAVITYRATIO;
+extern	real	CAVITYWIDTH;
+extern	real	TRANSITIONRADIUS;
+extern	real	TRANSITIONRATIO;
+extern	real	TRANSITIONWIDTH;
+extern	real	LAMBDADOUBLING;
+extern	real	INNERFOLDNUMBER;
+extern	real	OUTERFOLDNUMBER;
