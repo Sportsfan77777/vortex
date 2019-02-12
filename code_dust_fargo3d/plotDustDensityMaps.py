fixed normalization and contours

code_dust_fargo3d/plotDustDensityMaps.py
@@ -248,13 +248,17 @@ def make_plot(frame, show = False):
 
     # Data
     field = "dens"
+
+    gas_density = Fields("./", 'gas', frame).get_field(field).reshape(num_rad, num_theta)
+    normalized_gas_density = density / surface_density_zero
+
     density = Fields("./", 'dust%d' % dust_number, frame).get_field(field).reshape(num_rad, num_theta)
-    normalized_density = density / surface_density_zero
+    normalized_density = density / dust_surface_density_zero
 
     ### Plot ###
     x = rad
     y = theta * (180.0 / np.pi)
-    result = ax.pcolormesh(x, y, np.transpose(normalized_density), cmap = cmap)
+    result = ax.pcolormesh(x, y, np.transpose(normalized_gas_density), cmap = cmap)
 
     fig.colorbar(result)
     result.set_clim(clim[0], clim[1])