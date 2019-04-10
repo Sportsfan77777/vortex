be6d956918d34a2536d1fc8868717316fb8e606a
Sportsfan77777 <mhammer44444@gmail.com>
2019-04-09 19:12:39 -0700

    added zero comparison option

--- code_dust_fargo3d/plotAveragedDensity.py
+++ code_dust_fargo3d/plotAveragedDensity.py
@@ -203,7 +203,7 @@ def make_plot(frame, show = False):
 
     unit = "r_\mathrm{p}"
     plot.xlabel(r"Radius [$%s$]" % unit, fontsize = fontsize)
-    plot.ylabel(r"$\phi$", fontsize = fontsize)
+    plot.ylabel(r"$\Sigma / \Sigma_0$", fontsize = fontsize)
 
     #if title is None:
     #    plot.title("Dust Density Map\n(t = %.1f)" % (orbit), fontsize = fontsize + 1)
