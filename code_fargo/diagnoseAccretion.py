0c21fe2ddbb70ebcd293f2839c89a248ada3cb11
Sportsfan77777 <mhammer44444@gmail.com>
2019-07-18 01:46:49 -0700

    add new diagnose accretion

--- code_fargo/diagnoseAccretion.py
+++ code_fargo/diagnoseAccretion.py
@@ -1,24 +1,43 @@
 """
-Compares tapering "accretion rate" to usual measurements (involving viscosity or radial velocity)
+plot 2-D dust density maps
 
-Usage:
-python diagnoseAccretion.py
+usage: plotDustDensityMaps.py [-h] [-c NUM_CORES] [--dir SAVE_DIRECTORY]
+                              [--hide] [-v VERSION] [--range R_LIM R_LIM]
+                              [--shift] [--cmap CMAP] [--cmax CMAX]
+                              [--fontsize FONTSIZE] [--dpi DPI]
+                              frames [frames ...]
+
+positional arguments:
+  frames                select single frame or range(start, end, rate). error
+                        if nargs != 1 or 3
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c NUM_CORES          number of cores (default: 1)
+  --dir SAVE_DIRECTORY  save directory (default: dustDensityMaps)
+  --hide                for single plot, do not display plot (default: display
+                        plot)
+  -v VERSION            version number (up to 4 digits) for this set of plot
+                        parameters (default: None)
+  --range R_LIM R_LIM   radial range in plot (default: [r_min, r_max])
+  --shift               center frame on vortex peak or middle (default: do not
+                        center)
+  --cmap CMAP           color map (default: viridis)
+  --cmax CMAX           maximum density in colorbar (default: 10 for hcm+, 2.5
+                        otherwise)
+  --fontsize FONTSIZE   fontsize of plot annotations (default: 16)
+  --dpi DPI             dpi of plot annotations (default: 100)
 """
 
-import sys
-import os
-import subprocess
-import pickle
-import glob
+import sys, os, subprocess
+import pickle, glob
 from multiprocessing import Pool
+import argparse
 
 import math
 import numpy as np
 
-from scipy.ndimage import filters as ff
-
 import matplotlib
-#matplotlib.use('Agg')
 from matplotlib import rcParams as rc
 from matplotlib import pyplot as plot
 
@@ -26,97 +45,208 @@ from pylab import rcParams
 from pylab import fromfile
 
 import util
+import azimuthal as az
 from readTitle import readTitle
 
-## Set file names ##
-fargo_fn = "fargo2D1D"
-if os.path.exists(fargo_fn):
-    # fargo2D1D
-    planet_fn = "planet1.dat"
-else:
-    # fargo
-    planet_fn = "planet0.dat"
+from colormaps import cmaps
+for key in cmaps:
+    plot.register_cmap(name = key, cmap = cmaps[key])
 
-### Get FARGO Parameters ###
-# Create param file if it doesn't already exist
-param_fn = "params.p"
-if not os.path.exists(param_fn):
-    command = "python pickleParameters.py"
-    split_command = command.split()
-    subprocess.Popen(split_command)
-fargo_par = pickle.load(open(param_fn, "rb"))
+###############################################################################
 
-num_rad = np.loadtxt("dims.dat")[-2]
-num_theta = np.loadtxt("dims.dat")[-1]
+### Input Parameters ###
 
-rad = np.loadtxt("used_rad.dat")[:-1]
+def new_argument_parser(description = "Plot gas density maps."):
+    parser = argparse.ArgumentParser()
+
+    # Frame Selection
+    parser.add_argument('frames', type = int, nargs = '+',
+                         help = 'select single frame or range(start, end, rate). error if nargs != 1 or 3')
+    parser.add_argument('-c', dest = "num_cores", type = int, default = 1,
+                         help = 'number of cores (default: 1)')
+
+    # Files
+    parser.add_argument('--dir', dest = "save_directory", default = "gasDensityMaps",
+                         help = 'save directory (default: gasDensityMaps)')
+
+    # Plot Parameters (variable)
+    parser.add_argument('--hide', dest = "show", action = 'store_false', default = True,
+                         help = 'for single plot, do not display plot (default: display plot)')
+    parser.add_argument('-v', dest = "version", type = int, default = None,
+                         help = 'version number (up to 4 digits) for this set of plot parameters (default: None)')
+
+    parser.add_argument('--range', dest = "r_lim", type = float, nargs = 2, default = None,
+                         help = 'radial range in plot (default: [r_min, r_max])')
+    parser.add_argument('--shift', dest = "center", action = 'store_true', default = False,
+                         help = 'center frame on vortex peak or middle (default: do not center)')
+
+    # Plot Parameters (contours)
+    parser.add_argument('--contour', dest = "use_contours", action = 'store_true', default = False,
+                         help = 'use contours or not (default: do not use contours)')
+    parser.add_argument('--low', dest = "low_contour", type = float, default = 1.1,
+                         help = 'lowest contour (default: 1.1)')
+    parser.add_argument('--high', dest = "high_contour", type = float, default = 3.5,
+                         help = 'highest contour (default: 3.5)')
+    parser.add_argument('--num_levels', dest = "num_levels", type = int, default = None,
+                         help = 'number of contours (choose this or separation) (default: None)')
+    parser.add_argument('--separation', dest = "separation", type = float, default = 0.1,
+                         help = 'separation between contours (choose this or num_levels) (default: 0.1)')
+    
+    # Plot Parameters (rarely need to change)
+    parser.add_argument('--cmap', dest = "cmap", default = "viridis",
+                         help = 'color map (default: viridis)')
+    parser.add_argument('--cmax', dest = "cmax", type = float, default = 1.4,
+                         help = 'maximum density in colorbar (default: 1.4)')
+
+    parser.add_argument('--fontsize', dest = "fontsize", type = int, default = 16,
+                         help = 'fontsize of plot annotations (default: 16)')
+    parser.add_argument('--dpi', dest = "dpi", type = int, default = 100,
+                         help = 'dpi of plot annotations (default: 100)')
+
+    return parser
+
+###############################################################################
+
+### Parse Arguments ###
+args = new_argument_parser().parse_args()
+
+### Get Fargo Parameters ###
+fargo_par = util.get_pickled_parameters()
+
+num_rad = fargo_par["Nrad"]; num_theta = fargo_par["Nsec"]
+r_min = fargo_par["Rmin"]; r_max = fargo_par["Rmax"]
+
+jupiter_mass = 1e-3
+planet_mass = fargo_par["PlanetMass"] / jupiter_mass
+taper_time = fargo_par["MassTaper"]
+
+surface_density_zero = fargo_par["Sigma0"]
+disk_mass = 2 * np.pi * surface_density_zero * (r_max - r_min) / jupiter_mass # M_{disk} = (2 \pi) * \Sigma_0 * r_p * (r_out - r_in)
+
+scale_height = fargo_par["AspectRatio"]
+viscosity = fargo_par["Viscosity"]
+
+### Get Input Parameters ###
+
+# Frames
+frame_range = util.get_frame_range(args.frames)
+
+# Number of Cores 
+num_cores = args.num_cores
+
+# Files
+save_directory = args.save_directory
+if not os.path.isdir(save_directory):
+    os.mkdir(save_directory) # make save directory if it does not already exist
+
+# Plot Parameters (variable)
+show = args.show
+
+rad = np.linspace(r_min, r_max, num_rad)
 theta = np.linspace(0, 2 * np.pi, num_theta)
 
-surface_density = float(fargo_par["Sigma0"])
-scale_height = float(fargo_par["AspectRatio"])
-viscosity = float(fargo_par["Viscosity"])
+version = args.version
+if args.r_lim is None:
+    x_min = r_min; x_max = r_max
+else:
+    x_min = args.r_lim[0]; x_max = args.r_lim[1]
+center = args.center
 
-mass_taper = int(round(float((fargo_par["MassTaper"])), 0))
+# Plot Parameters (contours)
+use_contours = args.use_contours
+low_contour = args.low_contour
+high_contour = args.high_contour
+num_levels = args.num_levels
+if num_levels is None:
+    separation = args.separation
+    num_levels = int(round((high_contour - low_contour) / separation + 1, 0))
 
-##### PLOTTING #####
+# Plot Parameters (constant)
+cmap = args.cmap
+clim = [0, args.cmax]
 
-# Plot Parameters
-my_dpi = 100
+fontsize = args.fontsize
+dpi = args.dpi
 
-fontsize = 14
-linewidth = 4
+### Add new parameters to dictionary ###
+fargo_par["rad"] = rad
+fargo_par["theta"] = theta
 
-def make_plot():
+accretion_data = np.loadtxt("accreted0.dat")
+all_times = accretion_data[:, 0]; accreted = accretion_data[:, 1]
+
+def make_plot(frame, show = False):
     # Set up figure
-    fig = plot.figure(figsize = (700 / my_dpi, 600 / my_dpi), dpi = my_dpi)
+    #fig = plot.figure(figsize = (7, 6), dpi = dpi)
+    #ax = fig.add_subplot(111)
 
-    ### Data ###
-    xs = range(mass_taper)
+    # Data
+    if merge > 0:
+        num_merged_cores = merge
+        density = util.read_merged_data(frame, num_merged_cores, num_rad, num_theta)
+    elif mpi:
+        field = "dens"
+        density = Fields("./", 'gas', frame).get_field(field).reshape(num_rad, num_theta)
+    else:
+        density = fromfile("gasdens%d.dat" % frame).reshape(num_rad, num_theta)
+    normalized_density = density / surface_density_zero
 
-    # Measure 1: Diff Mass Accretion
-    data = np.loadtxt(planet_fn)
-    masses = (data[:, 5]) # Planet Mass from 0 to Mass Taper
-    tapering_accretion = np.diff(masses[:mass_taper + 1])
+    start = np.searchsorted(rad, 0.9)
+    end = np.searchsorted(rad, 1.1)
 
-    # Measure 2: Viscosity
-    viscous_accretion = 3.0 * np.pi * surface_density * viscosity
+    density_sliver = density[start:end]
 
-    # Measure 3: Radial Velocity
-    near_planet = 1.0
-    radius_near_planet = np.searchsorted(rad, near_planet)
-    
-    rate = 10
-    xs_vrad = range(0, mass_taper, rate)
-    radial_velocity_at_planet = np.zeros(len(xs_vrad))
+    planet_x = 1
+    planet_y = 0
+    planet_r = np.sqrt(planet_x**2 + planet_y**2)
 
-    for i, frame in enumerate(xs_vrad):
-        vrad = (fromfile("gasvrad%d.dat" % frame).reshape(num_rad, num_theta))
-        radial_velocity_at_planet[i] = np.average(vrad[radius_near_planet, :])
+    dt = all_times[1] # 0.00410595556163
 
-    radius = 1.0
-    radial_accretion = 2.0 * np.pi * radius * surface_density * radial_velocity_at_planet
+    accreted_mass = 0.0
+    count1 = 0; count2 = 0
 
-    # Curves
-    plot.plot(xs, tapering_accretion, color = "blue", label = "taper", linewidth = linewidth)
-    plot.plot([xs[0], xs[-1]], [viscous_accretion, viscous_accretion], color = "black", label = "viscous", linewidth = linewidth)
-    plot.plot(xs_vrad, radial_velocity_at_planet, color = "red", label = "v_rad (+)", linewidth = linewidth) # Positive
-    plot.plot(xs_vrad, -radial_velocity_at_planet, color = "orange", label = "v_rad (-)", linewidth = linewidth) # Negative
+    for i, r_i in enumerate(rad[start:end]):
+        for j, az_i in enumerate(theta):
+            xc = r_i * np.cos(az_i)
+            yc = r_i * np.sin(az_i)
 
-    # Limits
-    plot.xlim(xs[0], xs[-1])
-    plot.yscale('log')
+            dx = planet_x - xc
+            dy = planet_y - yc
 
-    # Annotate
-    this_title = readTitle()
-    plot.xlabel("Number of Orbits", fontsize = fontsize)
-    plot.ylabel("Accretion Rate", fontsize = fontsize)
-    plot.title("Accretion for %s" % (this_title), fontsize = fontsize + 1)
+            distance = np.sqrt(dx**2 + dy**2)
+            r_roche = (planet_mass * 1e-3 / 3.0)**(1.0 / 3.0) * planet_r
 
-    plot.legend(loc = "upper right", bbox_to_anchor = (1.36, 1.0)) # outside of plot
+            cell_mass = density[start+i, j] * (np.pi / num_theta) * (rad[start+i+1]**2 - r_i**2)
+            dm = 0.0
 
-    # Save + Close
-    plot.savefig("accretion_diagnosis.png", bbox_inches = 'tight', dpi = my_dpi)
-    plot.show()
-    plot.close(fig) # Close Figure (to avoid too many figures)
+            if distance < 0.75 * r_roche:
+                dm = (1.0 / 3.0 * dt) * cell_mass
+                cell_mass *= (1.0 - 1.0 / 3.0 * dt)
 
-make_plot()
+                count1 += 1
+                accreted_mass += dm
+
+            if distance < 0.45 * r_roche:
+                dm = (2.0 / 3.0 * dt) * cell_mass
+
+                count2 += 1
+                accreted_mass += dm
+
+    print count1, count2
+    print accreted_mass, accreted[0]
+
+
+##### Make Plots! #####
+
+# Iterate through frames
+
+if len(frame_range) == 1:
+    make_plot(frame_range[0], show = show)
+else:
+    if num_cores > 1:
+        p = Pool(num_cores) # default number of processes is multiprocessing.cpu_count()
+        p.map(make_plot, frame_range)
+        p.terminate()
+    else:
+        for frame in frame_range:
+            make_plot(frame)
---
+++ code_fargo/diagnoseAccretion_old.py
@@ -0,0 +1,122 @@
+"""
+Compares tapering "accretion rate" to usual measurements (involving viscosity or radial velocity)
+
+Usage:
+python diagnoseAccretion.py
+"""
+
+import sys
+import os
+import subprocess
+import pickle
+import glob
+from multiprocessing import Pool
+
+import math
+import numpy as np
+
+from scipy.ndimage import filters as ff
+
+import matplotlib
+#matplotlib.use('Agg')
+from matplotlib import rcParams as rc
+from matplotlib import pyplot as plot
+
+from pylab import rcParams
+from pylab import fromfile
+
+import util
+from readTitle import readTitle
+
+## Set file names ##
+fargo_fn = "fargo2D1D"
+if os.path.exists(fargo_fn):
+    # fargo2D1D
+    planet_fn = "planet1.dat"
+else:
+    # fargo
+    planet_fn = "planet0.dat"
+
+### Get FARGO Parameters ###
+# Create param file if it doesn't already exist
+param_fn = "params.p"
+if not os.path.exists(param_fn):
+    command = "python pickleParameters.py"
+    split_command = command.split()
+    subprocess.Popen(split_command)
+fargo_par = pickle.load(open(param_fn, "rb"))
+
+num_rad = np.loadtxt("dims.dat")[-2]
+num_theta = np.loadtxt("dims.dat")[-1]
+
+rad = np.loadtxt("used_rad.dat")[:-1]
+theta = np.linspace(0, 2 * np.pi, num_theta)
+
+surface_density = float(fargo_par["Sigma0"])
+scale_height = float(fargo_par["AspectRatio"])
+viscosity = float(fargo_par["Viscosity"])
+
+mass_taper = int(round(float((fargo_par["MassTaper"])), 0))
+
+##### PLOTTING #####
+
+# Plot Parameters
+my_dpi = 100
+
+fontsize = 14
+linewidth = 4
+
+def make_plot():
+    # Set up figure
+    fig = plot.figure(figsize = (700 / my_dpi, 600 / my_dpi), dpi = my_dpi)
+
+    ### Data ###
+    xs = range(mass_taper)
+
+    # Measure 1: Diff Mass Accretion
+    data = np.loadtxt(planet_fn)
+    masses = (data[:, 5]) # Planet Mass from 0 to Mass Taper
+    tapering_accretion = np.diff(masses[:mass_taper + 1])
+
+    # Measure 2: Viscosity
+    viscous_accretion = 3.0 * np.pi * surface_density * viscosity
+
+    # Measure 3: Radial Velocity
+    near_planet = 1.0
+    radius_near_planet = np.searchsorted(rad, near_planet)
+    
+    rate = 10
+    xs_vrad = range(0, mass_taper, rate)
+    radial_velocity_at_planet = np.zeros(len(xs_vrad))
+
+    for i, frame in enumerate(xs_vrad):
+        vrad = (fromfile("gasvrad%d.dat" % frame).reshape(num_rad, num_theta))
+        radial_velocity_at_planet[i] = np.average(vrad[radius_near_planet, :])
+
+    radius = 1.0
+    radial_accretion = 2.0 * np.pi * radius * surface_density * radial_velocity_at_planet
+
+    # Curves
+    plot.plot(xs, tapering_accretion, color = "blue", label = "taper", linewidth = linewidth)
+    plot.plot([xs[0], xs[-1]], [viscous_accretion, viscous_accretion], color = "black", label = "viscous", linewidth = linewidth)
+    plot.plot(xs_vrad, radial_velocity_at_planet, color = "red", label = "v_rad (+)", linewidth = linewidth) # Positive
+    plot.plot(xs_vrad, -radial_velocity_at_planet, color = "orange", label = "v_rad (-)", linewidth = linewidth) # Negative
+
+    # Limits
+    plot.xlim(xs[0], xs[-1])
+    plot.yscale('log')
+
+    # Annotate
+    this_title = readTitle()
+    plot.xlabel("Number of Orbits", fontsize = fontsize)
+    plot.ylabel("Accretion Rate", fontsize = fontsize)
+    plot.title("Accretion for %s" % (this_title), fontsize = fontsize + 1)
+
+    plot.legend(loc = "upper right", bbox_to_anchor = (1.36, 1.0)) # outside of plot
+
+    # Save + Close
+    plot.savefig("accretion_diagnosis.png", bbox_inches = 'tight', dpi = my_dpi)
+    plot.show()
+    plot.close(fig) # Close Figure (to avoid too many figures)
+
+make_plot()
