b58c2dc13f3a5fc89adb2e3ced367d3fbcc566a0
Sportsfan77777 <mhammer44444@gmail.com>
2019-08-30 03:17:53 -0400

    first commit

---
+++ code_synthetic_images3d/plotAzimuthalIntensityProfiles.py
@@ -0,0 +1,283 @@
+"""
+convolves intensity map to make it look like an alma image
+
+python convolveIntensityMap.py frame
+"""
+
+import sys, os, subprocess
+import pickle, glob
+from multiprocessing import Pool
+import argparse
+
+import math
+import numpy as np
+
+import matplotlib
+#matplotlib.use('Agg')
+from matplotlib import rcParams as rc
+from matplotlib import pyplot as plot
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+from pylab import rcParams
+from pylab import fromfile
+
+import util
+import square as sq
+import azimuthal as az
+
+from colormaps import cmaps
+for key in cmaps:
+    plot.register_cmap(name = key, cmap = cmaps[key])
+
+###############################################################################
+
+### Input Parameters ###
+
+def new_argument_parser(description = "Plot convolved intensity maps."):
+    parser = argparse.ArgumentParser()
+
+    # Frame Selection
+    parser.add_argument('frames', type = int, nargs = '+',
+                         help = 'select single frame or range(start, end, rate). error if nargs != 1 or 3')
+    parser.add_argument('-c', dest = "num_cores", type = int, default = 1,
+                         help = 'number of cores (default: 1)')
+
+    # Files
+    parser.add_argument('--dir', dest = "save_directory", default = "cartesianIntensityMaps",
+                         help = 'save directory (default: cartesianIntensityMaps)')
+
+    # Plot Parameters (variable)
+    parser.add_argument('--hide', dest = "show", action = 'store_false', default = True,
+                         help = 'for single plot, do not display plot (default: display plot)')
+    parser.add_argument('--id', dest = "id_number", type = int, default = 0,
+                         help = 'id number (up to 4 digits) for this set of input parameters (default: 0)')
+    parser.add_argument('-v', dest = "version", type = int, default = None,
+                         help = 'version number (up to 4 digits) for this set of plot parameters (default: None)')
+
+    parser.add_argument('--max_y', dest = "max_y", nargs = '+', type = float, default = None,
+                         help = 'max_y for each frame, or same for all (default: None)')
+    parser.add_argument('--profiles', dest = "num_profiles", type = int, default = 1,
+                         help = 'number of profiles (default: 1)')
+    parser.add_argument('-s', dest = "num_scale_heights", type = float, default = 8.0,
+                         help = 'number of scale heights (default: 8.0)')
+
+    parser.add_argument('-n', dest = "normalize", action = 'store_false', default = True,
+                         help = 'normalize by max (default: normalize)')
+
+    # Plot Parameters (rarely need to change)
+    parser.add_argument('--cmap', dest = "cmap", default = "inferno",
+                         help = 'color map (default: inferno)')
+    parser.add_argument('--cmax', dest = "cmax", type = int, default = None,
+                         help = 'maximum density in colorbar (default: 2.5)')
+
+    parser.add_argument('--fontsize', dest = "fontsize", type = int, default = 19,
+                         help = 'fontsize of plot annotations (default: 19)')
+    parser.add_argument('--labelsize', dest = "labelsize", type = int, default = 16,
+                         help = 'fontsize of plot annotations (default: 16)')
+    parser.add_argument('--dpi', dest = "dpi", type = int, default = 100,
+                         help = 'dpi of plot annotations (default: 100)')
+
+    return parser
+
+###############################################################################
+
+### Parse Arguments ###
+args = new_argument_parser().parse_args()
+
+### Get ID%04d Parameters ###
+fn = "id%04d_par.p" % args.id_number
+fargo_par = pickle.load(open(fn, "rb"))
+
+p = fargo_par["p"]
+
+num_rad = p.ny; num_theta = p.nx
+r_min = p.ymin; r_max = p.ymax
+
+surface_density_zero = p.sigma0
+dust_surface_density_zero = p.sigma0 * p.epsilon
+
+planet_mass = 1.0
+taper_time = p.masstaper
+
+scale_height = p.aspectratio
+viscosity = p.nu
+
+dt = p.ninterm * p.dt
+
+rad = np.linspace(r_min, r_max, num_rad)
+theta = np.linspace(0, 2 * np.pi, num_theta)
+
+planet_radius = fargo_par["Radius"]
+
+beam_size = fargo_par["Beam"]
+wavelength = fargo_par["Wavelength"]
+distance = fargo_par["Distance"]
+
+arc_beam = beam_size * planet_radius / distance
+
+### Get Input Parameters ###
+
+# Frames
+if len(args.frames) == 1:
+    frame_range = args.frames
+elif len(args.frames) == 3:
+    start = args.frames[0]; end = args.frames[1]; rate = args.frames[2]
+    frame_range = range(start, end + 1, rate)
+else:
+    print "Error: Must supply 1 or 3 frame arguments\nWith one argument, plots single frame\nWith three arguments, plots range(start, end + 1, rate)"
+    exit()
+
+# Number of Cores 
+num_cores = args.num_cores
+
+# Files
+save_directory = args.save_directory
+if not os.path.isdir(save_directory):
+    os.mkdir(save_directory) # make save directory if it does not already exist
+
+# Plot Parameters (variable)
+show = args.show
+
+rad = np.linspace(r_min, r_max, num_rad)
+theta = np.linspace(0, 2 * np.pi, num_theta)
+
+id_number = args.id_number
+version = args.version
+max_y = args.max_y
+if normalize:
+    max_y = 1
+
+num_profiles = args.num_profiles
+num_scale_heights = args.num_scale_heights
+
+normalize = args.normalize
+
+# Plot Parameters (constant)
+fontsize = args.fontsize
+labelsize = args.labelsize
+linewidth = args.linewidth
+alpha = args.alpha
+dpi = args.dpi
+
+rc['xtick.labelsize'] = labelsize
+rc['ytick.labelsize'] = labelsize
+
+# Planet File
+# Data
+data = np.loadtxt("../../planet0.dat")
+times = data[:, 0]; base_mass = data[:, 7]
+accreted_mass = data[:, 8] / jupiter_mass
+
+###############################################################################
+
+##### PLOTTING #####
+
+def make_plot(frame, show = False):
+    # Set up figure
+    fig = plot.figure(figsize = (7, 6), dpi = dpi)
+    ax = fig.add_subplot(111)
+
+    # Data
+    intensity_polar = util.read_data(frame, 'polar_intensity', fargo_par, id_number = id_number)
+    if normalize:
+        intensity_polar /= np.max(intensity_polar)
+    azimuthal_radii, azimuthal_profiles = az.get_profiles(intensity_polar, fargo_par, args, shift = None)
+
+    # Get Shift
+    gas_density = util.read_data(frame, 'gas', fargo_par, directory = "../../")
+
+    # Shift gas density with center of dust density
+    shift = az.shift_away_from_minimum(gas_density, fargo_par)
+
+    # Normalize
+    if normalize:
+        intensity_cart /= np.max(intensity_cart)
+
+    # Arcseconds or Planet Radii
+    if arc:
+        arc_weight = planet_radius / distance # related to parallax
+    else:
+        arc_weight = 1
+
+    ### Plot ###
+    for i, (radius, azimuthal_profile) in enumerate(zip(azimuthal_radii, azimuthal_profiles)):
+        plot.plot(x, azimuthal_profile, linewidth = linewidth, dashes = dashes[i], alpha = alpha)
+
+    # Locate Planet
+    if shift is None:
+        planet_loc = theta[0]
+    else:
+        if shift < -len(theta):
+            shift += len(theta)
+        planet_loc = theta[shift] * (180.0 / np.pi) - 180.0
+    plot.scatter(planet_loc, 0, c = "k", s = 150, marker = "D", zorder = 100) # planet
+
+    # Label star and planet
+    time = fargo_par["Ninterm"] * fargo_par["DT"]
+    orbit = (time / (2 * np.pi)) * frame
+    if orbit >= taper_time:
+        current_mass = planet_mass
+    else:
+        current_mass = np.power(np.sin((np.pi / 2) * (1.0 * orbit / taper_time)), 2) * planet_mass
+
+    current_mass += accreted_mass[frame]
+
+    # Axes
+    max_x = 180
+    plot.xlim(-max_x, max_x)
+    angles = np.linspace(-max_x, max_x, 7)
+    plot.xticks(angles)
+
+    if max_y is None:
+        plot.ylim(0, plot.ylim()[-1]) # No Input
+    else:
+        plot.ylim(0, max_y) # Input
+
+    # Annotate Axes
+    time = fargo_par["Ninterm"] * fargo_par["DT"]
+    orbit = (time / (2 * np.pi)) * frame
+    current_mass = util.get_current_mass(orbit, taper_time, planet_mass = planet_mass)
+
+    plot.xlabel(r"$\phi - \phi_\mathrm{center}$ $\mathrm{(degrees)}$", fontsize = fontsize + 2)
+    plot.ylabel(r"$I$ / $I_\mathrm{max}$", fontsize = fontsize)
+
+    # Title
+    title = r"$t$ $=$ $%.1f$  [$m_p(t)$ $=$ $%.2f$ $M_J$]" % (orbit, current_mass)
+    plot.title("%s" % (title), y = 1.015, fontsize = fontsize + 1)
+
+    taper_title = r"$T_\mathrm{growth} = %d$" % taper_time
+    plot.text(0.0 * box_size, 2 * arc_weight, taper_title, fontsize = fontsize, color = 'white', horizontalalignment = 'center', bbox=dict(facecolor = 'black', edgecolor = 'white', pad = 10.0), zorder = 100)
+
+    # Save, Show,  and Close
+    if version is None:
+        save_fn = "%s/id%04d_azimuthalIntensityProfiles_%04d.png" % (save_directory, id_number, frame)
+    else:
+        save_fn = "%s/v%04d_id%04d_azimuthalIntensityProfiles_%04d.png" % (save_directory, version, id_number, frame)
+    plot.savefig(save_fn, bbox_inches = 'tight', dpi = dpi)
+
+    if show:
+        plot.show()
+
+    plot.close(fig) # Close Figure (to avoid too many figures)
+
+
+###############################################################################
+
+
+##### Make Plots! #####
+
+# Iterate through frames
+
+if len(frame_range) == 1:
+    make_plot(frame_range[0], show = show)
+else:
+    if num_cores > 1:
+        p = Pool(num_cores) # default number of processes is multiprocessing.cpu_count()
+        p.map(make_plot, frame_range)
+        p.terminate()
+    else:
+        for frame in frame_range:
+            make_plot(frame)
+
+
+
