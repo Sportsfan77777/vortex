70aa280d1cd35c6f25885577992ae920f4b7149b
Sportsfan77777 <mhammer44444@gmail.com>
2019-11-19 16:58:30 -0700

    first commit to get last frame

--- code_dust_fargo3d/getLastFrame.py
+++ code_dust_fargo3d/getLastFrame.py
@@ -4,5 +4,5 @@ import numpy as np
 data = np.loadtxt("bigplanet0.dat")
 data = data[-2:-1] # second-to-last line, in case it is in the middle of writing
 
-frame = data[0]
+frame = int(round, data[0][0], 0)
 print frame
