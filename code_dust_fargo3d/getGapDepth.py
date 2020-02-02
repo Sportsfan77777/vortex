775699acbf33161e18e796f0cdba34e9b69f59a2
Sportsfan77777 <mhammer44444@gmail.com>
2020-02-01 21:22:00 -0700

    first commit to quick gap depth

---
+++ code_dust_fargo3d/getGapDepth.py
@@ -0,0 +1,92 @@
+"""
+plot azimuthally averaged density
+then, makes movies
+
+Usage:
+python plotAveragedDensity.py frame_number <== plot one frame
+python plotAveragedDensity.py -m <== make a movie instead of plots (plots already exist)
+python plotAveragedDensity.py -1 <<<===== Plots a sample
+python plotAveragedDensity.py <== plot all frames and make a movie
+
+"""
+
+import sys, os, subprocess
+import pickle, glob
+from multiprocessing import Pool
+from multiprocessing import Array as mp_array
+import argparse
+
+import math
+import numpy as np
+
+import matplotlib
+from matplotlib import rcParams as rc
+from matplotlib import pyplot as plot
+
+from pylab import rcParams
+from pylab import fromfile
+
+import util
+import azimuthal as az
+from readTitle import readTitle
+
+from advanced import Parameters
+from reader import Fields
+
+from colormaps import cmaps
+for key in cmaps:
+    plot.register_cmap(name = key, cmap = cmaps[key])
+
+# Input Parameter
+frame = sys.argv[1]
+ 
+### Get Fargo Parameters ###
+p = Parameters()
+
+num_rad = p.ny; num_theta = p.nx
+r_min = p.ymin; r_max = p.ymax
+
+surface_density_zero = p.sigma0
+
+# Mass Data
+data = np.loadtxt("planet0.dat")
+times = data[:, 0]
+base_mass = data[:, 7]
+accreted_mass = data[:, 8]
+
+total_mass = base_mass + accreted_mass
+
+# Gap Depth
+
+### Helper Functions ###
+
+def find_min(averagedDensity):
+    outer_disk_start = np.searchsorted(rad, 0.9) # look for max radial density beyond r = 1.1
+    outer_disk_end = np.searchsorted(rad, 1.25) # look for max radial density before r = 2.3
+    min_rad_outer_index = np.argmin(averagedDensity[outer_disk_start : outer_disk_end])
+
+    min_index = outer_disk_start + min_rad_outer_index
+    min_rad = rad[min_index]
+    min_density = averagedDensity[min_index]
+
+    return min_density
+
+def get_min(frame):
+    # Get Data
+    density = fromfile("gasdens%d.dat" % frame).reshape(num_rad, num_theta)
+    averagedDensity = np.average(density, axis = 1)
+    normalized_density = averagedDensity / surface_density_zero
+    
+    # Get Minima
+    min_density = find_min(normalized_density)
+
+    # Print Update
+
+    # Store Data
+    return 1.0 / min_density
+
+total_mass_i = total_mass[frame]
+gap_depth = get_min(frame)
+
+print total_mass_i, gap_depth
+
