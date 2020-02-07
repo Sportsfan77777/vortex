f7b5f5cc94d78811735c04d35dcc64b84a74dbb9
Sportsfan77777 <mhammer44444@gmail.com>
2020-02-07 13:30:13 -0700

    added mass axis

--- code_dust_fargo3d/trackExtentsAndRadii.py
+++ code_dust_fargo3d/trackExtentsAndRadii.py
@@ -313,11 +313,17 @@ def make_plot(show = False):
 
     host.set_xlabel("Time (planet orbits)", fontsize = fontsize)
     host.set_ylabel("Azimuthal Extent (degrees)", fontsize = fontsize)
-    par1.set_ylabel("Radial Extent (scale heights)", fontsize = fontsize, rotation = 180)
-    par2.set_ylabel("Radial Center (planet radii)", fontsize = fontsize, rotation = 180)
+    par1.set_ylabel("Radial Extent (scale heights)", fontsize = fontsize, rotation = 270)
+    par2.set_ylabel("Radial Center (planet radii)", fontsize = fontsize, rotation = 270)
     par3.set_xlabel(r"$M_\mathrm{p}$ [$M_\mathrm{J}$]", fontsize = fontsize)
 
-    title1 = r"$h = %.2f$     $\alpha_\mathrm{disk} = 3 \times 10^{%d}$     $A = %.2f$" % (scale_height, int(np.log(viscosity) / np.log(10)) + 2, accretion)
+    alpha_coefficent = "3"
+    if scale_height == 0.08:
+        alpha_coefficent = "1.5"
+    else if scale_height == 0.04:
+        alpha_coefficent = "6"
+
+    title1 = r"$h = %.2f$     $\alpha = %s \times 10^{%d}$     $A = %.2f$" % (scale_height, alpha_coefficent, int(np.log(viscosity) / np.log(10)) + 2, accretion)
     plot.title("%s" % (title1), y = 1.035, fontsize = fontsize + 1)
 
     # Annotate
