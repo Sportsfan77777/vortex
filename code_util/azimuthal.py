8f0012d2cc243261e19a356877d8787a8c047af1
Sportsfan77777 <mhammer44444@gmail.com>
2020-02-07 19:38:38 -0700

    add contrast method

--- code_util/azimuthal.py
+++ code_util/azimuthal.py
@@ -257,7 +257,7 @@ def get_lookup_shift(frame, directory = "."):
 
 ### Extract Values ###
 
-def get_contrast(data, fargo_par):
+def get_polar_contrast(data, fargo_par):
     """ for polar data, returns contrast between peak and opposite point """
     ######## Get Parameters #########
     rad = fargo_par["rad"]
@@ -280,6 +280,43 @@ def get_contrast(data, fargo_par):
 
     return contrast, data_peak, data_opposite
 
+def get_contrast(data, fargo_par, normalize = False, threshold = 0.5, sliver_width = 0.5, start = outer_start, end = outer_end):
+    """ Get azimuthal extent at peak across a given threshold """
+
+    ######## Get Parameters #########
+    rad = fargo_par["rad"]
+    theta = fargo_par["theta"]
+
+    scale_height = fargo_par["AspectRatio"]
+
+    ########### Method ##############
+
+    # Search outer disk only
+    outer_disk_start = np.searchsorted(rad, start) # look for max density beyond r = 1.1
+    outer_disk_end = np.searchsorted(rad, end) # look for max density before r = 2.3
+    data_segment = data[outer_disk_start : outer_disk_end]
+
+    # Get peak in azimuthal profile
+    avg_data = np.average(data_segment, axis = 1) # avg over theta
+    segment_arg_peak = np.argmax(avg_data)
+    arg_peak = np.searchsorted(rad, rad[outer_disk_start + segment_arg_peak])
+    peak_rad = rad[arg_peak]
+
+    # Zoom in on peak --- Average over half a scale height
+    half_width = (0.5 * sliver_width) * scale_height
+    zoom_start = np.searchsorted(rad, peak_rad - half_width)
+    zoom_end = np.searchsorted(rad, peak_rad + half_width)
+
+    data_sliver = data[zoom_start : zoom_end]
+    length = len(data_sliver); std = length / 3.0
+    weights = gaussian(length, std)
+    azimuthal_profile = np.average(data_sliver, weights = weights, axis = 0) # avg over rad to get azimuthal profile
+
+    if normalize:
+        azimuthal_profile /= np.max(azimuthal_profile)
+
+    contrast = np.max(azimuthal_profile) / np.min(azimuthal_profile)
+    return contrast
 
 def get_extent(data, fargo_par, normalize = False, threshold = 0.5, sliver_width = 0.5, start = outer_start, end = outer_end):
     """ Get azimuthal extent at peak across a given threshold """
