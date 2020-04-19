a9776ee30ab744d1648b31b943cbb7d7f7752d8b
Sportsfan77777 <mhammer44444@gmail.com>
2020-04-19 13:10:18 -0700

    first commit to wave action

--- code_dust_fargo3d/plotWaveAction.py
+++ code_dust_fargo3d/plotWaveAction.py
@@ -197,15 +197,15 @@ def make_plot(frame, show = False):
 
     print_a = np.searchsorted(rad, 1.1)
     print_b = np.searchsorted(rad, 1.4)
-    print (wave_locations[print_a : print_b] * (180.0 / np.pi))
-    print (shift[print_a : print_b] * (180.0 / np.pi))
+    print (theta[wave_locations[print_a : print_b]] * (180.0 / np.pi))
+    print (theta[shift[print_a : print_b]] * (180.0 / np.pi))
 
     left = np.searchsorted(theta, 177 * (np.pi / 180.0))
     right = np.searchsorted(theta, 183 * (np.pi / 180.0))
 
-    centered_density = np.shift(density, shift, axis = -1)[left : right]
-    centered_radial_velocity = np.shift(radial_velocity, shift, axis = -1)[left : right]
-    centered_azimuthal_velocity = np.shift(azimuthal_velocity, shift, axis = -1)[left : right]
+    centered_density = np.roll(density, shift, axis = -1)[left : right]
+    centered_radial_velocity = np.roll(radial_velocity, shift, axis = -1)[left : right]
+    centered_azimuthal_velocity = np.roll(azimuthal_velocity, shift, axis = -1)[left : right]
 
     residual_density = centered_density - np.min(centered_density)
 
