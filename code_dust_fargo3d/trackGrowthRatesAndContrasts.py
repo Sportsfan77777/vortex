36a751a8b3a8b1ace957453b69f50d9605d3d8ff
Sportsfan77777 <mhammer44444@gmail.com>
2020-05-03 14:55:54 -0700

    first commit to track growth rates

--- code_dust_fargo3d/trackGrowthRatesAndContrasts.py
+++ code_dust_fargo3d/trackGrowthRatesAndContrasts.py
@@ -353,10 +353,10 @@ def make_plot(show = False):
     # Axes
     ax3.set_xlim(x[0], x[-1])
     #plot.ylim(0.001, 0.1)
-    ax3.set_ylim(10**(-5), 10**(-2))
+    ax3.set_ylim(0.6 * 10**(-5), 0.6 * 10**(-2))
     ax3.set_yscale('log')
 
-    ax3.set_yticks([10**(-5), 10**(-4), 10**(-3), 10**(-2)])
+    ax3.set_yticks([10**(-5), 10**(-4), 10**(-3)])
 
     # Annotate
     ax3.set_xlabel("Time (planet orbits)", fontsize = fontsize)
