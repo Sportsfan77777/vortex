91332e0f596e867a97bd9baa6fa619e650b0c65a
Sportsfan77777 <mhammer44444@gmail.com>
2020-06-24 15:46:32 -0700

    first commit to two intensity maps

--- code_synthetic_images3d/plotTwoIntensityMapsOverTime.py
+++ code_synthetic_images3d/plotTwoIntensityMapsOverTime.py
@@ -37,7 +37,7 @@ def new_argument_parser(description = "Plot convolved intensity maps."):
     parser = argparse.ArgumentParser()
 
     # Frame Selection
-    parser.add_argument('frames', type = int, nargs = 3,
+    parser.add_argument('frames', type = int, nargs = 2,
                          help = 'select single frame or range(start, end, rate). error if nargs != 1 or 3')
     parser.add_argument('-c', dest = "num_cores", type = int, default = 1,
                          help = 'number of cores (default: 1)')
