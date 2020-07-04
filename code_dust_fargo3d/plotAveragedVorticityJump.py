e2e1231dd1e4d6b736b554c9a3026b575ce940c7
Sportsfan77777 <mhammer44444@gmail.com>
2020-07-03 17:45:03 -0700

    first commit to averaged vorticity jump

--- code_dust_fargo3d/plotAveragedVorticityJump.py
+++ code_dust_fargo3d/plotAveragedVorticityJump.py
@@ -259,7 +259,7 @@ def make_plot(frame, show = False):
 
     unit = "r_\mathrm{p}"
     plot.xlabel(r"Radius [$%s$]" % unit, fontsize = fontsize)
-    plot.ylabel(r"$v_\mathrm{\phi} - v_\mathrm{K, r=1}$", fontsize = fontsize)
+    plot.ylabel(r"$\Delta \omega$", fontsize = fontsize)
 
     #if title is None:
     #    plot.title("Dust Density Map\n(t = %.1f)" % (orbit), fontsize = fontsize + 1)
