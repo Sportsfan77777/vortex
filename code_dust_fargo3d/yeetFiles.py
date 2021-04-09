4178b312e44c741af7fd851704803edc86e094c4
Sportsfan77777 <mhammer@as.arizona.edu>
2021-04-09 03:27:30 -0700

    add yeet files

--- code_dust_fargo3d/yeetFiles.py
+++ code_dust_fargo3d/yeetFiles.py
@@ -7,7 +7,6 @@ import glob, pickle
 import argparse
 
 import numpy as np
-import util
 
 ###############################################################################
 
@@ -84,6 +83,26 @@ def new_argument_parser(description = "Plot convolved intensity maps."):
                          help = 'delete, instead of just test out (default: do not delete)')
 
     return parser
+
+###############################################################################
+
+### Helper Function ###
+
+def get_frame_range(frame_selection):
+    """ return array of selected frames"""
+    if len(frame_selection) == 1:
+        frame_range = frame_selection
+    elif len(frame_selection) == 2:
+        start = frame_selection[0]; end = frame_selection[1]
+        frame_range = range(start, end + 1)
+    elif len(frame_selection) == 3:
+        start = frame_selection[0]; end = frame_selection[1]; rate = frame_selection[2]
+        frame_range = range(start, end + 1, rate)
+    else:
+        print "Error: Must supply 1, 2, or 3 frame arguments\nWith one argument, plots single frame\nWith two arguments, plots range(start, end + 1)\nWith three arguments, plots range(start, end + 1, rate)"
+        exit()
+
+    return frame_range
 
 ###############################################################################
 
