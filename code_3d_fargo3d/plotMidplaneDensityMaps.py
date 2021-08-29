6bc8198fe52b9ebb9950383c5f49094b8b112180
Michael Hammer <sportsfan77777@Michaels-MacBook-Pro.local>
2021-08-28 23:45:47 -0400

    switch back to dat

--- code_3d_fargo3d/plotMidplaneDensityMaps.py
+++ code_3d_fargo3d/plotMidplaneDensityMaps.py
@@ -278,8 +278,8 @@ def make_plot(frame, show = False):
 
     # Data
     field = "dens"
-    density = Fields("./", 'gas', frame).get_field(field).reshape(num_z, num_rad, num_theta)
-    #density = fromfile("gasdens%d.dat" % frame).reshape(num_rad, num_theta, num_z)
+    #density = Fields("./", 'gas', frame).get_field(field).reshape(num_z, num_rad, num_theta)
+    density = fromfile("gasdens%d.dat" % frame).reshape(num_rad, num_theta, num_z)
     midplane_density = density[num_z / 2, :, :]
 
     scale_height_function = scale_height * rad
