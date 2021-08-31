cd4d6496e21d546f1dd87e4c8eb20534961b0092
Michael Hammer <sportsfan77777@Michaels-MacBook-Pro.local>
2021-08-30 22:53:10 -0400

    cmaps

--- code_3d_fargo3d/plotMidplaneDustDensityMaps.py
+++ code_3d_fargo3d/plotMidplaneDustDensityMaps.py
@@ -367,9 +367,9 @@ def make_plot(frame, show = False):
 
     # Save, Show, and Close
     if version is None:
-        save_fn = "%s/densityMap_%04d.png" % (save_directory, frame)
+        save_fn = "%s/dustDensityMap_%04d.png" % (save_directory, frame)
     else:
-        save_fn = "%s/v%04d_densityMap_%04d.png" % (save_directory, version, frame)
+        save_fn = "%s/v%04d_dustDensityMap_%04d.png" % (save_directory, version, frame)
     plot.savefig(save_fn, bbox_inches = 'tight', dpi = dpi)
 
     if show:
