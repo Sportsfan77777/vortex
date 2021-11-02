45c620b3a52ac027ec9544967d1d4a8e5b8cf6a0
Sportsfan77777 <sportsfan77777@Michaels-MacBook-Pro.local>
2021-11-02 15:26:11 -0400

    update

--- code_3d_fargo3d/plotRadialCutAveragedDensity.py
+++ code_3d_fargo3d/plotRadialCutAveragedDensity.py
@@ -273,9 +273,7 @@ def make_plot(frame, show = False):
 
     # Axes
     if args.max_y is None:
-        x_min_i = np.searchsorted(x, x_min)
-        x_max_i = np.searchsorted(x, x_max)
-        max_y = 1.1 * max(y[x_min_i : x_max_i])
+        max_y = 1.1 * max(y)
     else:
         max_y = args.max_y
 
