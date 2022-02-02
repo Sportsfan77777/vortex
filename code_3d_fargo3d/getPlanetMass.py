48668a17ed17238e7b1fef09f7aa7747cd25191d
Sportsfan77777 <mhammer44444@gmail.com>
2022-02-02 22:20:35 +0800

    first commit to get planet mass

---
+++ code_3d_fargo3d/getPlanetMass.py
@@ -0,0 +1,56 @@
+"""
+plot azimuthally averaged density
+then, makes movies
+
+Usage:
+python plotAveragedDensity.py frame_number <== plot one frame
+python plotAveragedDensity.py -m <== make a movie instead of plots (plots already exist)
+python plotAveragedDensity.py -1 <<<===== Plots a sample
+python plotAveragedDensity.py <== plot all frames and make a movie
+
+"""
+
+import sys, os, subprocess
+import pickle, glob
+from multiprocessing import Pool
+from multiprocessing import Array as mp_array
+import argparse
+
+import math
+import numpy as np
+
+import matplotlib
+from matplotlib import rcParams as rc
+from matplotlib import pyplot as plot
+
+from pylab import rcParams
+from pylab import fromfile
+
+import util
+import azimuthal as az
+from readTitle import readTitle
+
+from advanced import Parameters
+from reader import Fields
+
+from colormaps import cmaps
+for key in cmaps:
+    plot.register_cmap(name = key, cmap = cmaps[key])
+
+# Input Parameter
+frame = int(sys.argv[1])
+
+# Mass Data
+data = np.loadtxt("planet0.dat")
+times = data[:, 0]
+base_mass = data[:, 7]
+accreted_mass = data[:, 8]
+
+total_mass = base_mass + accreted_mass
+
+jupiter_mass = 1e-3
+total_mass_i = total_mass[frame] / jupiter_mass
+gap_depth = get_min(frame)
+
+print total_mass_i
+
