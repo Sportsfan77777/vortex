266014a9270a8959ea65e6e614a511da4544711d
Sportsfan77777 <mhammer44444@gmail.com>
2022-09-09 03:04:56 +0800

    add planet position

--- code_dust_fargo3d/plotMaximumConditionSequence.py
+++ code_dust_fargo3d/plotMaximumConditionSequence.py
@@ -265,7 +265,7 @@ def make_plot(frame_range, show = False):
 
     x_range = x_max - x_min; x_mid = x_min + x_range / 2.0
     x_shift = 0.35; y_shift = 0.12
-    x_text = 0.025; y_text = 0.74
+    x_text = 0.025; y_text = 0.76
     x_text_header = 0.5; y_text_header = 1.14
 
     alpha_coefficent = "3"
