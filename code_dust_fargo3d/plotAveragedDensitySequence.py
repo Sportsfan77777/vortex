d49fb30bf7ae010572386244a68e53935685837c
Sportsfan77777 <mhammer44444@gmail.com>
2022-09-09 02:31:42 +0800

    add planet position

--- code_dust_fargo3d/plotAveragedDensitySequence.py
+++ code_dust_fargo3d/plotAveragedDensitySequence.py
@@ -236,7 +236,7 @@ def make_plot(frame_range, show = False):
 
     unit = "r_\mathrm{p}"
     plot.xlabel(r"Radius [$%s$]" % unit, fontsize = fontsize)
-    plot.ylabel(r"$\Sigma$ $/$ $\Sigma_0$", fontsize = fontsize)
+    plot.ylabel(r"$<$ $\Sigma$ $/$ $\Sigma_0$ $>$", fontsize = fontsize)
 
     #if title is None:
     #    plot.title("Dust Density Map\n(t = %.1f)" % (orbit), fontsize = fontsize + 1)
