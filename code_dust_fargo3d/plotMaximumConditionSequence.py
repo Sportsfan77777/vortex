1baa87484f35b0e09d883328bef7692920601644
Sportsfan77777 <mhammer44444@gmail.com>
2022-09-09 04:59:29 +0800

    add planet position

--- code_dust_fargo3d/plotMaximumConditionSequence.py
+++ code_dust_fargo3d/plotMaximumConditionSequence.py
@@ -279,9 +279,9 @@ def make_plot(frame_range, show = False):
         alpha_coefficent = "6"
 
     text1 = r"$h = %.2f$" % (scale_height)
-    plot.text(x_text, y_text + y_shift, text1, horizontalalignment = 'left', fontsize = fontsize - 1, transform = ax.transAxes, zorder = 1000, bbox = dict(facecolor = 'white', alpha = alpha, edgecolor = None))
+    plot.text(x_text, y_text + y_shift, text1, horizontalalignment = 'left', fontsize = fontsize - 1, transform = ax.transAxes, zorder = 1000, bbox = dict(facecolor = 'white', alpha = alpha, edgecolor = 'none'))
     text2 = r"$\alpha \approx %s \times 10^{%d}$" % (alpha_coefficent, int(np.log(viscosity) / np.log(10)) + 2)
-    plot.text(x_text, y_text, text2, horizontalalignment = 'left', fontsize = fontsize - 1, transform = ax.transAxes, zorder = 1000, bbox = dict(facecolor = 'white', alpha = alpha - 0.2, edgecolor = None))
+    plot.text(x_text, y_text, text2, horizontalalignment = 'left', fontsize = fontsize - 1, transform = ax.transAxes, zorder = 1000, bbox = dict(facecolor = 'white', alpha = alpha - 0.2, edgecolor = 'none'))
 
     surface_density_base = 1.157e-4
     final_frame = 5000
