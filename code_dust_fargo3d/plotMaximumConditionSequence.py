209c52fae3bb5e36eb4b81f12df57c76e44de1ee
Sportsfan77777 <mhammer44444@gmail.com>
2022-09-09 04:23:26 +0800

    add planet position

--- code_dust_fargo3d/plotMaximumConditionSequence.py
+++ code_dust_fargo3d/plotMaximumConditionSequence.py
@@ -217,7 +217,7 @@ def make_plot(frame_range, show = False):
         x = rad[1:]
         y = maximum_condition # Highlight middle two!
         #result = plot.plot(x, y, c = colors[i % len(colors)], linewidth = linewidth + np.ceil(i/10.0), linestyle = linestyles[i % 2], zorder = 99 - abs(1.5 - i), label = r"$t$ $=$ $%d$ $T_\mathrm{p}$" % frame)
-        result = plot.plot(x, y, c = colors[i % len(colors)], linewidth = linewidth + np.ceil(i/10.0) + (i == 2), linestyle = linestyles[i % 2], zorder = 99 - abs(1.5 - i), label = r"$t$ $=$ $%d$ $T_\mathrm{p}$" % frame)
+        result = plot.plot(x, y, c = colors[i % len(colors)], linewidth = linewidth + np.ceil(i/10.0) + (i == 2), linestyle = linestyles[i % 2], zorder = 99 - abs(1.5 - i), label = r"$t$ $=$ $%d$ $T_\mathrm{p}$" % frame, alpha = 0.8)
 
 
         # Reference line for pressure bump
