fbceebfda286f05a50a4d6d37f790a1fc6980d6f
Sportsfan77777 <mhammer44444@gmail.com>
2022-09-13 05:23:20 +0800

    first commit to this plot

---
+++ code_3d_fargo3d/comparePlanetMassVsDiscMass.py
@@ -0,0 +1,148 @@
+import numpy as np
+from matplotlib import pyplot as plot
+from matplotlib import rcParams as rc
+from matplotlib import ticker as ticker
+
+from itertools import groupby
+import pickle
+
+###############################################################################
+
+master_directories = []
+disc_masses = []
+final_masses = []
+
+#master_directories.append(["h08_nu7_a167", "h08_nu7_a05", "h08_nu7_a02", "h08_nu7_a01"])
+#lifetimes.append([3573, 3875, 7249, 9625])
+#final_masses.append([1.22, 0.59, 0.33, 0.18])
+
+#### Note: Re-do ALL of these!!! ####
+master_directories.append(["h06_nu7_s0347", "h06_nu7_s0578", "h06_nu7_s0694", "h06_nu7_s0926", "h06_nu7_s1157", "h06_nu7_s1736", "h06_nu7_s2315", "h06_nu7_s3472"])
+disc_masses.append([0.3, 0.5, 0.6, 0.8, 1.0, 1.5, 2.0, 3.0, 5.0])
+final_masses.append([0.10, 0.15, 0.17, 0.21, 0.29, 0.42, 0.51, 0.65, 0.91])
+
+#master_directories.append(["h06_nu7_s1157", "h06_nu7_s1736", "h06_nu7_s2315", "h06_nu7_s3472", "h06_nu7_s5787"])
+#disc_masses.append([1760, 2620, 4120, 4875, 7500])
+#final_masses.append([0.29, 0.42, 0.51, 0.65, 0.91])
+
+master_directories.append(["h06_nu7_s0578", "h06_nu7_s0694", "h06_nu7_s0926", "h06_nu7_s1157", "h06_nu7_s1736", "h06_nu7_s2315", "h06_nu7_s3472", "h06_nu7_s4629", "h06_nu7_s5787"])
+disc_masses.append([0.5, 0.6, 0.8, 1.0, 1.5, 2.0, 3.0, 4.0, 5.0])
+final_masses.append([0.15, 0.17, 0.23, 0.28, 0.39, 0.50, 0.71, 0.94, 1.19]) # 1.19
+
+master_directories.append(["h06_nu7_s0694", "h06_nu7_s0809", "h06_nu7_s0926", "h06_nu7_s1157", "h06_nu7_s1736", "h06_nu7_s2315", "h06_nu7_s3472", "h06_nu7_s4629"])
+disc_masses.append([0.6, 0.8, 0.9, 1.0, 1.5, 2.0, 3.0, 5.0])
+final_masses.append([0.15, 0.17, 0.20, 0.26, 0.40, 0.51, 0.80, 1.4]) # 1.40
+
+
+#master_directories.append(["h04_nu7_a100", "h04_nu7_a50", "h04_nu7_a167", "h04_nu7_a05"])
+#lifetimes.append([2038, 1155, 1794, 2695])
+#final_masses.append([0.67, 0.53, 0.36, 0.21])
+
+#master_directories.append(["h08_nu6_a167", "h08_nu6_a05", "h08_nu6_a02"])
+#lifetimes.append([1440, 1526, 0])
+#final_masses.append([1.26, 0.70, 0.37])
+
+#master_directories.append(["h06_nu6_a50", "h06_nu6_a167", "h06_nu6_a05"])
+#lifetimes.append([559, 1089, 930])
+#final_masses.append([1.18, 0.84, 0.52])
+
+###############################################################################
+
+### Helper Functions ###
+
+def get_final_mass(directory):
+    planet_data = np.loadtxt("../%s/planet0.dat" % directory)
+    times = planet_data[:, 0]
+    masses = planet_data[:, 7] + planet_data[:, 8] # Core Mass + Accreted Mass
+
+    time_index = np.searchsorted(times, 2000)
+    final_mass = masses[time_index] / 0.001
+    return final_mass
+
+
+### PLOTTING ###
+linewidth = 6
+labelsize = 17
+fontsize = 20
+markersize = 12
+dpi = 100
+alpha = 0.8
+
+rc['xtick.labelsize'] = labelsize
+rc['ytick.labelsize'] = labelsize
+
+linestyles = ["-", "-", "-", "-", "--"]
+linewidths = [linewidth, linewidth, linewidth, linewidth]
+#colors = ["darkblue", "gold", "deepskyblue", "orange", "k"]
+#colors = ["b", "#17becf", "gold", "orange", "r"]
+colors = ["b", "orange", "gold"]
+#markers = ["s", "*", "p", "^", "h"]
+markers = ["s", "p", "^", "*", "h"]
+labels = ["2-D", "2-D low res", "3-D low-res"]
+
+def make_plot():
+    # Set up figure
+    fig = plot.figure(figsize = (8, 6), dpi = dpi)
+    ax = fig.add_subplot(111)
+
+    # Data
+    for i, (directories, disc_masses_i, final_masses_i) in enumerate(zip(master_directories, disc_masses, final_masses)):
+        scale_height = float(directories[0].split("_")[0][1:]) / 100.0
+        log_viscosity = float(directories[0].split("_")[1][2:]) - 2.0
+
+        alpha_coefficent = "3"
+        if scale_height == 0.08:
+            alpha_coefficent = "1.5"
+        elif scale_height == 0.04:
+            alpha_coefficent = "6"
+        #label = r"$h =$ $%.02f$, $\alpha = %s \times 10^{-%d}$" % (scale_height, alpha_coefficent, log_viscosity)
+        #label = r"$h =$ $%.02f$" % (scale_height)
+        label = labels[i]
+
+        xs = disc_masses_i
+        ys = final_masses_i
+
+        plot.plot(xs, ys, c = colors[i], marker = markers[i], markersize = markersize, linewidth = linewidths[i], linestyle = linestyles[i], label = label, zorder = 99, alpha = alpha)
+
+        # Indicate vortices that aren't dead yet
+        dx = 0; dy = 600; head_width = 0.05; head_length = 200
+
+        for (xi, yi) in zip(xs, ys):
+            if yi >= 5000:
+                plot.arrow(xi, yi, dx, dy, linewidth = linewidths[i] - 1, head_width = head_width, head_length = head_length, fc = colors[i],  ec = colors[i], alpha = alpha)
+
+        offset_x = 0.025; offset_y = 200
+        #plot.text(xs[0] + offset_x, ys[0] + offset_y, "S", color = colors[i], fontsize = fontsize - 3)
+        #plot.text(xs[1] + offset_x, ys[1] + offset_y, "S", color = colors[i], fontsize = fontsize - 3)
+
+        # Create break in legend
+        #if directories == master_directories[2]:
+        #    plot.plot([1, 1], [1, 1], c = "white", label = " ")
+
+    plot.legend(loc = 'lower right', fontsize = fontsize - 3)
+
+    # Axes
+    plot.xlim(0.2, 6)
+    plot.ylim(0.08, 1.5)
+    plot.xscale('log')
+    plot.yscale('log')
+
+    # Annotate
+    plot.xlabel(r"Disc Mass [$\Sigma_0$ / $\Sigma_\mathrm{base}$]", fontsize = fontsize)
+    plot.ylabel(r"Planet Mass [Jupiter masses]", fontsize = fontsize)
+    #plot.title("Total Lifetimes of Dust Asymmetry", y = 1.02, fontsize = fontsize + 1)
+
+    xtext = 0.025 * (plot.xlim()[1] - plot.xlim()[0]) + plot.xlim()[0]
+    ytext = 0.90 * (plot.ylim()[1] - plot.ylim()[0]) + plot.ylim()[0]
+    enter = 0.08 * (plot.ylim()[1] - plot.ylim()[0]) + plot.ylim()[0]
+
+    text1 = r"$h = 0.06$"; text2 = r"$\alpha = 3 \times 10^{-5}$"
+    plot.text(xtext, ytext, text1, fontsize = fontsize)
+    plot.text(xtext, ytext - enter, text2, fontsize = fontsize)
+
+    # Save + Show
+    plot.savefig("planet_mass_disc_mass_comparison-3D.png", bbox_inches = "tight")
+    plot.savefig("planet_mass_disc_mass_comparison-3D.pdf", bbox_inches = "tight", format = "pdf")
+    plot.show()
+
+make_plot()
