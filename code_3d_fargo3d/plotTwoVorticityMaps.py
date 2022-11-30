7c72c4fd668e88e1aa2347b73312e809392b8271
Sportsfan77777 <mhammer44444@gmail.com>
2022-12-01 04:12:58 +0800

    two vorticity

--- code_3d_fargo3d/plotTwoVorticityMaps.py
+++ code_3d_fargo3d/plotTwoVorticityMaps.py
@@ -49,7 +49,7 @@ from pylab import fromfile
 import util
 import utilVorticity
 import azimuthal as az
-from readTitle import readTitle
+#from readTitle import readTitle
 
 from advanced import Parameters
 #from reader import Fields
