bca10d28be351e2cd4de823ae2086cd5b6b3461d
Sportsfan77777 <mhammer44444@gmail.com>
2023-08-31 23:55:17 +0800

    first commit to new torque

--- code_dust_fargo3d/plotTorqueMap.py
+++ code_dust_fargo3d/plotTorqueMap.py
@@ -70,8 +70,8 @@ def new_argument_parser(description = "Plot gas density maps."):
                          help = 'number of cores (default: 1)')
 
     # Files
-    parser.add_argument('--dir', dest = "save_directory", default = "gasDensityMaps",
-                         help = 'save directory (default: gasDensityMaps)')
+    parser.add_argument('--dir', dest = "save_directory", default = "torqueMaps",
+                         help = 'save directory (default: torqueMaps)')
     parser.add_argument('-m', dest = "mpi", action = 'store_true', default = False,
                          help = 'use .mpio output files (default: use dat)')
     parser.add_argument('--merge', dest = "merge", type = int, default = 0,
