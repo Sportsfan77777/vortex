686daa9b33ad3cc07491d8231f6fd4892934c66e
Sportsfan77777 <mhammer44444@gmail.com>
2023-09-08 04:33:41 +0800

    first commit to radial torque

---
+++ code_dust_fargo3d/plotRadialTorqueProfile.py
@@ -0,0 +1,453 @@
+"""
+plot 2-D dust density maps
+
+usage: plotDustDensityMaps.py [-h] [-c NUM_CORES] [--dir SAVE_DIRECTORY]
+                              [--hide] [-v VERSION] [--range R_LIM R_LIM]
+                              [--shift] [--cmap CMAP] [--cmax CMAX]
+                              [--fontsize FONTSIZE] [--dpi DPI]
+                              frames [frames ...]
+
+positional arguments:
+  frames                select single frame or range(start, end, rate). error
+                        if nargs != 1 or 3
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c NUM_CORES          number of cores (default: 1)
+  --dir SAVE_DIRECTORY  save directory (default: dustDensityMaps)
+  --hide                for single plot, do not display plot (default: display
+                        plot)
+  -v VERSION            version number (up to 4 digits) for this set of plot
+                        parameters (default: None)
+  --range R_LIM R_LIM   radial range in plot (default: [r_min, r_max])
+  --shift               center frame on vortex peak or middle (default: do not
+                        center)
+  --cmap CMAP           color map (default: viridis)
+  --cmax CMAX           maximum density in colorbar (default: 10 for hcm+, 2.5
+                        otherwise)
+  --fontsize FONTSIZE   fontsize of plot annotations (default: 16)
+  --dpi DPI             dpi of plot annotations (default: 100)
+"""
+
+import sys, os, subprocess
+import pickle, glob
+from multiprocessing import Pool
+import argparse
+
+import math
+import numpy as np
+
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import rcParams as rc
+from matplotlib import pyplot as plot
+
+from pylab import rcParams
+from pylab import fromfile
+
+import util
+import azimuthal as az
+#from readTitle import readTitle
+
+from advanced import Parameters
+#from reader_mpiio import Fields
+
+from colormaps import cmaps
+for key in cmaps:
+    plot.register_cmap(name = key, cmap = cmaps[key])
+
+###############################################################################
+
+### Input Parameters ###
+
+def new_argument_parser(description = "Plot gas density maps."):
+    parser = argparse.ArgumentParser()
+
+    # Frame Selection
+    parser.add_argument('frames', type = int, nargs = '+',
+                         help = 'select single frame or range(start, end, rate). error if nargs != 1 or 3')
+    parser.add_argument('-c', dest = "num_cores", type = int, default = 1,
+                         help = 'number of cores (default: 1)')
+
+    # Files
+    parser.add_argument('--dir', dest = "save_directory", default = "radialTorqueProfiles",
+                         help = 'save directory (default: radialTorqueProfiles)')
+    parser.add_argument('-m', dest = "mpi", action = 'store_true', default = False,
+                         help = 'use .mpio output files (default: use dat)')
+    parser.add_argument('--merge', dest = "merge", type = int, default = 0,
+                         help = 'number of cores needed to merge data outputs (default: 0)')
+
+    # Plot Parameters (variable)
+    parser.add_argument('--hide', dest = "show", action = 'store_false', default = True,
+                         help = 'for single plot, do not display plot (default: display plot)')
+    parser.add_argument('-v', dest = "version", type = int, default = None,
+                         help = 'version number (up to 4 digits) for this set of plot parameters (default: None)')
+
+    parser.add_argument('--range', dest = "r_lim", type = float, nargs = 2, default = None,
+                         help = 'radial range in plot (default: [r_min, r_max])')
+    parser.add_argument('--shift', dest = "center", action = 'store_true', default = False,
+                         help = 'center frame on vortex peak or middle (default: do not center)')
+    
+    # Plot Parameters (rarely need to change)
+    parser.add_argument('--cmap', dest = "cmap", default = "seismic",
+                         help = 'color map (default: viridis)')
+    parser.add_argument('--cmax', dest = "cmax", type = float, default = 1.0e-5,
+                         help = 'maximum density in colorbar (default: 1.0e-5)')
+
+    parser.add_argument('--fontsize', dest = "fontsize", type = int, default = 16,
+                         help = 'fontsize of plot annotations (default: 16)')
+    parser.add_argument('--dpi', dest = "dpi", type = int, default = 100,
+                         help = 'dpi of plot annotations (default: 100)')
+
+    return parser
+
+###############################################################################
+
+### Parse Arguments ###
+args = new_argument_parser().parse_args()
+
+### Get Fargo Parameters ###
+p = Parameters()
+
+num_rad = p.ny; num_theta = p.nx; num_z = p.nz
+r_min = p.ymin; r_max = p.ymax
+z_min = p.zmin; z_max = p.zmax
+
+surface_density_zero = p.sigma0
+
+dt = p.ninterm * p.dt
+
+fargo_par = util.get_pickled_parameters()
+jupiter_mass = 1e-3
+planet_mass = fargo_par["PlanetMass"] / jupiter_mass
+accretion = fargo_par["Accretion"]
+taper_time = p.masstaper
+
+scale_height = p.aspectratio
+viscosity = 1e-7 #p.nu
+
+"""
+
+num_rad = fargo_par["Nrad"]; num_theta = fargo_par["Nsec"]
+r_min = fargo_par["Rmin"]; r_max = fargo_par["Rmax"]
+
+
+taper_time = fargo_par["MassTaper"]
+
+surface_density_zero = fargo_par["Sigma0"]
+disk_mass = 2 * np.pi * surface_density_zero * (r_max - r_min) / jupiter_mass # M_{disk} = (2 \pi) * \Sigma_0 * r_p * (r_out - r_in)
+
+scale_height = fargo_par["AspectRatio"]
+viscosity = fargo_par["Viscosity"]
+
+size = fargo_par["PSIZE"]
+"""
+
+### Get Input Parameters ###
+
+# Frames
+frame_range = util.get_frame_range(args.frames)
+
+# Number of Cores 
+num_cores = args.num_cores
+
+# Files
+save_directory = args.save_directory
+if not os.path.isdir(save_directory):
+    os.mkdir(save_directory) # make save directory if it does not already exist
+
+merge = args.merge
+mpi = args.mpi
+
+# Plot Parameters (variable)
+show = args.show
+
+rad = np.linspace(r_min, r_max, num_rad)
+theta = np.linspace(0, 2 * np.pi, num_theta)
+z_angles = np.linspace(z_min, z_max, num_z)
+
+version = args.version
+if args.r_lim is None:
+    x_min = r_min; x_max = r_max
+else:
+    x_min = args.r_lim[0]; x_max = args.r_lim[1]
+center = args.center
+
+# Plot Parameters (constant)
+cmap = args.cmap
+clim = [-args.cmax, args.cmax]
+
+fontsize = args.fontsize
+dpi = args.dpi
+
+# Planet File
+# Data
+data = np.loadtxt("planet0.dat")
+times = data[:, 0]; base_mass = data[:, 7]
+accreted_mass = data[:, 8] / jupiter_mass
+
+planet_xs = data[:, 1]; planet_ys = data[:, 2] #; planet_zs = data[:, 3]
+
+BigG = 1.0
+
+### Add new parameters to dictionary ###
+fargo_par["rad"] = rad
+fargo_par["theta"] = theta
+
+### Helper Functions ###
+
+def shift_density(normalized_density, fargo_par, option = "away", reference_density = None, frame = None):
+    """ shift density based on option """
+    if reference_density is None:
+       reference_density = normalized_density
+
+    # Options
+    if option == "peak":
+       shift_c = az.get_azimuthal_peak(reference_density, fargo_par)
+    elif option == "threshold":
+       threshold = util.get_threshold(fargo_par["PSIZE"])
+       shift_c = az.get_azimuthal_center(reference_density, fargo_par, threshold = threshold)
+    elif option == "away":
+       shift_c = az.shift_away_from_minimum(reference_density, fargo_par)
+    elif option == "lookup":
+       shift_c = az.get_lookup_shift(frame)
+    else:
+       print "Invalid centering option. Choose (cm-)peak, (cm-)threshold, (cm-)away, or lookup"
+
+    # Shift
+    shifted_density = np.roll(normalized_density, shift_c, axis = -1)
+    return shifted_density, shift_c
+
+###############################################################################
+
+def generate_colors(n):
+    c = ['yellow', 'b', 'firebrick', 'w', 'green']
+    colors = []
+    for i in range(n):
+        colors.append(c[i % len(c)])
+    return colors
+
+##### PLOTTING #####
+
+def make_plot(frame, show = False):
+    # Set up figure
+    fig = plot.figure(figsize = (7, 6), dpi = dpi)
+    ax = fig.add_subplot(111)
+
+    # Find planet
+    frame_i = np.searchsorted(times, frame)
+    px = planet_xs[frame_i]
+    py = planet_ys[frame_i]
+
+    # Data
+    density = fromfile("gasdens%d.dat" % frame).reshape(num_rad, num_theta)
+
+    r_element = np.array([np.outer(rad, np.cos(theta)), np.outer(rad, np.sin(theta))]) # star to fluid element 
+    r_diff = np.array([np.outer(rad, np.cos(theta)) - px, np.outer(rad, np.sin(theta)) - py]) # planet to fluid element
+    dist_sq = np.einsum("ijk,ijk->jk", r_diff, r_diff)
+
+    coeff = BigG * planet_mass * density / dist_sq
+    direction = np.cross(r_element, r_diff, axis = 0)
+
+    torque_density_per_area = coeff * direction
+    d_rad = rad[1] - rad[0]; d_theta = theta[1] - theta[0]
+    area = rad[:, None] * np.outer(d_rad, d_theta)
+
+    torque_density = torque_density_per_area * area
+    normalized_torque_density = torque_density / surface_density_zero # / np.sqrt(2.0 * np.pi) / scale_height_function[:, None]
+
+    if center:
+        normalized_density, shift_c = shift_density(normalized_torque_density, fargo_par, reference_density = normalized_density)
+
+    ### Plot ###
+    renormalization = 1.0e-5
+
+    x = rad
+    y = theta * (180.0 / np.pi)
+    result = ax.pcolormesh(x, y, np.transpose(normalized_torque_density) / renormalization, cmap = cmap)
+
+    cbar = fig.colorbar(result)
+    result.set_clim(clim[0] / renormalization, clim[1] / renormalization)
+
+    # Mark planet
+    planet_r = np.sqrt(np.power(px, 2.0) + np.power(py, 2.0))
+    off_plot_theta = -1.0 / 120.0
+    #plot.scatter(planet_r, off_plot_theta, c = "k", marker = "^", s = 20, clip_on = False)
+
+    # Axes
+    plot.xlim(x_min, x_max)
+    plot.ylim(0, 360)
+
+    angles = np.linspace(0, 360, 7)
+    plot.yticks(angles)
+
+    # Annotate Axes
+    orbit = (dt / (2 * np.pi)) * frame
+
+    if orbit >= taper_time:
+        current_mass = planet_mass
+    else:
+        current_mass = np.power(np.sin((np.pi / 2) * (1.0 * orbit / taper_time)), 2) * planet_mass
+
+    current_mass += accreted_mass[frame]
+
+    #title = readTitle()
+
+    unit = "r_\mathrm{p}"
+    plot.xlabel(r"Radius [$%s$]" % unit, fontsize = fontsize)
+    plot.ylabel(r"$\phi$ [degrees]", fontsize = fontsize)
+
+    x_range = x_max - x_min; x_mid = x_min + x_range / 2.0
+    y_text = 1.14
+
+    alpha_coefficent = "3"
+    if scale_height == 0.08:
+        alpha_coefficent = "1.5"
+    elif scale_height == 0.04:
+        alpha_coefficent = "6"
+
+    title1 = r"$h/r = %.2f$     $\alpha \approx %s \times 10^{%d}$    $A = %.2f$" % (scale_height, alpha_coefficent, int(np.log(viscosity) / np.log(10)) + 2, accretion)
+    #title1 = r"$\Sigma_0 = %.3e$  $M_c = %.2f\ M_J$  $A = %.2f$" % (surface_density_zero, planet_mass, accretion)
+    title2 = r"$t = %d$ $\mathrm{orbits}}$  [$m_\mathrm{p}(t)\ =\ %.2f$ $M_\mathrm{Jup}$]" % (orbit, current_mass)
+    plot.title("%s" % (title2), y = 1.015, fontsize = fontsize + 1)
+    plot.text(x_mid, y_text * plot.ylim()[-1], title1, horizontalalignment = 'center', bbox = dict(facecolor = 'none', edgecolor = 'black', linewidth = 1.5, pad = 7.0), fontsize = fontsize + 2)
+
+    cbar.set_label(r"Specific Torque / 1.0e-5", fontsize = fontsize, rotation = 270, labelpad = 25)
+
+
+##### PLOTTING #####
+
+def make_plot(show = False):
+    # Set up figure
+    fig = plot.figure(figsize = (7, 6), dpi = dpi)
+    ax = fig.add_subplot(111)
+
+    # Find planet
+    frame_i = np.searchsorted(times, frame)
+    px = planet_xs[frame_i]
+    py = planet_ys[frame_i]
+
+    # Data
+    density = fromfile("gasdens%d.dat" % frame).reshape(num_rad, num_theta)
+
+    r_element = np.array([np.outer(rad, np.cos(theta)), np.outer(rad, np.sin(theta))]) # star to fluid element 
+    r_diff = np.array([np.outer(rad, np.cos(theta)) - px, np.outer(rad, np.sin(theta)) - py]) # planet to fluid element
+    dist_sq = np.einsum("ijk,ijk->jk", r_diff, r_diff)
+
+    coeff = BigG * planet_mass * density / dist_sq
+    direction = np.cross(r_element, r_diff, axis = 0)
+
+    torque_density_per_area = coeff * direction
+    d_rad = rad[1] - rad[0]; d_theta = theta[1] - theta[0]
+    area = rad[:, None] * np.outer(d_rad, d_theta)
+
+    torque_density = torque_density_per_area * area
+    normalized_torque_density = torque_density / surface_density_zero # / np.sqrt(2.0 * np.pi) / scale_height_function[:, None]
+
+    radial_torque = np.average(normalized_density, axis = -1)
+
+    if center:
+        normalized_density, shift_c = shift_density(normalized_torque_density, fargo_par, reference_density = normalized_density)
+
+    ### Data ###
+    # Planet
+    cwd = os.getcwd().split("/")[-1]
+
+    ### Plot ###
+    kernel_size = 5
+
+    x = rad
+    y = radial_torque
+    result = plot.plot(x, y1, linewidth = linewidth, linestyle = "-.", zorder = 99, label = "%s (Inner)" % cwd)
+
+    if args.ref > 0:
+        x = times
+        y_ref = np.power(np.sin((np.pi / 2) * (1.0 * times / args.ref)), 2) * 1.0
+        plot.plot(x, y_ref, linewidth = linewidth, alpha = 0.5)
+
+    if args.compare is not None:
+        for i, directory in enumerate(args.compare):
+            data_comp = np.loadtxt("%s/planet0.dat" % directory)
+            times = data_comp[:, 0] + args.offset
+
+            planet_x = data_comp[:, 1]
+            planet_y = data_comp[:, 2]
+            planet_radii = np.sqrt(np.power(planet_x, 2) + np.power(planet_y, 2))
+
+            x_comp = times
+            y_comp = planet_radii
+            result = plot.plot(x_comp, y_comp, linewidth = linewidth, zorder = 1, label = "%s" % directory)
+
+    #plot.legend(loc = "lower left")
+
+    # Axes
+    if args.max_y is None:
+        min_y = -1.1 * min(y)
+        max_y = 1.1 * max(y)
+
+        max_y = max([min_y, max_y])
+    else:
+        max_y = args.max_y
+
+    plot.xlim(x_min, x_max)
+    plot.ylim(-max_y, max_y)
+
+    #title = readTitle()
+
+    # Annotate Axes
+    orbit = (dt / (2 * np.pi)) * frame
+
+    if orbit >= taper_time:
+        current_mass = planet_mass
+    else:
+        current_mass = np.power(np.sin((np.pi / 2) * (1.0 * orbit / taper_time)), 2) * planet_mass
+
+    current_mass += accreted_mass[frame]
+
+    unit = "r_\mathrm{p}"
+    plot.xlabel(r"Radius [%s]" % unit, fontsize = fontsize)
+    plot.ylabel(r"Torque", fontsize = fontsize)
+
+    x_range = x_max - x_min; x_mid = x_min + x_range / 2.0
+    y_text = 1.14
+
+    alpha_coefficent = "3"
+    if scale_height == 0.08:
+        alpha_coefficent = "1.5"
+    elif scale_height == 0.04:
+        alpha_coefficent = "6"
+
+    title1 = r"$h/r = %.2f$     $\alpha \approx %s \times 10^{%d}$    $A = %.2f$" % (scale_height, alpha_coefficent, int(np.log(viscosity) / np.log(10)) + 2, accretion)
+    #title1 = r"$\Sigma_0 = %.3e$  $M_c = %.2f\ M_J$  $A = %.2f$" % (surface_density_zero, planet_mass, accretion)
+    title2 = r"$t = %d$ $\mathrm{orbits}}$  [$m_\mathrm{p}(t)\ =\ %.2f$ $M_\mathrm{Jup}$]" % (orbit, current_mass)
+    plot.title("%s" % (title2), y = 1.015, fontsize = fontsize + 1)
+    plot.text(x_mid, y_text * plot.ylim()[-1], title1, horizontalalignment = 'center', bbox = dict(facecolor = 'none', edgecolor = 'black', linewidth = 1.5, pad = 7.0), fontsize = fontsize + 2)
+
+
+    # Save, Show, and Close
+    if version is None:
+        save_fn = "%s/radialTorqueProfile_%04d.png" % (save_directory, frame)
+    else:
+        save_fn = "%s/v%04d_radialTorqueProfile_%04d.png" % (save_directory, version, frame)
+    plot.savefig(save_fn, bbox_inches = 'tight', dpi = dpi)
+
+    if show:
+        plot.show()
+
+    plot.close(fig) # Close Figure (to avoid too many figures)
+
+##### Make Plots! #####
+
+# Iterate through frames
+
+if len(frame_range) == 1:
+    make_plot(frame_range[0], show = show)
+else:
+    if num_cores > 1:
+        p = Pool(num_cores) # default number of processes is multiprocessing.cpu_count()
+        p.map(make_plot, frame_range)
+        p.terminate()
+    else:
+        for frame in frame_range:
+            make_plot(frame)
+
