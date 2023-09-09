b4fdfbfe0f7095f55d2d73d6d4606e88f4357cab
Sportsfan77777 <mhammer44444@gmail.com>
2023-09-10 03:00:16 +0800

    add hill cut

--- code_dust_fargo3d/plotRadialTorqueProfile.py
+++ code_dust_fargo3d/plotRadialTorqueProfile.py
@@ -263,8 +263,8 @@ def make_plot(frame, show = False):
     hill_cut[dist_sq < hill_radius] = 0.0
     normalized_torque_density *= hill_cut
 
-    # Average
-    radial_torque = np.average(normalized_torque_density, axis = -1)
+    # Total
+    radial_torque = np.sum(normalized_torque_density, axis = -1)
 
     ### Data ###
     # Planet
@@ -317,7 +317,7 @@ def make_plot(frame, show = False):
     current_mass += accreted_mass[frame]
 
     unit = "r_\mathrm{p}"
-    plot.xlabel(r"Radius [%s$]" % unit, fontsize = fontsize)
+    plot.xlabel(r"Radius [$%s$]" % unit, fontsize = fontsize)
     plot.ylabel(r"Torque", fontsize = fontsize)
 
     x_range = x_max - x_min; x_mid = x_min + x_range / 2.0
