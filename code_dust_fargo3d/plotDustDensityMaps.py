66afaf61011a6b08cd9abae8420deab0b808a998
Sportsfan77777 <mhammer44444@gmail.com>
2024-10-17 13:19:38 -0400

    added dust number

--- code_dust_fargo3d/plotDustDensityMaps.py
+++ code_dust_fargo3d/plotDustDensityMaps.py
@@ -369,97 +369,10 @@ def make_plot(frame, show = False):
 
     # Save, Show, and Close
     if version is None:
-        save_fn = "%s/dustDensityMap_%04d.png" % (save_directory, frame)
-    else:
-        save_fn = "%s/v%04d_dustDensityMap_%04d.png" % (save_directory, version, frame)
-    plot.savefig(save_fn, bbox_inches = 'tight', dpi = dpi, pad_inches = 0.2)
-
-    if show:
-        plot.show()
-
-    plot.close(fig) # Close Figure (to avoid too many figures)
-
-def old_make_plot(frame, show = False):
-    # Set up figure
-    fig = plot.figure(figsize = (7, 6), dpi = dpi)
-    ax = fig.add_subplot(111)
-
-    # Data
-    density = (fromfile("gasdens%d.dat" % frame).reshape(num_rad, num_theta))
-    if center:
-        if taper_time < 10.1:
-            shift_c = az.get_azimuthal_peak(density, fargo_par)
-        else:
-            threshold = util.get_threshold(size)
-            shift_c = az.get_azimuthal_center(density, fargo_par, threshold = threshold * surface_density_zero)
-        density = np.roll(density, shift_c)
-    normalized_density = density / dust_surface_density_zero
-
-    ### Plot ###
-    x = rad
-    y = theta * (180.0 / np.pi)
-    result = ax.pcolormesh(x, y, np.transpose(normalized_density), cmap = cmap)
-
-    fig.colorbar(result)
-    result.set_clim(clim[0], clim[1])
-
-    if use_contours:
-        gas_density = Fields("./", 'gas', frame).get_field(field).reshape(num_rad, num_theta) / surface_density_zero
-        levels = np.linspace(low_contour, high_contour, num_levels)
-        colors = generate_colors(num_levels)
-        plot.contour(x, y, np.transpose(gas_density), levels = levels, origin = 'upper', linewidths = 1, colors = colors)
-
-    # Axes
-    plot.xlim(x_min, x_max)
-    plot.ylim(0, 360)
-
-    angles = np.linspace(0, 360, 7)
-    plot.yticks(angles)
-
-    # Annotate Axes
-    time = fargo_par["Ninterm"] * fargo_par["DT"]
-    orbit = (time / (2 * np.pi)) * frame
-
-    if orbit >= taper_time:
-        current_mass = planet_mass
-    else:
-        current_mass = np.power(np.sin((np.pi / 2) * (1.0 * orbit / taper_time)), 2) * planet_mass
-
-    #title = readTitle()
-
-    unit = "r_\mathrm{p}"
-    plot.xlabel(r"Radius [$%s$]" % unit, fontsize = fontsize)
-    plot.ylabel(r"$\phi$ (degrees)", fontsize = fontsize)
-
-    #if title is None:
-    #    plot.title("Dust Density Map\n(t = %.1f)" % (orbit), fontsize = fontsize + 1)
-    #else:
-    #    plot.title("Dust Density Map\n%s\n(t = %.1f)" % (title, orbit), fontsize = fontsize + 1)
-
-    x_range = x_max - x_min; x_mid = x_min + x_range / 2.0
-    y_text = 1.14
-
-    title1 = r"$T_\mathrm{growth} = %d$ $\mathrm{orbits}$" % (taper_time)
-    title2 = r"$t = %d$ $\mathrm{orbits}}$  [$m_\mathrm{p}(t)\ =\ %.2f$ $M_\mathrm{Jup}$]" % (orbit, current_mass)
-    plot.title("%s" % (title2), y = 1.015, fontsize = fontsize + 1)
-    plot.text(x_mid, y_text * plot.ylim()[-1], title1, horizontalalignment = 'center', bbox = dict(facecolor = 'none', edgecolor = 'black', linewidth = 1.5, pad = 7.0), fontsize = fontsize + 2)
-
-    # Text
-    text_mass = r"$M_\mathrm{p} = %d$ $M_\mathrm{Jup}$" % (int(planet_mass))
-    text_visc = r"$\alpha_\mathrm{disk} = 3 \times 10^{%d}$" % (int(np.log(viscosity) / np.log(10)) + 2)
-    #plot.text(-0.9 * box_size, 2, text_mass, fontsize = fontsize, color = 'black', horizontalalignment = 'left', bbox=dict(facecolor = 'white', edgecolor = 'black', pad = 10.0))
-    #plot.text(0.9 * box_size, 2, text_visc, fontsize = fontsize, color = 'black', horizontalalignment = 'right', bbox=dict(facecolor = 'white', edgecolor = 'black', pad = 10.0))
-    plot.text(-0.84 * x_range / 2.0 + x_mid, y_text * plot.ylim()[-1], text_mass, fontsize = fontsize, color = 'black', horizontalalignment = 'right')
-    plot.text(0.84 * x_range / 2.0 + x_mid, y_text * plot.ylim()[-1], text_visc, fontsize = fontsize, color = 'black', horizontalalignment = 'left')
-
-    cbar.set_label(r"Dust Surface Density  $\Sigma$ $/$ $\Sigma_0$", fontsize = fontsize, rotation = 270, labelpad = 25)
-
-    # Save, Show, and Close
-    if version is None:
         save_fn = "%s/dustDensityMap%d_%04d.png" % (save_directory, dust_number, frame)
     else:
         save_fn = "%s/v%04d_dustDensityMap%d_%04d.png" % (save_directory, version, dust_number, frame)
-    plot.savefig(save_fn, bbox_inches = 'tight', dpi = dpi)
+    plot.savefig(save_fn, bbox_inches = 'tight', dpi = dpi, pad_inches = 0.2)
 
     if show:
         plot.show()
