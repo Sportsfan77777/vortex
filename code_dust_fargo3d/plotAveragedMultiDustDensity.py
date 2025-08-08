d8caf4a3cb11e088ba303112ec8fdee2f7385405
Sportsfan77777 <mhammer44444@gmail.com>
2025-08-09 01:32:59 +0800

    fixes

--- code_dust_fargo3d/plotAveragedMultiDustDensity.py
+++ code_dust_fargo3d/plotAveragedMultiDustDensity.py
@@ -67,7 +67,7 @@ def new_argument_parser(description = "Plot gas density maps."):
                          help = 'radial range in plot (default: [r_min, r_max])')
     parser.add_argument('--max_y', dest = "max_y", type = float, default = None,
                          help = 'maximum density (default: 1.1 times the max)')
-    parser.add_argument('--y2_range', dest = "y2_range", type = float, nargs = 2, default = [1e-6, 2e2],
+    parser.add_argument('--y2_range', dest = "y2_range", type = float, nargs = 2, default = [1e-5, 1e3],
                          help = 'range in y-axis (default: [-0.2, 0.2])')
 
     parser.add_argument('-l', dest = "maximum_condition", action = 'store_true', default = False,
@@ -309,7 +309,7 @@ def make_plot(frame, show = False):
     #    plot.title("Dust Density Map\n%s\n(t = %.1f)" % (title, orbit), fontsize = fontsize + 1)
 
     x_range = x_max - x_min; x_mid = x_min + x_range / 2.0
-    y_text = 3.14
+    y_text = 103.14
 
     alpha_coefficent = "3"
     if scale_height == 0.08:
