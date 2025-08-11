9025e74d766b71d5843bdb23a2f1b69f6147694e
Sportsfan77777 <mhammer44444@gmail.com>
2025-08-11 22:13:14 +0800

    first commit to spatial density

--- code_dust_fargo3d/plotAveragedMultiDustSpatialDensity.py
+++ code_dust_fargo3d/plotAveragedMultiDustSpatialDensity.py
@@ -326,6 +326,7 @@ def make_plot(frame, show = False):
     unit = "r_\mathrm{p}"
     ax.set_xlabel(r"Radius [$%s$]" % unit, fontsize = fontsize)
     ax.set_ylabel(r"$\Sigma$ $/$ $\Sigma_0$", fontsize = fontsize)
+    ax2.set_ylabel(r"$\Sigma$ $/$ $\Sigma_0$ [g / cm$^3$]", fontsize = fontsize, rotation = 180)
 
     #if title is None:
     #    plot.title("Dust Density Map\n(t = %.1f)" % (orbit), fontsize = fontsize + 1)
