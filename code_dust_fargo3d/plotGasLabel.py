4c827811c75bac64006b97840282a6b9600d1ae9
Sportsfan77777 <mhammer44444@gmail.com>
2025-10-17 12:34:53 -0400

    first commit to gas label

--- code_dust_fargo3d/plotGasLabel.py
+++ code_dust_fargo3d/plotGasLabel.py
@@ -357,9 +357,9 @@ def make_plot(frame, show = False):
 
     # Save, Show, and Close
     if version is None:
-        save_fn = "%s/gasLabelMap%d_%04d.png" % (save_directory, dust_number, frame)
+        save_fn = "%s/gasLabelMap_%04d.png" % (save_directory, frame)
     else:
-        save_fn = "%s/v%04d_gasLabelMap%d_%04d.png" % (save_directory, version, dust_number, frame)
+        save_fn = "%s/v%04d_gasLabelMap_%04d.png" % (save_directory, version, frame)
     plot.savefig(save_fn, bbox_inches = 'tight', dpi = dpi, pad_inches = 0.2)
 
     if show:
